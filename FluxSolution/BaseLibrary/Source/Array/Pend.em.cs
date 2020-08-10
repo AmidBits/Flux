9674348fcdd9362dce5a1c5d3ab4bdce9c2706e5
Robert Hugo <robert_hugo@hotmail.com>
2020-08-09 23:16:09 -0700

    Consolidated the files for append and prepend.

--- FluxSolution/BaseLibrary/Source/Array/Pend.em.cs
+++ FluxSolution/BaseLibrary/Source/Array/Pend.em.cs
@@ -12,5 +12,16 @@ namespace Flux
       System.Array.Copy(append, 0, target, source.Length, append.Length);
       return target;
     }
+
+    public static T[] Prepend<T>(this T[] source, params T[] prepend)
+    {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+      if (prepend is null) throw new System.ArgumentNullException(nameof(prepend));
+
+      var target = new T[prepend.Length + source.Length];
+      System.Array.Copy(prepend, 0, target, 0, prepend.Length);
+      System.Array.Copy(source, 0, target, prepend.Length, source.Length);
+      return target;
+    }
   }
 }
