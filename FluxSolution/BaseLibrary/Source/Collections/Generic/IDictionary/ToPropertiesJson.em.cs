5573f9612132bcba03be284142720fc92cc6cbe9
Robert Hugo <Robert.Hugo@pima.gov>
2020-08-11 07:09:49 -0700

    Revert "Added stuff."
    This reverts commit dc45c3309bae596a92fdb74802a96360a80af18b.

---
+++ FluxSolution/BaseLibrary/Source/Collections/Generic/IDictionary/ToPropertiesJson.em.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+
+namespace Flux
+{
+  public static partial class XtensionsCollections
+  {
+    /// <summary></summary>
+    public static string ToPropertiesJson(this System.Collections.Generic.IDictionary<string, object> properties, System.Func<object, string> valueSelector)
+      => $"{{{string.Join(',', (properties ?? throw new System.ArgumentNullException(nameof(properties))).Select(kvp => $"{kvp.Key}={valueSelector(kvp.Value)}"))}}}";
+  }
+}
