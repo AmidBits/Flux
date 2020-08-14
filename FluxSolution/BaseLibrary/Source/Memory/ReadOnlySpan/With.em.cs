7e66260a17c4947662a82f78ca687c5e68ad4411
Robert Hugo <Robert.Hugo@pima.gov>
2020-08-14 15:54:16 -0700

    Add starts and ends with functions back in for the span types.

---
+++ FluxSolution/BaseLibrary/Source/Memory/ReadOnlySpan/With.em.cs
@@ -0,0 +1,35 @@
+namespace Flux
+{
+  public static partial class XtensionsReadOnlySpan
+  {
+    public static bool EndsWith<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> value, System.Collections.Generic.IEqualityComparer<T> comparer)
+    {
+      comparer ??= System.Collections.Generic.EqualityComparer<T>.Default;
+
+      if (source.Length < value.Length) return false;
+
+      for (int sourceIndex = source.Length - 1, valueIndex = value.Length - 1; sourceIndex >= 0 && valueIndex >= 0; sourceIndex--, valueIndex--)
+        if (!comparer.Equals(source[sourceIndex], value[valueIndex]))
+          return false;
+
+      return true;
+    }
+    public static bool EndsWith<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> value)
+      => EndsWith(source, value, System.Collections.Generic.EqualityComparer<T>.Default);
+
+    public static bool StartsWith<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> value, System.Collections.Generic.IEqualityComparer<T> comparer)
+    {
+      comparer ??= System.Collections.Generic.EqualityComparer<T>.Default;
+
+      if (source.Length < value.Length) return false;
+
+      for (var index = 0; index < value.Length; index++)
+        if (!comparer.Equals(source[index], value[index]))
+          return false;
+
+      return true;
+    }
+    public static bool StartsWith<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> value)
+       => EndsWith(source, value, System.Collections.Generic.EqualityComparer<T>.Default);
+  }
+}
