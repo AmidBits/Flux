f2d812e6238a70e9035f3a4b0784165604cf32c9
Robert Hugo <robert_hugo@hotmail.com>
2020-08-14 17:39:30 -0700

    Cleanup.

--- FluxSolution/BaseLibrary/Source/StringComparerEx.cs
+++ FluxSolution/BaseLibrary/Source/StringComparerEx.cs
@@ -7,34 +7,42 @@ namespace Flux
     , System.Collections.Generic.IComparer<char>
     , System.Collections.Generic.IEqualityComparer<char>
   {
-    public new static StringComparerEx CurrentCulture { get; private set; } = new StringComparerEx(System.StringComparer.CurrentCulture);
-    public new static StringComparerEx CurrentCultureIgnoreCase { get; private set; } = new StringComparerEx(System.StringComparer.CurrentCultureIgnoreCase);
-    public static StringComparerEx CurrentCultureIgnoreNonSpace { get; private set; } = new StringComparerEx(System.Globalization.CultureInfo.CurrentCulture, System.Globalization.CompareOptions.IgnoreNonSpace);
-    public static StringComparerEx CurrentCultureIgnoreNonSpaceAndCase { get; private set; } = new StringComparerEx(System.Globalization.CultureInfo.CurrentCulture, System.Globalization.CompareOptions.IgnoreCase | System.Globalization.CompareOptions.IgnoreNonSpace);
-    public new static StringComparerEx InvariantCulture { get; private set; } = new StringComparerEx(System.StringComparer.InvariantCulture);
-    public new static StringComparerEx InvariantCultureIgnoreCase { get; private set; } = new StringComparerEx(System.StringComparer.InvariantCultureIgnoreCase);
-    public static StringComparerEx InvariantCultureIgnoreNonSpace { get; private set; } = new StringComparerEx(System.Globalization.CultureInfo.InvariantCulture, System.Globalization.CompareOptions.IgnoreNonSpace);
-    public static StringComparerEx InvariantCultureIgnoreNonSpaceAndCase { get; private set; } = new StringComparerEx(System.Globalization.CultureInfo.InvariantCulture, System.Globalization.CompareOptions.IgnoreCase | System.Globalization.CompareOptions.IgnoreNonSpace);
-    public new static StringComparerEx Ordinal { get; private set; } = new StringComparerEx(System.StringComparer.Ordinal);
-    public new static StringComparerEx OrdinalIgnoreCase { get; private set; } = new StringComparerEx(System.StringComparer.OrdinalIgnoreCase);
+    public new static StringComparerEx CurrentCulture { get; } = new StringComparerEx(System.StringComparer.CurrentCulture);
+    public new static StringComparerEx CurrentCultureIgnoreCase { get; } = new StringComparerEx(System.StringComparer.CurrentCultureIgnoreCase);
+    public static StringComparerEx CurrentCultureIgnoreNonSpace { get; } = new StringComparerEx(System.Globalization.CultureInfo.CurrentCulture, System.Globalization.CompareOptions.IgnoreNonSpace);
+    public static StringComparerEx CurrentCultureIgnoreNonSpaceAndCase { get; } = new StringComparerEx(System.Globalization.CultureInfo.CurrentCulture, System.Globalization.CompareOptions.IgnoreCase | System.Globalization.CompareOptions.IgnoreNonSpace);
+    public new static StringComparerEx InvariantCulture { get; } = new StringComparerEx(System.StringComparer.InvariantCulture);
+    public new static StringComparerEx InvariantCultureIgnoreCase { get; } = new StringComparerEx(System.StringComparer.InvariantCultureIgnoreCase);
+    public static StringComparerEx InvariantCultureIgnoreNonSpace { get; } = new StringComparerEx(System.Globalization.CultureInfo.InvariantCulture, System.Globalization.CompareOptions.IgnoreNonSpace);
+    public static StringComparerEx InvariantCultureIgnoreNonSpaceAndCase { get; } = new StringComparerEx(System.Globalization.CultureInfo.InvariantCulture, System.Globalization.CompareOptions.IgnoreCase | System.Globalization.CompareOptions.IgnoreNonSpace);
+    public new static StringComparerEx Ordinal { get; } = new StringComparerEx(System.StringComparer.Ordinal);
+    public new static StringComparerEx OrdinalIgnoreCase { get; } = new StringComparerEx(System.StringComparer.OrdinalIgnoreCase);
 
     private readonly System.StringComparer m_stringComparer;
 
     public StringComparerEx(System.Globalization.CultureInfo cultureInfo, bool ignoreCase)
       => m_stringComparer = System.StringComparer.Create(cultureInfo, ignoreCase);
     public StringComparerEx(System.Globalization.CultureInfo cultureInfo, System.Globalization.CompareOptions compareOptions)
-      => m_stringComparer = System.Globalization.GlobalizationExtensions.GetStringComparer(cultureInfo.CompareInfo, compareOptions);
+      => m_stringComparer = System.Globalization.GlobalizationExtensions.GetStringComparer((cultureInfo ?? throw new System.ArgumentNullException(nameof(cultureInfo))).CompareInfo, compareOptions);
     private StringComparerEx(System.StringComparer stringComparer)
       => m_stringComparer = stringComparer; // Public instances must specify settings on creation.
 
-    public override int Compare(string? x, string? y) => m_stringComparer.Compare(x, y);
-    public override bool Equals(string? x, string? y) => m_stringComparer.Equals(x, y);
-    public override int GetHashCode(string s) => m_stringComparer.GetHashCode(s);
+    // IComparer
+    public override int Compare(string? x, string? y)
+      => m_stringComparer.Compare(x, y);
+    // IEqualityComparer
+    public override bool Equals(string? x, string? y)
+      => m_stringComparer.Equals(x, y);
+    public override int GetHashCode(string s)
+      => m_stringComparer.GetHashCode(s);
 
     // IComparer
-    public int Compare(char x, char y) => m_stringComparer.Compare(x.ToString(), y.ToString());
+    public int Compare(char x, char y)
+      => m_stringComparer.Compare(x.ToString(), y.ToString());
     // IEqualityComparer
-    public bool Equals(char x, char y) => m_stringComparer.Equals(x.ToString(), y.ToString());
-    public int GetHashCode(char c) => m_stringComparer.GetHashCode(c.ToString());
+    public bool Equals(char x, char y)
+      => m_stringComparer.Equals(x.ToString(), y.ToString());
+    public int GetHashCode(char c)
+      => m_stringComparer.GetHashCode(c.ToString());
   }
 }
