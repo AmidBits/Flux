0f46dc8ad59b99c5a785da2ac6f67098f1687b90
Robert Hugo <robert_hugo@hotmail.com>
2020-08-21 21:30:41 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Collections/Immutable/BinaryTree/IBinaryTree.cs
+++ FluxSolution/BaseLibrary/Source/Collections/Immutable/BinaryTree/IBinaryTree.cs
@@ -3,7 +3,35 @@ namespace Flux
   public static partial class XtendImmutable
   {
     public static int GetNodeCount<TValue>(this Collections.Immutable.IBinaryTree<TValue> source)
-      => source?.IsEmpty ?? throw new System.ArgumentNullException(nameof(source)) ? 0 : 1 + source.Left.GetNodeCount() + source.Right.GetNodeCount();
+      => source?.IsEmpty ?? throw new System.ArgumentNullException(nameof(source)) ? 0 : 1 + GetNodeCount(source.Left) + GetNodeCount(source.Right);
+
+    public static int Minimax<TValue>(this Collections.Immutable.IBinaryTree<TValue> source, int depth, bool isMax, int maxHeight, System.Func<TValue, int> valueSelector)
+    {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+      else if (source.IsEmpty) throw new System.ArgumentNullException(source.GetType().Name);
+
+      if (depth == maxHeight) // Terminating condition.
+        return valueSelector?.Invoke(source.Value) ?? throw new System.ArgumentNullException(nameof(valueSelector));
+
+      if (isMax)
+      {
+        int? left = default;
+        if (!source.Left.IsEmpty) left = Minimax(source.Left, depth + 1, false, maxHeight, valueSelector);
+        int? right = default;
+        if (!source.Right.IsEmpty) right = Minimax(source.Right, depth + 1, false, maxHeight, valueSelector);
+
+        return System.Math.Max(left.HasValue ? left.Value : right!.Value, right.HasValue ? right.Value : left!.Value);
+      }
+      else
+      {
+        int? left = default;
+        if (!source.Left.IsEmpty) left = Minimax(source.Right, depth + 1, true, maxHeight, valueSelector);
+        int? right = default;
+        if (!source.Right.IsEmpty) right = Minimax(source.Left, depth + 1, true, maxHeight, valueSelector);
+
+        return System.Math.Min(left.HasValue ? left.Value : right!.Value, right.HasValue ? right.Value : left!.Value);
+      }
+    }
   }
 
   namespace Collections.Immutable
