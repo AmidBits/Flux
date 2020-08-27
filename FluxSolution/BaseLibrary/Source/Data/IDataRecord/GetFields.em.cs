63d37124933fc843b2ab5d34890c238cd1d148e1
Robert Hugo <Robert.Hugo@pima.gov>
2020-08-27 15:58:58 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Data/IDataRecord/GetFields.em.cs
+++ FluxSolution/BaseLibrary/Source/Data/IDataRecord/GetFields.em.cs
@@ -4,6 +4,9 @@ namespace Flux
   {
     public static System.Collections.Generic.IEnumerable<TResult> GetFields<TResult>(this System.Data.IDataRecord source, System.Func<System.Data.IDataRecord, int, TResult> resultSelector)
     {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+      if (resultSelector is null) throw new System.ArgumentNullException(nameof(resultSelector));
+
       for (var index = 0; index < source.FieldCount; index++)
       {
         yield return resultSelector(source, index);
--- FluxSolution/BaseLibrary/Source/Data/IDataRecord/GetName.em.cs
+++ FluxSolution/BaseLibrary/Source/Data/IDataRecord/GetName.em.cs
@@ -4,7 +4,7 @@ namespace Flux
   {
     /// <summary>Returns a string with the column name (header) for the specified index in the current row. This version will replace null or blank names with "ColumnN" where N is the column index + 1.</summary>
     public static string GetNameEx(this System.Data.IDataRecord source, int index)
-      => source.GetName(index) is var name && string.IsNullOrWhiteSpace(name) ? $"Column{index + 1}" : name;
+      => (source ?? throw new System.ArgumentNullException(nameof(source))).GetName(index) is var name && string.IsNullOrWhiteSpace(name) ? $"Column{index + 1}" : name;
 
     /// <summary>Results in a string array of all column names.</summary>
     public static System.Collections.Generic.IEnumerable<string> GetNames(this System.Data.IDataRecord source)
