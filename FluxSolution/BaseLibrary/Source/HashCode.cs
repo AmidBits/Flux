11d0db4eeb3262720b098e238f057171b59e7b3c
Robert Hugo <robert_hugo@hotmail.com>
2020-09-10 22:47:40 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/HashCode.cs
+++ FluxSolution/BaseLibrary/Source/HashCode.cs
@@ -5,10 +5,12 @@ namespace Flux
     /// <summary>Combines hash codes FOR multiple objects. Useful for combining hash codes of composite types.</summary>
     public static int Combine(System.Collections.Generic.IEnumerable<object> source)
     {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+
       unchecked
       {
         var hashCode = 0;
-        foreach (var o in source ?? throw new System.ArgumentNullException(nameof(source)))
+        foreach (var o in source )
           hashCode = ((hashCode << 5) + hashCode) ^ o.GetHashCode();
         return hashCode;
       }
@@ -19,10 +21,12 @@ namespace Flux
 
     public static int CombineBoost(System.Collections.Generic.IEnumerable<object> source)
     {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+
       unchecked
       {
         var hashCode = 0L;
-        foreach (var o in source ?? throw new System.ArgumentNullException(nameof(source)))
+        foreach (var o in source )
           hashCode ^= o.GetHashCode() + 0x9e3779b9 + (hashCode << 6) + (hashCode >> 2);
         return (int)hashCode;
       }
@@ -33,8 +37,10 @@ namespace Flux
 
     public static int CombineCore(System.Collections.Generic.IEnumerable<object> source)
     {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+
       var hash = new System.HashCode();
-      foreach (var o in source ?? throw new System.ArgumentNullException(nameof(source)))
+      foreach (var o in source)
         hash.Add(o);
       return hash.ToHashCode();
     }
