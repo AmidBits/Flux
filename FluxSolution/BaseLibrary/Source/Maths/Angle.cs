b541ea3bfc17a0771cc3ef53d8dc87ca494e9db5
Robert Hugo <Robert.Hugo@pima.gov>
2020-09-18 13:31:06 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Maths/Angle.cs
+++ FluxSolution/BaseLibrary/Source/Maths/Angle.cs
@@ -7,7 +7,7 @@ namespace Flux
   {
     /// <summary>Convert the cartesian 2D coordinate (x, y) where 'right-center' is 'zero' (i.e. positive-x and neutral-y) to a counter-clockwise rotation angle [0, PI*2] (radians). Looking at the face of a clock, this goes counter-clockwise from and to 3 o'clock.</summary>
     public static double CartesianToRotationAngle(double x, double y)
-      => System.Math.Atan2(y, x) is var atan2 && atan2 < 0d ? Maths.PiX2 + atan2 : atan2;
+      => System.Math.Atan2(y, x) is var atan2 && atan2 < 0 ? Maths.PiX2 + atan2 : atan2;
     /// <summary>Convert the cartesian 2D coordinate (x, y) where 'right-center' is 'zero' (i.e. positive-x and neutral-y) to a clockwise rotation angle [0, PI*2] (radians). Looking at the face of a clock, this goes counter-clockwise from and to 3 o'clock.</summary>
     public static double CartesianToRotationAngle(this System.Numerics.Vector2 vector)
       => CartesianToRotationAngle(vector.X, vector.Y);
