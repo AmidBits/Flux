941a9b472e7dc494ec3242ac56199403010fa7f1
Robert Hugo <Robert.Hugo@pima.gov>
2020-09-18 16:01:41 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Types/TimeSpan.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/TimeSpan.em.cs
@@ -5,6 +5,22 @@ namespace Flux
     public static string ToStringOptimized(this System.TimeSpan source)
       => System.Text.RegularExpressions.Regex.Replace(source.ToString(), @"^(00.)(00\:)+0+(?=.)", string.Empty);
 
+    public static string ToStringDays(this System.TimeSpan source)
+      => source.TotalDays > 0 ? $"{source.TotalDays} day{(source.TotalDays > 1 ? "s" : string.Empty)}" : string.Empty;
+    public static string ToStringOfHours(this System.TimeSpan source)
+      => source.TotalHours > 0 ? $"{source.TotalHours} hour{(source.TotalHours > 1 ? "s" : string.Empty)}" : string.Empty;
+    public static string ToStringOfMinutes(this System.TimeSpan source)
+      => source.TotalMinutes > 0 ? $"{source.TotalMinutes} min{(source.TotalMinutes > 1 ? "s" : string.Empty)}" : string.Empty;
+    public static string ToStringOfSeconds(this System.TimeSpan source)
+      => source.TotalSeconds > 0 ? $"{source.TotalSeconds} sec{(source.TotalSeconds > 1 ? "s" : string.Empty)}" : string.Empty;
+    public static string ToStringDaysHoursMinsSecs(this System.TimeSpan source)
+      => string.Join(' ', new string[] {
+        source.Days > 0 ? $"{source.Days} day{(source.Days > 1 ? "s" : string.Empty)}" : string.Empty,
+        source.Hours > 0 ? $"{source.Hours} hour{(source.Hours > 1 ? "s" : string.Empty)}" : string.Empty,
+        source.Minutes > 0 ? $"{source.Minutes} min{(source.Minutes > 1 ? "s" : string.Empty)}" : string.Empty,
+        source.Seconds > 0 ? $"{source.Seconds} sec{(source.Seconds > 1 ? "s" : string.Empty)}" : string.Empty
+      });
+
     //public static string ToStringDays(this System.TimeSpan source, string suffix)
     //  => source.Days > 0 ? $"{source.Days}{suffix}" : string.Empty;
 
