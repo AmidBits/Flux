e67bd30b3f84efa86fef95e7fc6d235c04f047a7
Robert Hugo <Robert.Hugo@pima.gov>
2020-10-05 15:57:28 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Maths/Periodic.cs
+++ FluxSolution/BaseLibrary/Source/Maths/Periodic.cs
@@ -4,11 +4,11 @@ namespace Flux
   {
     /// <summary>Generates a pulse wave using radians [0, PI2].</summary>
     public static double PulseWave(double phasePi2, double pulseWidthPi2 = System.Math.PI)
-      => phasePi2 < pulseWidthPi2 ? 1d : -1d;
+      => (phasePi2 % PiX2) < System.Math.Clamp(pulseWidthPi2, 0, PiX2) ? 1 : -1;
 
     /// <summary>Generates a sawtooth wave using radians [0, PI2].</summary>
     public static double SawtoothWave(double phasePi2)
-      => 1d - phasePi2 / System.Math.PI;
+      => 1 - (phasePi2 % PiX2) / System.Math.PI;
 
     /// <summary>Generates a sine wave using radians [0, PI2].</summary>
     public static double SineWave(double phasePi2)
@@ -16,13 +16,13 @@ namespace Flux
 
     /// <summary>Generates a square wave using radians [0, PI2].</summary>
     public static double SquareWave(double phasePi2)
-      => phasePi2 < System.Math.PI ? 1d : -1d;
+      => (phasePi2 % PiX2) < System.Math.PI ? 1 : -1;
 
     /// <summary>Generates a triangle wave using radians [0, PI2].</summary>
     public static double TriangleWave(double phasePi2)
-      => phasePi2 < System.Math.PI ? phasePi2 / PiOver2 - 1d : 3d - phasePi2 / PiOver2;
+      => (phasePi2 % PiX2) is var phase && phase < System.Math.PI ? phase / PiOver2 - 1 : 3 - phase / PiOver2;
 
     public static double WhiteNoise()
-      => 1d - 2d * Flux.Random.NumberGenerator.Crypto.NextDouble();
+      => 1 - 2 * Flux.Random.NumberGenerator.Crypto.NextDouble();
   }
 }
