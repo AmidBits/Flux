9255d6d1fefea6c5fc94298c40e48c870b7b72dd
Robert Hugo <robert_hugo@hotmail.com>
2020-10-26 23:03:52 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/NativeMethods/User32.cs
+++ FluxSolution/BaseLibrary/Source/NativeMethods/User32.cs
@@ -1,31 +1,8 @@
 ﻿namespace Flux.User32
 {
-  [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
-  public struct Point
-  {
-    public int X;
-    public int Y;
-
-    public static implicit operator Point(Point point)
-    {
-      return new Point(point.X, point.Y);
-    }
-  }
-
   internal class NativeMethods
   {
     [System.Runtime.InteropServices.DllImport("user32.dll")]
-    public static extern bool GetCursorPos(out Point lpPoint);
-
-    public static Point GetCursorPosition()
-    {
-      Point lpPoint;
-      GetCursorPos(out lpPoint);
-      // NOTE: If you need error handling
-      // bool success = GetCursorPos(out lpPoint);
-      // if (!success)
-
-      return lpPoint;
-    }
+    public static extern bool GetCursorPos(out Geometry.Point2 lpPoint);
   }
 }
