07d54c844fe613c24a8fda111e42e9f7b1270a7a
Robert Hugo <robert_hugo@hotmail.com>
2020-11-09 00:09:59 -0700

    Breaking changes to the wrapping extensions.

--- FluxSolution/BaseLibrary/Source/Types/Memory/ReadOnlySpan/Wrapping.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/Memory/ReadOnlySpan/Wrapping.em.cs
@@ -3,15 +3,15 @@ namespace Flux
   public static partial class Xtensions
   {
     /// <summary>Indicates whether the source is already wrapped in the specified characters. E.g. in SQL brackets, or parenthesis.</summary>
-    public static bool IsWrapped<T>(this System.Span<T> source, T left, T right)
+    public static bool IsWrapped<T>(this System.ReadOnlySpan<T> source, T left, T right)
       where T : System.IEquatable<T>
       => source.Length >= 2 && left.Equals(source[0]) && right.Equals(source[source.Length - 1]);
     /// <summary>Remove the specified wrapping characters from the source, if they exist. E.g. SQL brackets, or parenthesis.</summary>
-    public static System.Span<T> Unwrap<T>(this System.Span<T> source, T left, T right)
+    public static System.ReadOnlySpan<T> Unwrap<T>(this System.ReadOnlySpan<T> source, T left, T right)
       where T : System.IEquatable<T>
       => source.Slice(1, source.Length - 2);
     /// <summary>Add the specified characters to the source, if they do not already exist. Change the default force to true to always wrap the source, even if it is null (which produces a wrapped empty string) or already wrapped. E.g. in SQL brackets, or parenthesis.</summary>
-    public static T[] Wrap<T>(this System.Span<T> source, T left, T right)
+    public static T[] Wrap<T>(this System.ReadOnlySpan<T> source, T left, T right)
       where T : System.IEquatable<T>
     {
       var array = source.ToArray(1, 1);
@@ -21,15 +21,15 @@ namespace Flux
     }
 
     /// <summary>Indicates whether the source is already wrapped in the strings.</summary>
-    public static bool IsWrapped<T>(this System.Span<T> source, System.Span<T> left, System.Span<T> right)
+    public static bool IsWrapped<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> left, System.ReadOnlySpan<T> right)
       where T : System.IEquatable<T>
       => source.Length >= 2 && source.StartsWith(left) && source.EndsWith(right);
     /// <summary>Remove the specified wrap strings from the source, if they exist.</summary>
-    public static System.Span<T> Unwrap<T>(this System.Span<T> source, System.Span<T> left, System.Span<T> right)
+    public static System.ReadOnlySpan<T> Unwrap<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> left, System.ReadOnlySpan<T> right)
       where T : System.IEquatable<T>
       => source.Slice(left.Length, source.Length - (left.Length + right.Length));
     /// <summary>Add the specified wrap strings to the source, if they do not already exist. Change the default force to true to always wrap the source, even if it is null (which produces a wrapped empty string) or already wrapped.</summary>
-    public static T[] Wrap<T>(this System.Span<T> source, System.Span<T> left, System.Span<T> right)
+    public static System.Span<T> Wrap<T>(this System.ReadOnlySpan<T> source, System.ReadOnlySpan<T> left, System.ReadOnlySpan<T> right)
       where T : System.IEquatable<T>
     {
       var array = source.ToArray(left.Length, right.Length);
