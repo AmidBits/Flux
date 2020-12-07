3c606b6cb826edd3763a412531ea8e43230d7ede
Robert Hugo <Robert.Hugo@pima.gov>
2020-12-07 15:16:53 -0700

    Various changes and alignments.

--- FluxSolution/BaseLibrary/Source/Text/StringBuilder/With.em.cs
+++ FluxSolution/BaseLibrary/Source/Text/StringBuilder/With.em.cs
@@ -1,51 +1,51 @@
 namespace Flux
 {
-  public static partial class StringBuilderEm
-  {
-    /// <summary>Indicates whether the source ends with value. Uses the specified comparer.</summary>
-    public static bool EndsWith(this System.Text.StringBuilder source, string target, System.Collections.Generic.IEqualityComparer<char> comparer)
-    {
-      if (source is null) throw new System.ArgumentNullException(nameof(source));
-      if (target is null) throw new System.ArgumentNullException(nameof(target));
-      if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
+	public static partial class StringBuilderEm
+	{
+		/// <summary>Indicates whether the source ends with value. Uses the specified comparer.</summary>
+		public static bool EndsWith(this System.Text.StringBuilder source, string target, System.Collections.Generic.IEqualityComparer<char> comparer)
+		{
+			if (source is null) throw new System.ArgumentNullException(nameof(source));
+			if (target is null) throw new System.ArgumentNullException(nameof(target));
+			if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
 
-      var sourceIndex = source.Length;
-      var targetIndex = target.Length;
+			var sourceIndex = source.Length;
+			var targetIndex = target.Length;
 
-      if (sourceIndex < targetIndex)
-        return false;
+			if (sourceIndex < targetIndex)
+				return false;
 
-      while (--sourceIndex >= 0 && --targetIndex >= 0)
-        if (!comparer.Equals(source[sourceIndex], target[targetIndex]))
-          return false;
+			while (--sourceIndex >= 0 && --targetIndex >= 0)
+				if (!comparer.Equals(source[sourceIndex], target[targetIndex]))
+					return false;
 
-      return true;
-    }
-    /// <summary>Indicates whether the source ends with value. Uses the default comparer.</summary>
-    public static bool EndsWith(this System.Text.StringBuilder source, string target)
-      => EndsWith(source, target, System.Collections.Generic.EqualityComparer<char>.Default);
+			return true;
+		}
+		/// <summary>Indicates whether the source ends with value. Uses the default comparer.</summary>
+		public static bool EndsWith(this System.Text.StringBuilder source, string target)
+			=> EndsWith(source, target, System.Collections.Generic.EqualityComparer<char>.Default);
 
-    /// <summary>Indicates whether the source starts with value. Uses the specified comparer.</summary>
-    public static bool StartsWith(this System.Text.StringBuilder source, string target, System.Collections.Generic.IEqualityComparer<char> comparer)
-    {
-      if (source is null) throw new System.ArgumentNullException(nameof(source));
-      if (target is null) throw new System.ArgumentNullException(nameof(target));
-      if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
+		/// <summary>Indicates whether the source starts with value. Uses the specified comparer.</summary>
+		public static bool StartsWith(this System.Text.StringBuilder source, string target, System.Collections.Generic.IEqualityComparer<char> comparer)
+		{
+			if (source is null) throw new System.ArgumentNullException(nameof(source));
+			if (target is null) throw new System.ArgumentNullException(nameof(target));
+			if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
 
-      var sourceLength = source.Length;
-      var targetLength = target.Length;
+			var sourceLength = source.Length;
+			var targetLength = target.Length;
 
-      if (sourceLength < targetLength)
-        return false;
+			if (sourceLength < targetLength)
+				return false;
 
-      for (var index = 0; index < targetLength; index++)
-        if (!comparer.Equals(source[index], target[index]))
-          return false;
+			for (var index = 0; index < targetLength; index++)
+				if (!comparer.Equals(source[index], target[index]))
+					return false;
 
-      return true;
-    }
-    /// <summary>Indicates whether the source starts with value. Uses the default comparer.</summary>
-    public static bool StartsWith(this System.Text.StringBuilder source, string target)
-       => StartsWith(source, target, System.Collections.Generic.EqualityComparer<char>.Default);
-  }
+			return true;
+		}
+		/// <summary>Indicates whether the source starts with value. Uses the default comparer.</summary>
+		public static bool StartsWith(this System.Text.StringBuilder source, string target)
+			 => StartsWith(source, target, System.Collections.Generic.EqualityComparer<char>.Default);
+	}
 }
