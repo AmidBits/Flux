f251bc6bc49b5b2b656bf992dbc86e29ee0b179b
Robert Hugo <Robert.Hugo@pima.gov>
2020-12-10 14:46:40 -0700

    Consolidated some static resources and added some extension methods.

--- FluxSolution/BaseLibrary/Source/Convert/IntegerToScript.cs
+++ FluxSolution/BaseLibrary/Source/Convert/IntegerToScript.cs
@@ -4,22 +4,18 @@ namespace Flux
 {
   public static partial class Convert
   {
-    public const string SubscriptsDigits = "\u2080\u0081\u0082\u0083\u2084\u2085\u2086\u2087\u2088\u2089"; // "0123456789"
-
     /// <summary>Returns a char with the numeric subscript.</summary>
     public static char SingleDigitToSubscript(int number)
-      => number >= 0 && number <= 9 ? SubscriptsDigits[number] : throw new System.ArgumentOutOfRangeException(nameof(number));
+      => number >= 0 && number <= 9 ? Text.Sequences.Subscript0Through9[number][0] : throw new System.ArgumentOutOfRangeException(nameof(number));
     /// <summary>Returns a char with the numeric subscript.</summary>
     public static string SingleDigitToSubscript(string text)
-      => string.Concat(text.Select(c => char.IsDigit(c) ? SubscriptsDigits[c - '0'] : c));
-
-    public const string SuperscriptsDigits = "\u2070\u00B9\u00B2\u00B3\u2074\u2075\u2076\u2077\u2078\u2079"; // "0123456789"
+      => string.Concat(text.Select(c => char.IsDigit(c) ? Text.Sequences.Subscript0Through9[c - '0'][0] : c));
 
     /// <summary>Returns a char with the numeric superscript.</summary>
     public static char SingleDigitToSuperscript(int number)
-      => number >= 0 && number <= 9 ? SuperscriptsDigits[number] : throw new System.ArgumentOutOfRangeException(nameof(number));
+      => number >= 0 && number <= 9 ? Text.Sequences.Superscript0Through9[number][0] : throw new System.ArgumentOutOfRangeException(nameof(number));
     /// <summary>Returns a char with the numeric superscript.</summary>
     public static string SingleDigitsToSuperscript(string text)
-      => string.Concat(text.Select(c => char.IsDigit(c) ? SuperscriptsDigits[c - '0'] : c));
+      => string.Concat(text.Select(c => char.IsDigit(c) ? Text.Sequences.Superscript0Through9[c - '0'][0] : c));
   }
 }
--- FluxSolution/BaseLibrary/Source/Convert/ToString/ToGroupString.cs
+++ FluxSolution/BaseLibrary/Source/Convert/ToString/ToGroupString.cs
@@ -4,13 +4,13 @@ namespace Flux
   {
     /// <summary></summary>
     public static string ToGroupString(this System.Numerics.BigInteger source)
-      => source.ToString("#,###0", System.Globalization.CultureInfo.CurrentCulture);
+      => source.ToString("#,###0", System.Globalization.CultureInfo.InvariantCulture);
 
     /// <summary></summary>
     public static string ToGroupString(this int source)
-      => source.ToString("#,###0", System.Globalization.CultureInfo.CurrentCulture);
+      => source.ToString("#,###0", System.Globalization.CultureInfo.InvariantCulture);
     /// <summary></summary>
     public static string ToGroupString(this long source)
-      => source.ToString("#,###0", System.Globalization.CultureInfo.CurrentCulture);
+      => source.ToString("#,###0", System.Globalization.CultureInfo.InvariantCulture);
   }
 }
