0a84eb07c32e3defa4681b3abd39ffee11edfa51
Robert Hugo <robert_hugo@hotmail.com>
2020-12-15 23:50:50 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Model/RulesEngine/Rule.cs
+++ FluxSolution/BaseLibrary/Source/Model/RulesEngine/Rule.cs
@@ -1,99 +1,107 @@
+using System;
+
 namespace Flux.Model
 {
-	public struct Rule
-		: System.IEquatable<Rule>
-	{
-		public string Name { get; }
-		public string Operator { get; }
-		public string Value { get; }
+  public struct Rule
+    : System.IEquatable<Rule>
+  {
+    public string Name { get; }
+    public string Operator { get; }
+    public string Value { get; }
 
-		public Rule(string Name, string Operator, string Value)
-		{
-			this.Name = Name;
-			this.Operator = Operator;
-			this.Value = Value;
-		}
+    public Rule(string Name, string Operator, string Value)
+    {
+      this.Name = Name;
+      this.Operator = Operator;
+      this.Value = Value;
+    }
 
-		// Statics
-		//public static bool IsBinaryBoolean(string value)
-		//{
-		//	try
-		//	{
-		//		if (System.Linq.Expressions.ExpressionType.TryParse(value, out System.Linq.Expressions.ExpressionType expressionType))
-		//		{
-		//			var binaryExpression = System.Linq.Expressions.Expression.MakeBinary(expressionType, System.Linq.Expressions.Expression.Constant(0), System.Linq.Expressions.Expression.Constant(0));
+    // Operators
+    public static bool operator ==(Rule a, Rule b)
+      => a.Equals(b);
+    public static bool operator !=(Rule a, Rule b)
+      => !a.Equals(b);
+    // System.IEquatable<Rule>
+    public bool Equals(Rule other)
+      => Name == other.Name && Operator == other.Operator && Value == other.Value;
+    // Overrides
+    public override bool Equals(object? obj)
+      => obj is Rule o && Equals(o);
+    public override int GetHashCode()
+      => System.HashCode.Combine(Name, Operator, Value);
+    public override string? ToString()
+      => $"<{nameof(Rule)}: \"{Name}\" {Operator} '{Value}'>";
+  }
 
-		//			var lambda = System.Linq.Expressions.Expression.Lambda(binaryExpression, null);
+  public class RulesDictionary<T>
+    : System.Collections.Generic.IDictionary<Flux.Model.Rule, System.Func<T, bool>>
+  {
+    public System.Collections.Generic.IDictionary<Flux.Model.Rule, System.Func<T, bool>> m_rules = new System.Collections.Generic.Dictionary<Flux.Model.Rule, System.Func<T, bool>>();
 
-		//			return lambda.ReturnType == typeof(bool);
-		//		}
-		//	}
-		//	catch { }
+    //public System.Collections.Generic.IList<Flux.Model.Rule> m_rules = new System.Collections.Generic.List<Flux.Model.Rule>();
 
-		//	return false;
-		//}
 
-		// Operators
-		public static bool operator ==(Rule a, Rule b)
-			=> a.Equals(b);
-		public static bool operator !=(Rule a, Rule b)
-			=> !a.Equals(b);
+    //public void Add(Rule rule)
+    //{
+    //	m_rules.Add(rule, Flux.Model.RulesEngine.CompileRule<T>(rule));
+    //}
 
-		// IEquatable
-		public bool Equals(Rule other)
-			=> Name == other.Name && Operator == other.Operator && Value == other.Value;
+    //public bool Evaluate(T value)
+    //{
+    //	var exceptions = new System.Collections.Generic.List<System.Exception>();
 
-		// Object (overrides)
-		public override bool Equals(object? obj)
-			=> obj is Rule o && Equals(o);
-		public override int GetHashCode()
-			=> System.HashCode.Combine(Name, Operator, Value);
-		public override string? ToString()
-			=> $"<{nameof(Rule)}: \"{Name}\" {Operator} '{Value}'>";
-	}
+    //	foreach (var rule in m_rules)
+    //		if (!rule.Value(value))
+    //			exceptions.Add(new System.ArgumentOutOfRangeException(nameof(value), $"{rule.Key}."));
 
-	public class RuleCollection
-	{
-		public System.Collections.Generic.IList<Flux.Model.Rule> m_rules = new System.Collections.Generic.List<Flux.Model.Rule>();
+    //	try
+    //	{
+    //		if (exceptions.Count > 0)
+    //			throw new System.AggregateException($"{value} passed {m_rules.Count - exceptions.Count} and failed {exceptions.Count} rules.", exceptions);
+    //	}
+    //	catch (System.Exception se)
+    //	{ }
 
-		public void Add(Rule rule)
-		{
-			m_rules.Add(rule);
-		}
+    //	return exceptions.Count == 0;
+    //}
 
-		public void Evaluate<T>(T value)
-		{
-			foreach (var rule in m_rules)
-				if (!Flux.Model.RulesEngine.CompileRule<T>(rule)(value))
-					throw new System.ArgumentOutOfRangeException(nameof(value));
-		}
-	}
-	public class RuleCollection<T>
-	{
-		public System.Collections.Generic.IDictionary<Flux.Model.Rule, System.Func<T, bool>> m_rules = new System.Collections.Generic.Dictionary<Flux.Model.Rule, System.Func<T, bool>>();
+    #region IDictionary implementation
+    public Func<T, bool> this[Rule key] { get => m_rules[key]; set => throw new System.NotImplementedException(); }
 
-		public void Add(Rule rule)
-		{
-			m_rules.Add(rule, Flux.Model.RulesEngine.CompileRule<T>(rule));
-		}
+    public System.Collections.Generic.ICollection<Rule> Keys
+      => m_rules.Keys;
+    public System.Collections.Generic.ICollection<Func<T, bool>> Values
+      => m_rules.Values;
+    public int Count
+      => m_rules.Count;
+    public bool IsReadOnly
+      => false;
 
-		public bool Evaluate(T value)
-		{
-			var exceptions = new System.Collections.Generic.List<System.Exception>();
+    public void Add(Rule key, Func<T, bool> value)
+      => m_rules.Add(key, value);
+    public void Add(System.Collections.Generic.KeyValuePair<Rule, Func<T, bool>> item)
+      => m_rules.Add(item);
+    public void Clear()
+      => m_rules.Clear();
+    public bool Contains(System.Collections.Generic.KeyValuePair<Rule, Func<T, bool>> item)
+      => m_rules.Contains(item);
+    public bool ContainsKey(Rule key)
+      => m_rules.ContainsKey(key);
+    public void CopyTo(System.Collections.Generic.KeyValuePair<Rule, Func<T, bool>>[] array, int arrayIndex)
+      => m_rules.CopyTo(array, arrayIndex);
+    public System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<Rule, Func<T, bool>>> GetEnumerator()
+      => m_rules.GetEnumerator();
+    public bool Remove(Rule key)
+      => m_rules.Remove(key);
+    public bool Remove(System.Collections.Generic.KeyValuePair<Rule, Func<T, bool>> item)
+      => m_rules.Remove(item);
+    public bool TryGetValue(Rule key, [System.Diagnostics.CodeAnalysis.MaybeNullWhen(false)] out Func<T, bool> value)
+      => m_rules.TryGetValue(key, out value);
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+      => m_rules.GetEnumerator();
+    #endregion IDictionary implementation
 
-			foreach (var rule in m_rules)
-				if (!rule.Value(value))
-					exceptions.Add(new System.ArgumentOutOfRangeException(nameof(value), $"{rule.Key}."));
-
-			try
-			{
-				if (exceptions.Count > 0)
-					throw new System.AggregateException($"{value} passed {m_rules.Count - exceptions.Count} and failed {exceptions.Count} rules.", exceptions);
-			}
-			catch (System.Exception se)
-			{ }
-
-			return exceptions.Count == 0;
-		}
-	}
+    //public bool All()
+    //public bool Any()
+  }
 }
