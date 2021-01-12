52ac2b4e22ea56594d0000dfb7cfaee7137793bb
Robert Hugo <Robert.Hugo@pima.gov>
2021-01-12 16:04:09 -0700

    Changes.

--- FluxSolution/BaseLibrary/Source/Types/DateTime/JulianCalendar.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/DateTime/JulianCalendar.em.cs
@@ -3,14 +3,14 @@ namespace Flux
   /// <seealso cref="https://en.wikipedia.org/wiki/Julian_day"/>
   public static partial class SystemDateTimeEm
   {
-    public static System.DateTime FromJulianDate(this double julianDay)
-      => System.DateTime.FromOADate(julianDay - 2415018.5);
-    public static System.DateTime FromModifiedJulianDate(this double modifiedJulianDay)
-      => FromJulianDate(modifiedJulianDay + 2400000.5);
-    public static System.DateTime FromReducedJulianDate(this double reducedJulianDay)
-      => FromJulianDate(reducedJulianDay + 2400000);
-    public static System.DateTime FromTruncatedJulianDate(this double truncatedJulianDay)
-      => FromJulianDate(truncatedJulianDay + 2440000.5);
+    public static System.DateTime FromJulianDate(this double julianDate)
+      => System.DateTime.FromOADate(julianDate - 2415018.5);
+    public static System.DateTime FromModifiedJulianDate(this double modifiedJulianDate)
+      => FromJulianDate(modifiedJulianDate + 2400000.5);
+    public static System.DateTime FromReducedJulianDate(this double reducedJulianDate)
+      => FromJulianDate(reducedJulianDate + 2400000);
+    public static System.DateTime FromTruncatedJulianDate(this double truncatedJulianDate)
+      => FromJulianDate(truncatedJulianDate + 2440000.5);
 
     public static double ToJulianDate(this System.DateTime date)
       => date.ToOADate() + 2415018.5;
@@ -20,5 +20,101 @@ namespace Flux
       => date.ToJulianDate() - 2400000;
     public static double ToTruncatedJulianDate(this System.DateTime date)
       => System.Math.Floor(date.ToJulianDate() - 2440000.5);
+  }
+  //https://ia802807.us.archive.org/20/items/astronomicalalgorithmsjeanmeeus1991/Astronomical%20Algorithms-%20Jean%20Meeus%20%281991%29.pdf
+  public static class JulianDate
+  {
+    private static double DateToJD(int year, int month, int day, int hour, int minute, int second, int millisecond)
+    {
+      bool isJD = IsJulianDate(year, month, day);
+
+      int M = month > 2 ? month : month + 12;
+      int Y = month > 2 ? year : year - 1;
+      double D = day + hour / 24.0 + minute / 1440.0 + (second + millisecond / 1000.0) / 86400.0;
+      int B = isJD ? 0 : 2 - Y / 100 + Y / 100 / 4;
+
+      return (int)(365.25 * (Y + 4716)) + (int)(30.6001 * (M + 1)) + D + B - 1524.5;
+    }
+
+    public static double From(int year, int month, int day, int hour, int minute, int second, int millisecond)
+      => DateToJD(year, month, day, hour, minute, second, millisecond);
+
+    public static double From(System.DateTime dateTime)
+      => DateToJD(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second, dateTime.Millisecond);
+
+    public static bool IsJulianDate(int year, int month, int day)
+    {
+      if (year < 1582) // Dates prior to 1582 are Julian dates.
+        return true;
+      else if (year > 1582) // Dates after 1582 are Gregorian dates.
+        return false;
+      else // If 1582, check before October 4 (Julian) or after October 15 (Gregorian)
+      {
+        if (month < 10) // Dates before October, 1582 are Julian dates.
+          return true;
+        else if (month > 10) // Dates after October, 1582 are Gregorian dates.
+          return false;
+        else
+        {
+          if (day < 5)
+            return true;
+          else if (day > 14)
+            return false;
+          else
+            throw new System.InvalidOperationException($"This date is invalid.");
+        }
+      }
+    }
+  }
+
+  /// <summary></summary>
+  /// <see cref="https://en.wikipedia.org/wiki/Julian_day"/>
+  public static class JulianDayNumber
+  {
+    public static System.DayOfWeek ToDayOfWeek(int julianDayNumber)
+      => (System.DayOfWeek)(((julianDayNumber % 7) + 1) % 7);
+    /// <summary>Returns the JDN (integer value ) for the Gregorian date.</summary>
+    /// <param name="julianDayNumber">The Julian day number (JDN) is the integer assigned to a whole solar day in the Julian day count starting from noon Universal time, with Julian day number 0 assigned to the day starting at noon on Monday, January 1, 4713 BC, proleptic Julian calendar (November 24, 4714 BC, in the proleptic Gregorian calendar), a date at which three multi-year cycles started (which are: Indiction, Solar, and Lunar cycles) and which preceded any dates in recorded history.[a] For example, the Julian day number for the day starting at 12:00 UT (noon) on January 1, 2000, was 2,451,545.</param>
+    /// <see cref="https://en.wikipedia.org/wiki/Proleptic_Gregorian_calendar"/>
+    public static (int year, int month, int day) ToGregorianDate(int julianDayNumber)
+    {
+      var J = julianDayNumber;
+
+      var y = 4716;
+      var v = 3;
+      var j = 1401;
+      var u = 5;
+      var m = 2;
+      var s = 153;
+      var n = 12;
+      var w = 2;
+      var r = 4;
+      var B = 274277;
+      var p = 1461;
+      var C = -38;
+
+      var f = J + j + (((4 * J + B) / 146097) * 3) / 4 + C;
+      var e = r * f + v;
+      var g = (e % p) / r;
+      var h = u * g + w;
+
+      var D = (h % s) / u + 1;
+      var M = ((h / s + m) % n) + 1;
+      var Y = (e / p) - y + (n + m - M) / n;
+
+      return (Y, M, D);
+    }
+
+    /// <summary>Returns the Gregorian date for the JDN.</summary>
+    /// <see cref="https://en.wikipedia.org/wiki/Proleptic_Julian_calendar"/>
+    public static int FromGregorianDate(int year, int month, int day)
+    //=> (1461 * (year + 4800 + (month - 14) / 12)) / 4 + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12 - (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4 + day - 32075;
+    {
+      var a = (14 - month) / 12;
+      var y = year + 4800 - a;
+      var m = month + 12 * a - 3;
+
+      return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    }
   }
 }
--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -8,119 +8,124 @@ using Flux;
 
 namespace ConsoleApp
 {
-	class Program
-	{
-		private static void TimedMain(string[] _)
-		{
-			var dr = Flux.Resources.Presets.GetScowlTwoOfTwelveFull(Flux.Resources.Presets.UriScowlTwoOfTwelveFullLocal);
+  class Program
+  {
+    private static void TimedMain(string[] _)
+    {
+      var date = new System.DateTime(1, 3, 2);
+      //date = System.DateTime.Now;
+      System.Console.WriteLine($"{System.DateTime.MinValue} - {System.DateTime.MaxValue}");
+      System.Console.WriteLine($"{date} = {date.ToJulianDate()} = {Flux.JulianDayNumber.FromGregorianDate(date.Year, date.Month, date.Day)}");
+      System.Console.WriteLine($"{2459227.32708} = {Flux.JulianDayNumber.ToGregorianDate(2459227)}");
+      //var dr = Flux.Resources.Presets.GetScowlTwoOfTwelveFull(Flux.Resources.Presets.UriScowlTwoOfTwelveFullLocal);
 
-			var a = dr.ToArray();
+      //var a = dr.ToArray();
 
-			var cs = a.ToConsoleStringJagged();
+      //var cs = a.ToConsoleStringJagged();
 
-			System.Console.WriteLine(cs);
+      //System.Console.WriteLine(cs);
 
-			//foreach (var r in dr)
-			//{
-			//	System.Console.WriteLine(string.Join(',', r.GetNames()));
-			//	System.Console.WriteLine(string.Join(',', r.GetValues()));
-			//}
+      //foreach (var r in dr)
+      //{
+      //	System.Console.WriteLine(string.Join(',', r.GetNames()));
+      //	System.Console.WriteLine(string.Join(',', r.GetValues()));
+      //}
 
-			//System.Console.WriteLine(dt.ToConsoleString());
+      //System.Console.WriteLine(dt.ToConsoleString());
 
-			//var showIPStatistics = true;
+      //var showIPStatistics = true;
 
-			//foreach (var nic in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
-			//{
-			//	System.Console.WriteLine($"Network Interface");
-			//	System.Console.WriteLine($"Description: {nic.Description}");
-			//	System.Console.WriteLine($"ID: {new System.Guid(nic.Id).ToString()}");
-			//	System.Console.WriteLine($"Name: {nic.Name}");
-			//	System.Console.WriteLine($"Operational status: {nic.OperationalStatus}");
-			//	System.Console.WriteLine($"Receive only: {nic.IsReceiveOnly}");
-			//	System.Console.WriteLine($"Speed: {nic.Speed.ToGroupString()}");
-			//	System.Console.WriteLine($"Supports multicast: {nic.SupportsMulticast}");
-			//	System.Console.WriteLine($"Type: {nic.NetworkInterfaceType}");
-			//	if (nic.GetPhysicalAddress().ToStringMAC() is var mac && !string.IsNullOrWhiteSpace(mac))
-			//		System.Console.WriteLine($"Physical address: {mac}");
-			//	System.Console.WriteLine();
+      //foreach (var nic in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+      //{
+      //	System.Console.WriteLine($"Network Interface");
+      //	System.Console.WriteLine($"Description: {nic.Description}");
+      //	System.Console.WriteLine($"ID: {new System.Guid(nic.Id).ToString()}");
+      //	System.Console.WriteLine($"Name: {nic.Name}");
+      //	System.Console.WriteLine($"Operational status: {nic.OperationalStatus}");
+      //	System.Console.WriteLine($"Receive only: {nic.IsReceiveOnly}");
+      //	System.Console.WriteLine($"Speed: {nic.Speed.ToGroupString()}");
+      //	System.Console.WriteLine($"Supports multicast: {nic.SupportsMulticast}");
+      //	System.Console.WriteLine($"Type: {nic.NetworkInterfaceType}");
+      //	if (nic.GetPhysicalAddress().ToStringMAC() is var mac && !string.IsNullOrWhiteSpace(mac))
+      //		System.Console.WriteLine($"Physical address: {mac}");
+      //	System.Console.WriteLine();
 
-			//	if (showIPStatistics)
-			//	{
-			//		var ips = nic.GetIPStatistics();
-			//		System.Console.WriteLine($"\tIP Statistics");
-			//		System.Console.WriteLine($"\tIncoming packets: {ips.IncomingPacketsDiscarded.ToGroupString()} discarded, {ips.IncomingPacketsWithErrors.ToGroupString()} errors, {ips.IncomingUnknownProtocolPackets.ToGroupString()} unknown.");
-			//		System.Console.WriteLine($"\tNon-unicast packets: {ips.NonUnicastPacketsReceived.ToGroupString()} received, {ips.NonUnicastPacketsSent.ToGroupString()} sent.");
-			//		System.Console.WriteLine($"\tOutgoing packets: {ips.OutgoingPacketsDiscarded.ToGroupString()} discarded, {ips.OutgoingPacketsWithErrors.ToGroupString()} errors.");
-			//		System.Console.WriteLine($"\tOutput queue length: {ips.OutputQueueLength.ToGroupString()}");
-			//		System.Console.WriteLine($"\tUnicast packets: {ips.UnicastPacketsReceived.ToGroupString()} received, {ips.UnicastPacketsSent.ToGroupString()} sent.");
-			//		System.Console.WriteLine();
-			//	}
+      //	if (showIPStatistics)
+      //	{
+      //		var ips = nic.GetIPStatistics();
+      //		System.Console.WriteLine($"\tIP Statistics");
+      //		System.Console.WriteLine($"\tIncoming packets: {ips.IncomingPacketsDiscarded.ToGroupString()} discarded, {ips.IncomingPacketsWithErrors.ToGroupString()} errors, {ips.IncomingUnknownProtocolPackets.ToGroupString()} unknown.");
+      //		System.Console.WriteLine($"\tNon-unicast packets: {ips.NonUnicastPacketsReceived.ToGroupString()} received, {ips.NonUnicastPacketsSent.ToGroupString()} sent.");
+      //		System.Console.WriteLine($"\tOutgoing packets: {ips.OutgoingPacketsDiscarded.ToGroupString()} discarded, {ips.OutgoingPacketsWithErrors.ToGroupString()} errors.");
+      //		System.Console.WriteLine($"\tOutput queue length: {ips.OutputQueueLength.ToGroupString()}");
+      //		System.Console.WriteLine($"\tUnicast packets: {ips.UnicastPacketsReceived.ToGroupString()} received, {ips.UnicastPacketsSent.ToGroupString()} sent.");
+      //		System.Console.WriteLine();
+      //	}
 
-			//}
+      //}
 
-			//using (var sr = new System.IO.StreamReader(@"C:\Test\Xml.xml"))
-			//	foreach (var rune in sr.EnumerateTextElements())
-			//		System.Console.Write(rune.ToString());
+      //using (var sr = new System.IO.StreamReader(@"C:\Test\Xml.xml"))
+      //	foreach (var rune in sr.EnumerateTextElements())
+      //		System.Console.Write(rune.ToString());
 
-			//using (var sr = new System.IO.StreamReader(@"C:\Test\Xml.xml"))
-			//	foreach (var rune in sr.EnumerateRunes())
-			//		System.Console.Write(rune.ToString());
+      //using (var sr = new System.IO.StreamReader(@"C:\Test\Xml.xml"))
+      //	foreach (var rune in sr.EnumerateRunes())
+      //		System.Console.Write(rune.ToString());
 
-			//System.Console.WriteLine(Flux.Diagnostics.Performance.Measure(() => RegularForLoop(10, 0.1), 1));
-			//System.Console.WriteLine(Flux.Diagnostics.Performance.Measure(() => ParallelForLoop(10, 0.1), 1));
-		}
+      //System.Console.WriteLine(Flux.Diagnostics.Performance.Measure(() => RegularForLoop(10, 0.1), 1));
+      //System.Console.WriteLine(Flux.Diagnostics.Performance.Measure(() => ParallelForLoop(10, 0.1), 1));
+    }
 
-		static void RegularForLoop(int taskCount = 10, double taskLoad = 1)
-		{
-			//var startDateTime = DateTime.Now;
-			//System.Console.WriteLine($"{nameof(RegularForLoop)} started at {startDateTime}.");
-			for (int i = 0; i < taskCount; i++)
-			{
-				ExpensiveTask(taskLoad);
-				//var total = ExpensiveTask(taskLoad);
-				//System.Console.WriteLine($"{nameof(ExpensiveTask)} {i} - {total}.");
-			}
-			//var endDateTime = DateTime.Now;
-			//System.Console.WriteLine($"{nameof(RegularForLoop)} ended at {endDateTime}.");
-			//var span = endDateTime - startDateTime;
-			//System.Console.WriteLine($"{nameof(RegularForLoop)} executed in {span.TotalSeconds} seconds.");
-			//System.Console.WriteLine();
-		}
+    static void RegularForLoop(int taskCount = 10, double taskLoad = 1)
+    {
+      //var startDateTime = DateTime.Now;
+      //System.Console.WriteLine($"{nameof(RegularForLoop)} started at {startDateTime}.");
+      for (int i = 0; i < taskCount; i++)
+      {
+        ExpensiveTask(taskLoad);
+        //var total = ExpensiveTask(taskLoad);
+        //System.Console.WriteLine($"{nameof(ExpensiveTask)} {i} - {total}.");
+      }
+      //var endDateTime = DateTime.Now;
+      //System.Console.WriteLine($"{nameof(RegularForLoop)} ended at {endDateTime}.");
+      //var span = endDateTime - startDateTime;
+      //System.Console.WriteLine($"{nameof(RegularForLoop)} executed in {span.TotalSeconds} seconds.");
+      //System.Console.WriteLine();
+    }
 
-		static void ParallelForLoop(int taskCount = 10, double taskLoad = 1)
-		{
-			//var startDateTime = DateTime.Now;
-			System.Threading.Tasks.Parallel.For(0, taskCount, i =>
-			{
-				ExpensiveTask(taskLoad);
-				//var total = ExpensiveTask(taskLoad);
-				//System.Console.WriteLine($"{nameof(ExpensiveTask)} {i} - {total}.");
-			});
-			//var endDateTime = DateTime.Now;
-			//System.Console.WriteLine($"{nameof(ParallelForLoop)} ended at {endDateTime}.");
-			//var span = endDateTime - startDateTime;
-			//System.Console.WriteLine($"{nameof(ParallelForLoop)} executed in {span.TotalSeconds} seconds");
-			//System.Console.WriteLine();
-		}
+    static void ParallelForLoop(int taskCount = 10, double taskLoad = 1)
+    {
+      //var startDateTime = DateTime.Now;
+      System.Threading.Tasks.Parallel.For(0, taskCount, i =>
+      {
+        ExpensiveTask(taskLoad);
+        //var total = ExpensiveTask(taskLoad);
+        //System.Console.WriteLine($"{nameof(ExpensiveTask)} {i} - {total}.");
+      });
+      //var endDateTime = DateTime.Now;
+      //System.Console.WriteLine($"{nameof(ParallelForLoop)} ended at {endDateTime}.");
+      //var span = endDateTime - startDateTime;
+      //System.Console.WriteLine($"{nameof(ParallelForLoop)} executed in {span.TotalSeconds} seconds");
+      //System.Console.WriteLine();
+    }
 
-		static long ExpensiveTask(double taskLoad = 1)
-		{
-			var total = 0L;
-			for (var i = 1; i < int.MaxValue * taskLoad; i++)
-				total += i;
-			return total;
-		}
+    static long ExpensiveTask(double taskLoad = 1)
+    {
+      var total = 0L;
+      for (var i = 1; i < int.MaxValue * taskLoad; i++)
+        total += i;
+      return total;
+    }
 
-		static void Main(string[] args)
-		{
-			System.Console.InputEncoding = System.Text.Encoding.UTF8;
-			System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+    static void Main(string[] args)
+    {
+      System.Console.InputEncoding = System.Text.Encoding.UTF8;
+      System.Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-			System.Console.WriteLine(Flux.Diagnostics.Performance.Measure(() => TimedMain(args), 1));
+      System.Console.WriteLine(Flux.Diagnostics.Performance.Measure(() => TimedMain(args), 1));
 
-			System.Console.WriteLine($"{System.Environment.NewLine}Press any key to exit...");
-			System.Console.ReadKey();
-		}
-	}
+      System.Console.WriteLine($"{System.Environment.NewLine}Press any key to exit...");
+      System.Console.ReadKey();
+    }
+  }
 }
