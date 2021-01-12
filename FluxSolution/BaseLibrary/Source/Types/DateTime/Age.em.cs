d8db2c908e4274075e0552b97d12842434d2a774
Robert Hugo <Robert.Hugo@pima.gov>
2021-01-12 12:12:24 -0700

    Updated comment for Age.

--- FluxSolution/BaseLibrary/Source/Types/DateTime/Age.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/DateTime/Age.em.cs
@@ -17,7 +17,7 @@ namespace Flux
 		public static double AgeInTotalYears(this System.DateTime source, System.DateTime target)
 			=> AgeInTotalYears(source, target, out var _);
 
-		/// <summary>Determines the life time in integer years (age) between the source (e.g. a birth) and the specified target (e.g. a birthday). I.e. the typical western way of calculating age. Work in either direction.</summary>
+		/// <summary>Determines the life time in integer years (age) between the source (e.g. a birth) and the specified target (e.g. a birthday). I.e. the common western way of calculating age. There are a multitude of scenarios where this way of calulcating would not be applicable. Work in either direction.</summary>
 		public static int AgeInYears(this System.DateTime source, System.DateTime target)
 			=> unchecked(target.Year - source.Year is int age && age > 0 && source.AddYears(age) > target ? age - 1 : age < 0 && source.AddYears(age) < target ? age + 1 : age);
 	}
