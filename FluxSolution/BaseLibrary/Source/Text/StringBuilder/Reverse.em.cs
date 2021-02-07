5fc24b568a7576cf4a2f3a9f05111062f704088d
Robert Hugo <robert_hugo@hotmail.com>
2021-02-07 09:28:32 -0700

    Removed unnecessary allocations and replaced with inline increment/decrement.

--- FluxSolution/BaseLibrary/Source/Text/StringBuilder/Reverse.em.cs
+++ FluxSolution/BaseLibrary/Source/Text/StringBuilder/Reverse.em.cs
@@ -1,35 +1,31 @@
 namespace Flux
 {
-	public static partial class SystemTextStringBuilderEm
-	{
-		/// <summary>Reverse all ranged characters, in-place.</summary>
-		internal static System.Text.StringBuilder ReverseImpl(this System.Text.StringBuilder source, int startIndex, int lastIndex)
-		{
-			for (int indexL = startIndex, indexR = lastIndex; indexL < indexR; indexL++, indexR--)
-			{
-				var tmp = source[indexL];
-				source[indexL] = source[indexR];
-				source[indexR] = tmp;
-			}
+  public static partial class SystemTextStringBuilderEm
+  {
+    /// <summary>Reverse all ranged characters, in-place.</summary>
+    internal static System.Text.StringBuilder ReverseImpl(this System.Text.StringBuilder source, int startIndex, int lastIndex)
+    {
+      while (startIndex < lastIndex)
+        Swap(source, startIndex++, lastIndex--);
 
-			return source;
-		}
+      return source;
+    }
 
-		/// <summary>Reverse all characters in-place.</summary>
-		public static System.Text.StringBuilder Reverse(this System.Text.StringBuilder source)
-		{
-			if (source is null) throw new System.ArgumentNullException(nameof(source));
+    /// <summary>Reverse all characters in-place.</summary>
+    public static System.Text.StringBuilder Reverse(this System.Text.StringBuilder source)
+    {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
 
-			return ReverseImpl(source, 0, source.Length - 1);
-		}
-		/// <summary>Reverse all ranged characters in-place.</summary>
-		public static System.Text.StringBuilder Reverse(this System.Text.StringBuilder source, int startIndex, int endIndex)
-		{
-			if (source is null) throw new System.ArgumentNullException(nameof(source));
-			if (startIndex < 0 || startIndex >= source.Length) throw new System.ArgumentOutOfRangeException(nameof(startIndex));
-			if (endIndex < startIndex || endIndex >= source.Length) throw new System.ArgumentOutOfRangeException(nameof(endIndex));
+      return ReverseImpl(source, 0, source.Length - 1);
+    }
+    /// <summary>Reverse all ranged characters in-place.</summary>
+    public static System.Text.StringBuilder Reverse(this System.Text.StringBuilder source, int startIndex, int endIndex)
+    {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
+      if (startIndex < 0 || startIndex >= source.Length) throw new System.ArgumentOutOfRangeException(nameof(startIndex));
+      if (endIndex < startIndex || endIndex >= source.Length) throw new System.ArgumentOutOfRangeException(nameof(endIndex));
 
-			return ReverseImpl(source, startIndex, endIndex);
-		}
-	}
+      return ReverseImpl(source, startIndex, endIndex);
+    }
+  }
 }
