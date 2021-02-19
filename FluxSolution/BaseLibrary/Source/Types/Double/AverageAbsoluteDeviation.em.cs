e9867b7b36fe415258a729a97234b560794e29ce
Robert Hugo <Robert.Hugo@pima.gov>
2021-02-19 15:29:32 -0700

    Limited statistical update.

--- FluxSolution/BaseLibrary/Source/Types/Double/AverageAbsoluteDeviation.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/Double/AverageAbsoluteDeviation.em.cs
@@ -4,15 +4,14 @@ namespace Flux
 {
   public static partial class SystemDoubleEm
   {
-    /// <summary>The average absolute deviation, or mean absolute deviation (MAD), of a data set is the average of the absolute deviations from a central point.</summary>
-    /// <see cref="https://en.wikipedia.org/wiki/Average_absolute_deviation"/>
-    public static double AverageAbsoluteDeviation(this System.Collections.Generic.IList<double> source, double mean, double center)
-    {
-      return source.Sum(v => System.Math.Abs(v - center)) / mean;
-    }
-    /// <summary>The average absolute deviation, or mean absolute deviation (MAD), of a data set is the average of the absolute deviations from a central point.</summary>
-    /// <see cref="https://en.wikipedia.org/wiki/Average_absolute_deviation"/>
-    public static double AverageAbsoluteDeviationFromMean(this System.Collections.Generic.IEnumerable<double> source)
+		/// <summary>The average absolute deviation, or mean absolute deviation (MAD), of a data set is the average of the absolute deviations from a central point.</summary>
+		/// <see cref="https://en.wikipedia.org/wiki/Average_absolute_deviation"/>
+		public static double AverageAbsoluteDeviation(this System.Collections.Generic.IList<double> source, double mean, double center) 
+      => source.Sum(v => System.Math.Abs(v - center)) / mean;
+
+		/// <summary>The average absolute deviation, or mean absolute deviation (MAD), of a data set is the average of the absolute deviations from a central point.</summary>
+		/// <see cref="https://en.wikipedia.org/wiki/Average_absolute_deviation"/>
+		public static double AverageAbsoluteDeviationFromMean(this System.Collections.Generic.IEnumerable<double> source)
     {
       var mean = Mean(source, out System.Collections.Generic.List<double> values);
 
