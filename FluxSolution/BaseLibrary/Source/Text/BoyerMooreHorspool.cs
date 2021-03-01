709a30d5096b95215cca57158b7bc7a567ca50aa
Robert Hugo <Robert.Hugo@pima.gov>
2021-03-01 07:01:18 -0700

    Git sync.

---
+++ FluxSolution/BaseLibrary/Source/Text/BoyerMooreHorspool.cs
@@ -0,0 +1,19 @@
+﻿namespace Flux
+{
+	public class BoyerMooreHorspool
+	{
+		public static System.Collections.Generic.Dictionary<char, int> PreProcess(System.ReadOnlySpan<char> pattern)
+		{
+			var table = new System.Collections.Generic.Dictionary<char, int>();
+
+			for (var i = 0; i < pattern.Length; i++)
+				if (!table.ContainsKey(pattern[i]))
+					table.Add(pattern[i], pattern.Length);
+
+			for (var i = 0; i < pattern.Length; i++)
+				table[pattern[i]] = pattern.Length - 1 - i;
+
+			return table;
+		}
+	}
+}
