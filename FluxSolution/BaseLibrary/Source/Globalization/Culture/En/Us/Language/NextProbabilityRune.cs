2070610b277674eb84f34f5bfa18593cd94e39af
Robert Hugo <robert_hugo@hotmail.com>
2021-03-01 23:11:10 -0700

    More changes to rune API.

--- FluxSolution/BaseLibrary/Source/Globalization/Culture/En/Us/Language/NextProbabilityRune.cs
+++ FluxSolution/BaseLibrary/Source/Globalization/Culture/En/Us/Language/NextProbabilityRune.cs
@@ -1,76 +1,75 @@
-using System.Linq;
-
 namespace Flux
 {
-	public static partial class GlobalizationEnUsEm
-	{
-		public static System.Text.Rune NextProbabilityRuneEnUs(this System.Random source)
-		{
-			if (source is null) throw new System.ArgumentNullException(nameof(source));
+  public static partial class GlobalizationEnUsEm
+  {
+    /// <summary></summary>
+    public static System.Text.Rune NextProbabilityRuneEnUs(this System.Random source)
+    {
+      if (source is null) throw new System.ArgumentNullException(nameof(source));
 
-			var value = source.NextDouble();
+      double spectrum = 1.0, value = source.NextDouble();
 
-			var spectrum = 0.0;
+      for (var index = Globalization.EnUs.Language.RelativeFrequencyOfLetters.Count - 1; index >= 0; index--)
+      {
+        var kvp = Globalization.EnUs.Language.RelativeFrequencyOfLetters[index];
 
-			for (var index = 0; index < Globalization.EnUs.Language.RelativeFrequencyOfLetters.Count; index++)
-			{
-				var kvp = Globalization.EnUs.Language.RelativeFrequencyOfLetters[index];
+        spectrum -= kvp.Value;
 
-				spectrum += kvp.Value;
+        if (value >= spectrum)
+          return kvp.Key;
+      }
 
-				if (value < spectrum)
-					return kvp.Key;
-			}
+      return Globalization.EnUs.Language.RelativeFrequencyOfLetters[0].Key;
+    }
+    /// <summary></summary>
+    public static System.Text.Rune NextProbabilityRuneEnUsConsonant(this System.Random source, bool includeY)
+    {
+      var rune = NextProbabilityRuneEnUs(source);
+      while (!Globalization.EnUs.Language.IsEnglishConsonant(rune, includeY))
+        rune = NextProbabilityRuneEnUs(source);
+      return rune;
+    }
+    /// <summary></summary>
+    public static System.Text.Rune NextProbabilityRuneEnUsVowel(this System.Random source, bool includeY)
+    {
+      var rune = NextProbabilityRuneEnUs(source);
+      while (!Globalization.EnUs.Language.IsEnglishVowel(rune, includeY))
+        rune = NextProbabilityRuneEnUs(source);
+      return rune;
+    }
 
-			return Globalization.EnUs.Language.RelativeFrequencyOfLetters[0].Key;
-		}
-		public static System.Text.Rune NextProbabilityRuneEnUsConsonant(this System.Random source, bool includeY)
-		{
-			var rune = NextProbabilityRuneEnUs(source);
-			while (!Globalization.EnUs.Language.IsEnglishConsonant(rune, includeY))
-				rune = NextProbabilityRuneEnUs(source);
-			return rune;
-		}
-		public static System.Text.Rune NextProbabilityRuneEnUsVowel(this System.Random source, bool includeY)
-		{
-			var rune = NextProbabilityRuneEnUs(source);
-			while (!Globalization.EnUs.Language.IsEnglishVowel(rune, includeY))
-				rune = NextProbabilityRuneEnUs(source);
-			return rune;
-		}
+    ///// <summary>Indicates whether the char is an English letter.</summary>
+    //public static bool IsEnglishLetter(this char source)
+    //  => IsEnglishLetterLower(source) || IsEnglishLetterUpper(source);
+    ///// <summary>Indicates whether the char is an English lower case letter.</summary>
+    //public static bool IsEnglishLetterLower(this char source)
+    //  => source >= 'a' && source <= 'z';
+    ///// <summary>Indicates whether the char is an English upper case letter.</summary>
+    //public static bool IsEnglishLetterUpper(this char source)
+    //  => source >= 'A' && source <= 'Z';
 
-		///// <summary>Indicates whether the char is an English letter.</summary>
-		//public static bool IsEnglishLetter(this char source)
-		//  => IsEnglishLetterLower(source) || IsEnglishLetterUpper(source);
-		///// <summary>Indicates whether the char is an English lower case letter.</summary>
-		//public static bool IsEnglishLetterLower(this char source)
-		//  => source >= 'a' && source <= 'z';
-		///// <summary>Indicates whether the char is an English upper case letter.</summary>
-		//public static bool IsEnglishLetterUpper(this char source)
-		//  => source >= 'A' && source <= 'Z';
-
-		///// <summary>Indicates whether the char is an English vowel, where y|Y is optional.</summary>
-		//public static bool IsEnglishVowel(this char source, bool includeY)
-		//{
-		//  switch (source)
-		//  {
-		//    case 'a':
-		//    case 'e':
-		//    case 'i':
-		//    case 'o':
-		//    case 'u':
-		//    case 'A':
-		//    case 'E':
-		//    case 'I':
-		//    case 'O':
-		//    case 'U':
-		//      return true;
-		//    case 'y':
-		//    case 'Y':
-		//      return includeY;
-		//    default:
-		//      return false;
-		//  }
-		//}
-	}
+    ///// <summary>Indicates whether the char is an English vowel, where y|Y is optional.</summary>
+    //public static bool IsEnglishVowel(this char source, bool includeY)
+    //{
+    //  switch (source)
+    //  {
+    //    case 'a':
+    //    case 'e':
+    //    case 'i':
+    //    case 'o':
+    //    case 'u':
+    //    case 'A':
+    //    case 'E':
+    //    case 'I':
+    //    case 'O':
+    //    case 'U':
+    //      return true;
+    //    case 'y':
+    //    case 'Y':
+    //      return includeY;
+    //    default:
+    //      return false;
+    //  }
+    //}
+  }
 }
