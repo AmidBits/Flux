83de8e891a15c5d6e856ae7e013bc22fb34e7d86
Robert Hugo <Robert.Hugo@pima.gov>
2021-05-06 15:33:20 -0700

    Updated type reference.

--- FluxSolution/PowerShell/Flux.ps1
+++ FluxSolution/PowerShell/Flux.ps1
@@ -1,45 +1,51 @@
 
+# Converts a local project URI to a 'binary' URI, for debugging purposes.
+function ConvertTo-BinUri ( [uri]$VsUri, [hashtable]$VsReference ) 
+{ New-Object System.Uri $VsUri.OriginalString.Replace("/\", "/\$($VsReference.SolutionName)\$($VsReference.ProjectName)\bin\$($VsReference.Configuration)\$($VsReference.TargetFramework)\") }
+# Expand a relative solution resource reference to a file path.
+function Expand-FileToPath ( [string]$VsRelativeFileName, [hashtable]$VsReference ) 
+{ ".$((ConvertTo-BinUri (New-Object System.Uri "file://\$VsRelativeFileName") $VsReference).LocalPath.Replace('/', '\'))" }
+# Tests whether a pipeline contains any objects. 
+function Test-Any() 
+{ begin { $any = $false } process { $any = $true } end { $any } }
+
 Clear-Host
+
+"PowerShell $($PSVersionTable.PSVersion) $($PSVersionTable.PSEdition) on $($PSVersionTable.OS) ($($PSVersionTable.Platform))"
 
 $vsProjectReference = @{ SolutionName='FluxSolution'; ProjectName='BaseLibrary'; Configuration='Debug'; TargetFramework='net5.0' }
 
-# Converts a local project URI to a 'binary' URI, for debugging purposes.
-function ConvertTo-BinUri ( [uri]$VsUri, [hashtable]$VsReference ) { New-Object System.Uri $VsUri.OriginalString.Replace("/\", "/\$($VsReference.SolutionName)\$($VsReference.ProjectName)\bin\$($VsReference.Configuration)\$($VsReference.TargetFramework)\") }
-# Expand a relative solution resource reference to a file path.
-function Expand-FileToPath ( [string]$VsRelativeFileName, [hashtable]$VsReference ) { ".$((ConvertTo-BinUri (New-Object System.Uri "file://\$VsRelativeFileName") $VsReference).LocalPath.Replace('/', '\'))" }
-# Tests whether a pipeline contains any objects. 
-function Test-Any() { begin { $any = $false } process { $any = $true } end { $any } }
+"Checking availability of <$($vsProjectReference.SolutionName)\$($vsProjectReference.ProjectName)\$($vsProjectReference.TargetFramework)\$($vsProjectReference.Configuration)>"
 
 if(-not ([System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName -match "^$($vsProjectReference.ProjectName)" } | Test-Any)) # Check whether the project library is already loaded.
 {
     # Various ways to include/use the Flux BaseLibray in PowerShell:
 
     [string]$assemblyFileName = Expand-FileToPath "$($vsProjectReference.ProjectName).dll" $vsProjectReference # The file name of the binary assembly.
-    "Loading assembly from file ($assemblyFileName)."
+    "Loading assembly <$assemblyFileName>"
 
     [byte[]]$bytes = [System.IO.File]::ReadAllBytes($assemblyFileName) # Read the binary assembly to a byte array.
 
-    # [string]$base64 = [System.Convert]::ToBase64String($bytes) # Encode the byte array to a Base64 string. 
+    <#
+    [string]$base64 = [System.Convert]::ToBase64String($bytes) # Encode the byte array to a Base64 string. 
 
-    # [string]$base64TextFile = 'C:\Flux\AssemblyString.txt' # The path of the base64 string.
-    # [void][System.IO.Directory]::CreateDirectory([System.IO.Path]::GetDirectoryName($base64TextFile)) # Ensure the folder structure is there.
-    # [System.IO.File]::WriteAllText($base64TextFile, $base64) # Write the Base64 string to a text file.
+    [string]$base64TextFile = 'C:\Flux\AssemblyString.txt' # The path of the base64 string.
+    [void][System.IO.Directory]::CreateDirectory([System.IO.Path]::GetDirectoryName($base64TextFile)) # Ensure the folder structure is there.
+    [System.IO.File]::WriteAllText($base64TextFile, $base64) # Write the Base64 string to a text file.
+   
+    # Optionally use a literal Base64 string (obtain from the text file created above) to create independence from files.
+    [string]$base64 = '[REPLACE_WITH_LITERAL_BASE64_STRING_FROM_FILE]'
 
-    # # Optionally use a literal Base64 string (obtain from the text file created above) to create independence from files.
-    # [string]$base64 = '[REPLACE_WITH_LITERAL_BASE64_STRING_FROM_FILE]'
+    if([System.IO.File]::Exists($base64TextFile)) # Check if the Base64 file exists.
+    { [string]$base64 = [System.IO.File]::ReadAllText($base64TextFile) } # Read a Base64 string from the text file.
 
-    # if([System.IO.File]::Exists($base64TextFile)) # Check if the Base64 file exists.
-    # { [string]$base64 = [System.IO.File]::ReadAllText($base64TextFile) } # Read a Base64 string from the text file.
-
-    # [byte[]]$bytes = [System.Convert]::FromBase64String($base64) # Decode the Base64 string to a byte array.
+    [byte[]]$bytes = [System.Convert]::FromBase64String($base64) # Decode the Base64 string to a byte array.
+    #>
 
     [void][System.Reflection.Assembly]::Load($bytes) # Load the byte array as an assembly into the current context.
 }
-else { "Assembly already loaded..." }
 
 # Sample use from Flux BaseLibrary:
-
-# $PSVersionTable
 
 # [Flux.Locale].GetProperties() | Select-Object Name | ForEach-Object { "$($_.Name)=`"$([Flux.Locale]::"$($_.Name)")`"" }
 # [Flux.Locale]::SpecialFolders | Format-Table
