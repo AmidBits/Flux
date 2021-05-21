58a52c9b6360a13c3154e089df41945129073f21
Robert Hugo <Robert.Hugo@pima.gov>
2021-05-21 14:08:24 -0700

    Updated new structure for adjacent grouping.

---
+++ FluxSolution/BaseLibrary/Source/Collections/Generic/IEnumerable/Partition/AdjacentGrouping.cs
@@ -0,0 +1,67 @@
+namespace Flux
+{
+	/// <summary>Class used to group adjacent elements in a sequence. Derives from System.Linq.IGrouping<TKey, TSource>.</summary>
+	internal class AdjacentGrouping<TKey, TElement>
+		: System.Collections.Generic.IEnumerable<TElement>, System.Linq.IGrouping<TKey, TElement>
+	{
+		private System.Collections.Immutable.IImmutableList<TElement> m_elements = System.Collections.Immutable.ImmutableList<TElement>.Empty;
+
+		public int Count
+			=> m_elements.Count;
+
+		public void Add(TElement element)
+			=> m_elements = m_elements.Add(element);
+
+		// IEnumerable
+		System.Collections.Generic.IEnumerator<TElement> System.Collections.Generic.IEnumerable<TElement>.GetEnumerator()
+			=> m_elements.GetEnumerator();
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+			=> m_elements.GetEnumerator();
+
+		// IGrouping
+		public TKey Key { get; }
+
+		public AdjacentGrouping(TKey key, TElement source)
+		{
+			Key = key;
+			m_elements.Add(source);
+		}
+
+		/// <summary>Creates a new sequence of equal (based on the specified keySelector) adjacent (consecutive) items grouped together as a key and a list. Uses the specified equality comparer.</summary>
+		public static System.Collections.Generic.IEnumerable<System.Linq.IGrouping<TKey, TElement>> GroupAdjacent(System.Collections.Generic.IEnumerable<TElement> source, System.Func<TElement, TKey> keySelector, System.Collections.Generic.IEqualityComparer<TKey> comparer)
+		{
+			if (source is null) throw new System.ArgumentNullException(nameof(source));
+			if (keySelector is null) throw new System.ArgumentNullException(nameof(keySelector));
+			if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
+
+			using var e = source.GetEnumerator();
+
+			if (e.MoveNext())
+			{
+				var ag = new AdjacentGrouping<TKey, TElement>(keySelector(e.Current), e.Current);
+
+				while (e.MoveNext())
+				{
+					var key = keySelector(e.Current);
+
+					if (comparer.Equals(ag.Key, key))
+					{
+						ag.Add(e.Current);
+					}
+					else
+					{
+						yield return ag;
+
+						ag = new AdjacentGrouping<TKey, TElement>(key, e.Current);
+					}
+				}
+
+				if (ag.Count > 0) 
+					yield return ag;
+			}
+		}
+		/// <summary>Creates a new sequence of equal (based on the specified keySelector) consecutive (adjacent) items grouped together as a key and a list. Uses the default equality comparer.</summary>
+		public static System.Collections.Generic.IEnumerable<System.Linq.IGrouping<TKey, TElement>> GroupAdjacent(System.Collections.Generic.IEnumerable<TElement> source, System.Func<TElement, TKey> keySelector)
+			=> GroupAdjacent(source, keySelector, System.Collections.Generic.EqualityComparer<TKey>.Default);
+	}
+}
--- FluxSolution/BaseLibrary/Source/Collections/Generic/IEnumerable/Partition/GroupAdjacent.em.cs
+++ FluxSolution/BaseLibrary/Source/Collections/Generic/IEnumerable/Partition/GroupAdjacent.em.cs
@@ -1,63 +1,12 @@
 namespace Flux
 {
-  /// <summary>Class used to group adjacent elements in a sequence. Derives from System.Linq.IGrouping<TKey, TSource>.</summary>
-  internal class AdjacentGrouping<TKey, TSource>
-    : System.Collections.Generic.IEnumerable<TSource>, System.Linq.IGrouping<TKey, TSource>
-  {
-    public TKey Key { get; }
-
-    public System.Collections.Generic.List<TSource> Consecutive { get; } = new System.Collections.Generic.List<TSource>();
-
-    System.Collections.Generic.IEnumerator<TSource> System.Collections.Generic.IEnumerable<TSource>.GetEnumerator()
-    {
-      foreach (var groupItem in Consecutive)
-        yield return groupItem;
-    }
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => ((System.Collections.Generic.IEnumerable<TSource>)this).GetEnumerator();
-
-    public AdjacentGrouping(TKey key, TSource source)
-    {
-      Key = key;
-      Consecutive.Add(source);
-    }
-  }
-
-  public static partial class SystemCollectionsGenericEm
-  {
-    /// <summary>Creates a new sequence of equal (based on the specified keySelector) consecutive (adjacent) items grouped together as a key and a list. Uses the specified equality comparer.</summary>
-    public static System.Collections.Generic.IEnumerable<System.Linq.IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TKey> keySelector, System.Collections.Generic.IEqualityComparer<TKey> comparer)
-    {
-      comparer ??= System.Collections.Generic.EqualityComparer<TKey>.Default;
-
-      if (keySelector is null) throw new System.ArgumentNullException(nameof(keySelector));
-
-      using var e = source?.GetEnumerator() ?? throw new System.ArgumentNullException(nameof(source));
-
-      if (e.MoveNext())
-      {
-        var goa = new AdjacentGrouping<TKey, TSource>(keySelector(e.Current), e.Current);
-
-        while (e.MoveNext())
-        {
-          var key = keySelector(e.Current);
-
-          if (comparer.Equals(goa.Key, key))
-          {
-            goa.Consecutive.Add(e.Current);
-          }
-          else
-          {
-            yield return goa;
-
-            goa = new AdjacentGrouping<TKey, TSource>(key, e.Current);
-          }
-        }
-
-        if (goa.Consecutive.Count > 0) yield return goa;
-      }
-    }
-    /// <summary>Creates a new sequence of equal (based on the specified keySelector) consecutive (adjacent) items grouped together as a key and a list. Uses the default equality comparer.</summary>
-    public static System.Collections.Generic.IEnumerable<System.Linq.IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TKey> keySelector)
-      => GroupAdjacent(source, keySelector, System.Collections.Generic.EqualityComparer<TKey>.Default);
-  }
+	public static partial class SystemCollectionsGenericEm
+	{
+		/// <summary>Creates a new sequence of equal (based on the specified keySelector) adjacent (consecutive) items grouped together as a key and a list. Uses the specified equality comparer.</summary>
+		public static System.Collections.Generic.IEnumerable<System.Linq.IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TKey> keySelector, System.Collections.Generic.IEqualityComparer<TKey> comparer)
+			=> AdjacentGrouping<TKey, TSource>.GroupAdjacent(source, keySelector, comparer);
+		/// <summary>Creates a new sequence of equal (based on the specified keySelector) consecutive (adjacent) items grouped together as a key and a list. Uses the default equality comparer.</summary>
+		public static System.Collections.Generic.IEnumerable<System.Linq.IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TKey> keySelector)
+			=> AdjacentGrouping<TKey, TSource>.GroupAdjacent(source, keySelector, System.Collections.Generic.EqualityComparer<TKey>.Default);
+	}
 }
