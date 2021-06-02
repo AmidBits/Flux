3eebf785c8d16466afd69445a0f7f813662cc5ba
Robert Hugo <Robert.Hugo@pima.gov>
2021-06-02 16:00:57 -0700

    Latest!

---
+++ FluxSolution/BaseLibrary/Source/Text/Rune/IsUnicodeCategory.em.cs
@@ -0,0 +1,79 @@
+namespace Flux
+{
+  public static partial class SystemTextRuneEm
+  {
+    /// <summary>Determines whether the character is in the specified unicode category.</summary>
+    public static bool IsUnicodeCategory(this System.Text.Rune source, System.Globalization.UnicodeCategory category)
+      => System.Text.Rune.GetUnicodeCategory(source) == category;
+    /// <summary>Determines whether the character is in any of the specified unicode categories.</summary>
+    public static bool IsUnicodeCategory(this System.Text.Rune source, params System.Globalization.UnicodeCategory[] category)
+      => System.Array.Exists(category, uc => System.Text.Rune.GetUnicodeCategory(source) == uc);
+
+    public static bool IsUnicodeLowercaseLetter(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.LowercaseLetter;
+    public static bool IsUnicodeModifierLetter(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.ModifierLetter;
+    public static bool IsUnicodeOtherLetter(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.OtherLetter;
+    public static bool IsUnicodeTitlecaseLetter(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.TitlecaseLetter;
+    public static bool IsUnicodeUppercaseLetter(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.UppercaseLetter;
+
+    public static bool IsUnicodeEnclosingMark(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.EnclosingMark;
+    public static bool IsUnicodeNonSpacingMark(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.NonSpacingMark;
+    public static bool IsUnicodeSpacingCombiningMark(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.SpacingCombiningMark;
+
+    public static bool IsUnicodeDecimalDigitNumber(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.DecimalDigitNumber;
+    public static bool IsUnicodeLetterNumber(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.LetterNumber;
+    public static bool IsUnicodeOtherNumber(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.OtherNumber;
+
+    public static bool IsUnicodeClosePunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.ClosePunctuation;
+    public static bool IsUnicodeConnectorPunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.ConnectorPunctuation;
+    public static bool IsUnicodeDashPunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.DashPunctuation;
+    public static bool IsUnicodeFinalQuotePunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.FinalQuotePunctuation;
+    public static bool IsUnicodeInitialQuotePunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.InitialQuotePunctuation;
+    public static bool IsUnicodeOpenPunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.OpenPunctuation;
+    public static bool IsUnicodeOtherPunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.OtherPunctuation;
+
+    public static bool IsUnicodeCurrencySymbol(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.CurrencySymbol;
+    public static bool IsUnicodeMathSymbol(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.MathSymbol;
+    public static bool IsUnicodeModifierSymbol(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.ModifierSymbol;
+    public static bool IsUnicodeOtherSymbol(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.OtherSymbol;
+
+    public static bool IsUnicodeLineSeparator(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.LineSeparator;
+    public static bool IsUnicodeParagraphSeparator(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.ParagraphSeparator;
+    public static bool IsUnicodeSpaceSeparator(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.SpaceSeparator;
+
+    public static bool IsUnicodeControlOther(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.Control;
+    public static bool IsUnicodeFormatOther(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.Format;
+    public static bool IsUnicodeNotAssignedOther(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.OtherNotAssigned;
+    public static bool IsUnicodePrivateUseOther(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.PrivateUse;
+    public static bool IsUnicodeSurrogateOther(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source) == System.Globalization.UnicodeCategory.Surrogate;
+  }
+}
---
+++ FluxSolution/BaseLibrary/Source/Text/Rune/IsUnicodeMajorCode.em.cs
@@ -0,0 +1,24 @@
+namespace Flux
+{
+  public static partial class SystemTextRuneEm
+  {
+    /// <summary>Determines whether the character is of the specified  unicode category major code.</summary>
+    public static bool IsUnicodeMajorCode(this System.Text.Rune source, Text.UnicodeCategoryMajorCode majorCode)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == majorCode;
+
+    public static bool IsUnicodeLetter(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Letter;
+    public static bool IsUnicodeMark(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Mark;
+    public static bool IsUnicodeNumber(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Number;
+    public static bool IsUnicodePunctuation(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Punctuation;
+    public static bool IsUnicodeSymbol(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Symbol;
+    public static bool IsUnicodeSeparator(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Separator;
+    public static bool IsUnicodeOther(this System.Text.Rune source)
+      => System.Text.Rune.GetUnicodeCategory(source).ToMajorCode() == Text.UnicodeCategoryMajorCode.Other;
+  }
+}
