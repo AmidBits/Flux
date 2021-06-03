32f977f8ad1633b42f8287c252efd586643fdc51
Robert Hugo <Robert.Hugo@pima.gov>
2021-06-03 15:58:31 -0700

    Last check-in for the day.

--- FluxSolution/BaseLibrary/Source/Collections/Generic/IEnumerable/GetNearestGreaterAndLessOrEqual.em.cs
+++ FluxSolution/BaseLibrary/Source/Collections/Generic/IEnumerable/GetNearestGreaterAndLessOrEqual.em.cs
@@ -1,97 +1,102 @@
-//namespace Flux
-//{
-//  public static partial class SystemCollectionsGenericIEnumerableEm
-//  {
-//    /// <summary>Compute both the minimum and maximum element in a single pass, and return them as a 2-tuple. Uses the specified comparer.</summary>
-//    public static bool GetNearestGreaterAndLessOrEqual<TSource, TValue>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TValue> valueSelector, TValue referenceValue, out int indexOfLtMax, out int indexOfEq, out int indexOfGtMin)
-//      where TValue : struct, System.IComparable<TValue>
-//    {
-//      using var e = source.GetEnumerator();
+namespace Flux
+{
+  public static partial class SystemCollectionsGenericIEnumerableEm
+  {
+    /// <summary>Compute both the minimum and maximum element in a single pass, and return them as a 2-tuple. Uses the specified comparer.</summary>
+    public static bool GetNearestGreaterAndLessOrEqual<TSource, TValue>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TValue> valueSelector, TValue referenceValue, out int indexOfLtMax, out int indexOfEq, out int indexOfGtMin)
+      where TValue : struct, System.IComparable<TValue>
+    {
+      using var e = source.GetEnumerator();
 
-//      var ltCurrent = new TValue?();
-//      var gtCurrent = new TValue?();
+      var ltCurrent = new TValue?();
+      var gtCurrent = new TValue?();
 
-//      indexOfLtMax = -1;
-//      indexOfEq = -1;
-//      indexOfGtMin = -1;
+      indexOfLtMax = -1;
+      indexOfEq = -1;
+      indexOfGtMin = -1;
 
-//      var index = 0;
+      var index = 0;
 
-//      while (e.MoveNext())
-//      {
-//        var currentValue = valueSelector(e.Current);
+      while (e.MoveNext())
+      {
+        var currentValue = valueSelector(e.Current);
 
-//        switch (currentValue.CompareTo(referenceValue))
-//        {
-//          case int lo when lo < 0:
-//            if (!ltCurrent.HasValue || currentValue.CompareTo(ltCurrent.Value) > 0)
-//            {
-//              indexOfLtMax = index;
-//              ltCurrent = currentValue;
-//            }
-//            break;
-//          case int hi when hi > 0:
-//            if (!gtCurrent.HasValue || currentValue.CompareTo(gtCurrent.Value) < 0)
-//            {
-//              indexOfGtMin = index;
-//              gtCurrent = currentValue;
-//            }
-//            break;
-//          default:
-//            indexOfEq = index;
-//            break;
-//        }
+        switch (currentValue.CompareTo(referenceValue))
+        {
+          case int lo when lo < 0:
+            if (!ltCurrent.HasValue || currentValue.CompareTo(ltCurrent.Value) > 0)
+            {
+              indexOfLtMax = index;
+              ltCurrent = currentValue;
+            }
+            break;
+          case int hi when hi > 0:
+            if (!gtCurrent.HasValue || currentValue.CompareTo(gtCurrent.Value) < 0)
+            {
+              indexOfGtMin = index;
+              gtCurrent = currentValue;
+            }
+            break;
+          default:
+            indexOfEq = index;
+            break;
+        }
 
-//        index++;
-//      }
+        index++;
+      }
 
-//      return indexOfLtMax != -1 || indexOfEq != -1 || indexOfGtMin != -1;
-//    }
-//    public static bool GetNearestGreaterAndLessOrEqual<TSource, TValue>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TValue> valueSelector, TValue referenceValue, System.Collections.Generic.IComparer<TValue> comparer)
-//       where TValue : struct
-//    {
-//      using var e = source.GetEnumerator();
+      return indexOfLtMax != -1 || indexOfEq != -1 || indexOfGtMin != -1;
+    }
+    public static (TSource lessThan, TSource equal, TSource greaterThan) GetNearestGreaterAndLessOrEqual<TSource, TValue>(this System.Collections.Generic.IEnumerable<TSource> source, System.Func<TSource, TValue> valueSelector, TValue referenceValue, System.Collections.Generic.IComparer<TValue> comparer)
+       where TValue : struct
+    {
+      using var e = source.GetEnumerator();
 
-//      if (e.MoveNext())
-//      {
-//        var ltSource = e.Current;
+      if (e.MoveNext())
+      {
+        var cValue = valueSelector(e.Current);
 
+        var cmp = comparer.Compare(cValue, referenceValue);
 
-//        var ltCurrent = new TValue?();
-//        var gtCurrent = new TValue?();
+        var ltSource = cmp < 0 ? e.Current : default!;
+        var eqSource = cmp == 0 ? e.Current : default!;
+        var gtSource = cmp > 0 ? e.Current : default!;
 
-//        var index = 0;
+        var ltValue = cmp < 0 ? cValue : referenceValue;
+        var eqValue = cmp == 0 ? cValue : referenceValue;
+        var gtValue = cmp > 0 ? cValue : referenceValue;
 
-//        while (e.MoveNext())
-//        {
-//          var currentValue = valueSelector(e.Current);
+        while (e.MoveNext())
+        {
+          cValue = valueSelector(e.Current);
 
-//          switch (currentValue.CompareTo(referenceValue))
-//          {
-//            case int lo when lo < 0:
-//              if (!ltCurrent.HasValue || currentValue.CompareTo(ltCurrent.Value) > 0)
-//              {
-//                indexOfLtMax = index;
-//                ltCurrent = currentValue;
-//              }
-//              break;
-//            case int hi when hi > 0:
-//              if (!gtCurrent.HasValue || currentValue.CompareTo(gtCurrent.Value) < 0)
-//              {
-//                indexOfGtMin = index;
-//                gtCurrent = currentValue;
-//              }
-//              break;
-//            default:
-//              indexOfEq = index;
-//              break;
-//          }
+          switch (comparer.Compare(cValue, referenceValue))
+          {
+            case int lt when lt < 0:
+              if (comparer.Compare(cValue, ltValue) > 0)
+              {
+                ltSource = e.Current;
+                ltValue = cValue;
+              }
+              break;
+            case int gt when gt > 0:
+              if (comparer.Compare(cValue, gtValue) < 0)
+              {
+                gtSource = e.Current;
+                gtValue = cValue;
+              }
+              break;
+            default:
+              eqSource = e.Current;
+              eqValue = cValue;
+              break;
+          }
+        }
 
-//          index++;
-//        }
+        return (ltSource, eqSource, gtSource);
+      }
 
-//        return indexOfLtMax != -1 || indexOfEq != -1 || indexOfGtMin != -1;
-//      }
-//    }
-//  }
-//}
+      return (default!, default!, default!);
+    }
+  }
+}
