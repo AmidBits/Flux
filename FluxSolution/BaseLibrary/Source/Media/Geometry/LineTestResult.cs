8b5723861542d6323ce52455f56d8fb8a51cda1e
Rob <robert_hugo@hotmail.com>
2021-06-05 17:31:37 -0700

    Preparing for .NET MAUI.

--- FluxSolution/BaseLibrary/Source/Media/Geometry/LineTestResult.cs
+++ FluxSolution/BaseLibrary/Source/Media/Geometry/LineTestResult.cs
@@ -1,42 +1,39 @@
 namespace Flux.Media.Geometry
 {
-	public struct LineTestResult
-		: System.IEquatable<LineTestResult>
-	{
-		public static readonly LineTestResult Empty;
-		public bool IsEmpty => Equals(Empty);
+  public struct LineTestResult
+    : System.IEquatable<LineTestResult>
+  {
+    public static readonly LineTestResult Empty;
+    public bool IsEmpty => Equals(Empty);
 
-		public LineTestOutcome Outcome { get; set; }
+    public LineTestOutcome Outcome { get; set; }
 
-		public System.Numerics.Vector2? Point { get; set; }
+    public Flux.Maui.Graphics.Point? Point { get; set; }
 
-		public LineTestResult(LineTestOutcome outcome, System.Numerics.Vector2 point)
-		{
-			Outcome = outcome;
+    public LineTestResult(LineTestOutcome outcome)
+    {
+      Outcome = outcome;
 
-			Point = point;
-		}
-		public LineTestResult(LineTestOutcome outcome)
-		{
-			Outcome = outcome;
+      Point = null;
+    }
+    public LineTestResult(LineTestOutcome outcome, double x, double y)
+      : this(outcome)
+      => Point = new Flux.Maui.Graphics.Point(x, y);
 
-			Point = null;
-		}
-
-		// Operators
-		public static bool operator ==(LineTestResult a, LineTestResult b)
-			=> a.Equals(b);
-		public static bool operator !=(LineTestResult a, LineTestResult b)
-			=> !a.Equals(b);
-		// IEquatable
-		public bool Equals(LineTestResult other)
-			=> Outcome == other.Outcome && Point!.HasValue == other.Point!.HasValue && Point!.Value == other.Point!.Value;
-		// Object (overrides)
-		public override bool Equals(object? obj)
-			=> obj is LineTestResult o && Equals(o);
-		public override int GetHashCode()
-			=> System.HashCode.Combine(Outcome, Point);
-		public override string? ToString()
-			=> $"<{Outcome}{(Point!.HasValue ? $", {Point!.Value.ToString()}" : string.Empty)}>";
-	}
+    // Operators
+    public static bool operator ==(LineTestResult a, LineTestResult b)
+      => a.Equals(b);
+    public static bool operator !=(LineTestResult a, LineTestResult b)
+      => !a.Equals(b);
+    // IEquatable
+    public bool Equals(LineTestResult other)
+      => Outcome == other.Outcome && Point!.HasValue == other.Point!.HasValue && Point!.Value == other.Point!.Value;
+    // Object (overrides)
+    public override bool Equals(object? obj)
+      => obj is LineTestResult o && Equals(o);
+    public override int GetHashCode()
+      => System.HashCode.Combine(Outcome, Point);
+    public override string? ToString()
+      => $"<{Outcome}{(Point!.HasValue ? $", {Point!.Value.ToString()}" : string.Empty)}>";
+  }
 }
