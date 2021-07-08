f7c4fca4725f50a798464271355800eb87e02f92
Robert Hugo <robert_hugo@hotmail.com>
2021-07-07 17:50:37 -0700

    Added interval.

---
+++ FluxSolution/BaseLibrary/Source/Foundation/Interval.cs
@@ -0,0 +1,27 @@
+//namespace Flux
+//{
+//  public enum EndpointBehavior
+//  {
+//    /// <summary>Includes endpoint.</summary>
+//    Closed,
+//    /// <summary>Excludes endpoint.</summary>
+//    Open,
+//  }
+
+//  public struct IntervalEndpoint
+//  {
+//    public EndpointBehavior Behavior { get; }
+//    public double Value { get; }
+
+//    public IntervalEndpoint(double value)
+//      => Value = value;
+//  }
+
+//  public struct Interval
+//  {
+//    public IntervalType OpenCLosed { get; }
+
+//    public double Low { get; }
+//    public double High { get; }
+//  }
+//}
