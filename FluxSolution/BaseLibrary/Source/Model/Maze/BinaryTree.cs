caa3112fe21984a5f4f3d26754e54163e006f9c5
AmidBits <Robert.Hugo@pima.gov>
2021-07-23 13:44:15 -0700

    Fixed error.

--- FluxSolution/BaseLibrary/Source/Model/Maze/BinaryTree.cs
+++ FluxSolution/BaseLibrary/Source/Model/Maze/BinaryTree.cs
@@ -5,19 +5,14 @@ namespace Flux.Model.Maze
   public class BinaryTreeMaze
     : AMaze
   {
-    private int m_diagonal = (int)InterCardinalDirection.NE;
-    public InterCardinalDirection Diagonal
-    {
-      get { return (InterCardinalDirection)m_diagonal; }
-      set { m_diagonal = (int)value; }
-    }
+    public InterCardinalDirection Diagonal { get; set; } = InterCardinalDirection.NE;
 
     public override void CarveMaze(Grid grid)
     {
       if (grid is null) throw new System.ArgumentNullException(nameof(grid));
 
-      var direction1 = m_diagonal - 4;
-      var direction2 = (m_diagonal + 4) % 32;
+      var direction1 = (int)Diagonal - 4;
+      var direction2 = (direction1 + 8) % 32;
 
       foreach (var cell in grid.GetValues())
       {
