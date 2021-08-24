ab4d208d014440dde07f2a3e9585bab68220b6ad
Robert Hugo <Robert.Hugo@pima.gov>
2021-08-24 15:53:26 -0700

    Removed "in" keywords for parameters.

--- FluxSolution/BaseLibrary/Source/Foundation/Maths/DetentInterval.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/Maths/DetentInterval.cs
@@ -3,24 +3,24 @@ namespace Flux
   public static partial class Maths
   {
     /// <summary>Snaps the value to the nearest interval if it's within the specified distance of an interval.</summary>
-    public static System.Numerics.BigInteger DetentInterval(in System.Numerics.BigInteger value, in System.Numerics.BigInteger interval, in System.Numerics.BigInteger distance)
+    public static System.Numerics.BigInteger DetentInterval(System.Numerics.BigInteger value, System.Numerics.BigInteger interval, System.Numerics.BigInteger distance)
       => (value / interval) * interval is var nearestInterval && System.Numerics.BigInteger.Abs(nearestInterval - value) < distance
       ? nearestInterval
       : value;
 
     /// <summary>Snaps the value to the nearest interval if it's within the specified distance of an interval.</summary>
-    public static decimal DetentInterval(in decimal value, in decimal interval, in decimal distance)
+    public static decimal DetentInterval(decimal value, decimal interval, decimal distance)
       => System.Math.Round(value / interval) * interval is var nearestInterval && System.Math.Abs(nearestInterval - value) < distance
       ? nearestInterval
       : value;
 
     /// <summary>Snaps the value to the nearest interval if it's within the specified distance of an interval.</summary>
-    public static float DetentInterval(in float value, in float interval, in float distance)
+    public static float DetentInterval(float value, float interval, float distance)
       => (float)System.Math.Round(value / interval) * interval is var nearestInterval && (float)System.Math.Abs(nearestInterval - value) < distance
       ? nearestInterval
       : value;
     /// <summary>Snaps the value to the nearest interval if it's within the specified distance of an interval.</summary>
-    public static double DetentInterval(in double value, in double interval, in double distance)
+    public static double DetentInterval(double value, double interval, double distance)
       => System.Math.Round(value / interval) * interval is var nearestInterval && System.Math.Abs(nearestInterval - value) < distance
       ? nearestInterval
       : value;
