cd85384cd0586318041498f8174fe628c4ee83fb
Robert Hugo <Robert.Hugo@pima.gov>
2021-10-19 15:47:23 -0700

    Updates.

--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -19,35 +19,38 @@ namespace ConsoleApp
   {
     private static void TimedMain(string[] args)
     {
-      
-      var gam = new Flux.DataStructures.Graphs.AdjacencyList();
 
-      gam.AddVertex(0);
-      gam.AddVertex(1);
-      gam.AddVertex(2);
-      gam.AddVertex(3);
-      gam.AddVertex(4);
+      if (1 == 0)
+      {
+        var gam = new Flux.DataStructures.Graphs.AdjacencyList();
 
-      // 6, 8
-      gam.AddEdge(0, 1, (3, 1));
-      gam.AddEdge(0, 2, (1, 0));
-      gam.AddEdge(0, 4, (3, 2));
-      gam.AddEdge(1, 2, (2, 0));
-      gam.AddEdge(1, 3, (0, 3));
-      gam.AddEdge(2, 3, (1, 0));
-      gam.AddEdge(2, 4, (6, 0));
-      gam.AddEdge(3, 4, (2, 1));
+        gam.AddVertex(0);
+        gam.AddVertex(1);
+        gam.AddVertex(2);
+        gam.AddVertex(3);
+        gam.AddVertex(4);
 
-      // 10, 1
-      //gam.AddEdge(0, 1, (3, 1));
-      //gam.AddEdge(0, 2, (4, 0));
-      //gam.AddEdge(0, 3, (5, 0));
-      //gam.AddEdge(1, 2, (2, 0));
-      //gam.AddEdge(2, 3, (4, 0));
-      //gam.AddEdge(2, 4, (1, 0));
-      //gam.AddEdge(3, 4, (10, 0));
+        // 6, 8
+        gam.AddEdge(0, 1, (3, 1));
+        gam.AddEdge(0, 2, (1, 0));
+        gam.AddEdge(0, 4, (3, 2));
+        gam.AddEdge(1, 2, (2, 0));
+        gam.AddEdge(1, 3, (0, 3));
+        gam.AddEdge(2, 3, (1, 0));
+        gam.AddEdge(2, 4, (6, 0));
+        gam.AddEdge(3, 4, (2, 1));
 
-      System.Console.WriteLine(gam.ToConsoleString());
+        // 10, 1
+        //gam.AddEdge(0, 1, (3, 1));
+        //gam.AddEdge(0, 2, (4, 0));
+        //gam.AddEdge(0, 3, (5, 0));
+        //gam.AddEdge(1, 2, (2, 0));
+        //gam.AddEdge(2, 3, (4, 0));
+        //gam.AddEdge(2, 4, (1, 0));
+        //gam.AddEdge(3, 4, (10, 0));
+
+        System.Console.WriteLine(gam.ToConsoleString());
+      }
 
       ////var mcmf = gam.GetBellmanFordMaxFlowMinCost(0, 4, o => o is null ? 0 : ((System.ValueTuple<int, int>)o).Item1, o => o is null ? 0 : ((System.ValueTuple<int, int>)o).Item2);
       ////System.Console.WriteLine($"BellmanFord Min-Cost-Max-Flow: {mcmf}");
@@ -70,62 +73,65 @@ namespace ConsoleApp
 
       return;
 
-      //var am = new Flux.DataStructures.Graphs.AdjacencyMatrix();
+      if (1 == 0)
+      {
+        var am = new Flux.DataStructures.Graphs.AdjacencyMatrix();
 
-      //am.AddVertex(0, 'a');
-      //am.AddVertex(1, 'b');
-      //am.AddVertex(2, 'c');
-      //am.AddVertex(3, 'd');
-      //am.AddVertex(4, 'e');
-      //am.AddVertex(5, 'f');
-      //am.AddVertex(6, 'g');
-      //am.AddVertex(7, 'h');
-      //am.AddVertex(8, 'i');
+        am.AddVertex(0, 'a');
+        am.AddVertex(1, 'b');
+        am.AddVertex(2, 'c');
+        am.AddVertex(3, 'd');
+        am.AddVertex(4, 'e');
+        am.AddVertex(5, 'f');
+        am.AddVertex(6, 'g');
+        am.AddVertex(7, 'h');
+        am.AddVertex(8, 'i');
 
-      //am.AddEdge(0, 1, 4);
-      //am.AddEdge(1, 0, 4);
+        am.AddEdge(0, 1, 4);
+        am.AddEdge(1, 0, 4);
 
-      //am.AddEdge(0, 7, 8);
+        am.AddEdge(0, 7, 8);
 
-      //am.AddEdge(1, 2, 8);
+        am.AddEdge(1, 2, 8);
 
-      //am.AddEdge(2, 1, 8);
-      //am.AddEdge(2, 3, 7);
-      //am.AddEdge(2, 5, 4);
-      //am.AddEdge(2, 8, 2);
+        am.AddEdge(2, 1, 8);
+        am.AddEdge(2, 3, 7);
+        am.AddEdge(2, 5, 4);
+        am.AddEdge(2, 8, 2);
 
-      //am.AddEdge(3, 2, 7);
-      //am.AddEdge(3, 5, 14);
-      //am.AddEdge(3, 3, 13);
-      //am.AddEdge(3, 4, 9);
+        am.AddEdge(3, 2, 7);
+        am.AddEdge(3, 5, 14);
+        am.AddEdge(3, 3, 13);
+        am.AddEdge(3, 4, 9);
 
-      //am.AddEdge(4, 3, 9);
-      //am.AddEdge(4, 5, 10);
+        am.AddEdge(4, 3, 9);
+        am.AddEdge(4, 5, 10);
 
-      //am.AddEdge(5, 2, 4);
-      //am.AddEdge(5, 3, 14);
-      //am.AddEdge(5, 4, 10);
-      //am.AddEdge(5, 6, 2);
+        am.AddEdge(5, 2, 4);
+        am.AddEdge(5, 3, 14);
+        am.AddEdge(5, 4, 10);
+        am.AddEdge(5, 6, 2);
 
-      //am.AddEdge(6, 5, 2);
-      //am.AddEdge(6, 7, 1);
-      //am.AddEdge(6, 8, 6);
+        am.AddEdge(6, 5, 2);
+        am.AddEdge(6, 7, 1);
+        am.AddEdge(6, 8, 6);
 
-      //am.AddEdge(7, 0, 8);
-      //am.AddEdge(7, 1, 11);
-      //am.AddEdge(7, 6, 1);
-      //am.AddEdge(7, 8, 7);
+        am.AddEdge(7, 0, 8);
+        am.AddEdge(7, 1, 11);
+        am.AddEdge(7, 6, 1);
+        am.AddEdge(7, 8, 7);
 
-      //am.AddEdge(8, 2, 2);
-      //am.AddEdge(8, 6, 6);
-      //am.AddEdge(8, 7, 7);
+        am.AddEdge(8, 2, 2);
+        am.AddEdge(8, 6, 6);
+        am.AddEdge(8, 7, 7);
 
-      //System.Console.WriteLine(am.ToConsoleString());
+        System.Console.WriteLine(am.ToConsoleString());
 
-      //System.Console.WriteLine(@"Dijkstra 'Shortest Path Tree' (a.k.a. SPT) from vertex 0 (destination, distance):");
-      //System.Console.WriteLine($"{string.Join(System.Environment.NewLine, am.GetDijkstraShortestPathTree(0, o => System.Convert.ToDouble(o)))}");
+        //System.Console.WriteLine(@"Dijkstra 'Shortest Path Tree' (a.k.a. SPT) from vertex 0 (destination, distance):");
+        //System.Console.WriteLine($"{string.Join(System.Environment.NewLine, am.GetDijkstraShortestPathTree(0, o => System.Convert.ToDouble(o)))}");
 
-      //return;
+        //return;
+      }
 
 #if ZAMPLEZ
       Flux.Zamplez.Run();
