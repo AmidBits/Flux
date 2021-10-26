b9b6663b112d28ff944a593a76257d63487bf174
Robert Hugo <robert_hugo@hotmail.com>
2021-10-25 19:07:11 -0700

    Updates.

--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -15,48 +15,33 @@ using Flux;
 
 namespace ConsoleApp
 {
-  public static class Xtensions
-  {
-    public static void Xtension()
-    {
-    }
-  }
-
-  class Program
+  public class Program
   {
     private static void TimedMain(string[] args)
     {
-      
+      if (args.Length is var argsLength && argsLength > 0) { System.Console.WriteLine($"Args ({argsLength}):{System.Environment.NewLine}{string.Join(System.Environment.NewLine, System.Linq.Enumerable.Select(args, s => $"\"{s}\""))}"); }
 
-      if (Flux.Zamplez.IsSupported) { Flux.Zamplez.Run(); return; }
+      // if (Flux.Zamplez.IsSupported) { Flux.Zamplez.Run(); return; }
+
+
     }
 
-    #region Main method
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
       var originalOutputEncoding = System.Console.OutputEncoding;
 
-      try
-      {
-        System.Console.OutputEncoding = new System.Text.UnicodeEncoding(!System.BitConverter.IsLittleEndian, false);
-        System.Console.WriteLine("Output encoding set to UTF-16");
-      }
-      catch
-      {
-        System.Console.OutputEncoding = System.Text.UnicodeEncoding.UTF8;
-        System.Console.WriteLine("Output encoding set to UTF-8");
-      }
+      try { System.Console.OutputEncoding = new System.Text.UnicodeEncoding(!System.BitConverter.IsLittleEndian, false); }
+      catch { System.Console.OutputEncoding = System.Text.UnicodeEncoding.UTF8; }
 
-      System.Console.WriteLine($"The console encoding is {System.Console.OutputEncoding.EncodingName} (code page {System.Console.OutputEncoding.CodePage})");
+      System.Console.WriteLine($"The console encoding is {System.Console.OutputEncoding.EncodingName} {System.Console.OutputEncoding.HeaderName.ToUpper()} (code page {System.Console.OutputEncoding.CodePage})");
       System.Console.WriteLine();
 
       System.Console.WriteLine(Flux.Services.Performance.Measure(() => TimedMain(args), 1));
 
-      Console.OutputEncoding = originalOutputEncoding;
+      System.Console.OutputEncoding = originalOutputEncoding;
 
       System.Console.WriteLine($"{System.Environment.NewLine}Press any key to exit...");
       System.Console.ReadKey();
     }
-    #endregion Main method
   }
 }
