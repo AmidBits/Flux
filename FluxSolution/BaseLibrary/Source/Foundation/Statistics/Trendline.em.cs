6cfec42aa19511b7939c74e70634b4b5d4ef7528
Rob <robert_hugo@hotmail.com>
2021-10-30 21:20:04 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Foundation/Statistics/Trendline.em.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/Statistics/Trendline.em.cs
@@ -2,53 +2,54 @@ using System.Linq;
 
 namespace Flux
 {
-	public static partial class ExtensionMethods
-	{
-		public static Numerics.TrendLine<double> Trend(this System.Collections.Generic.IEnumerable<double> source)
-			=> new Numerics.TrendLine<double>(source, d => d);
-	}
+  public static partial class ExtensionMethods
+  {
+    public static Numerics.TrendLine<double> Trend(this System.Collections.Generic.IEnumerable<double> source)
+      => new Numerics.TrendLine<double>(source, d => d);
+  }
 
-	namespace Numerics
-	{
-		/// <summary>Computes slope and trending data on a sequence.</summary>
-		public class TrendLine<T>
-		{
-			// http://dynamicnotions.blogspot.com/2009/05/linear-regression-in-c.html
+  namespace Numerics
+  {
+    /// <summary>Computes slope and trending data on a sequence.</summary>
+    public class TrendLine<T>
+    {
+      // http://dynamicnotions.blogspot.com/2009/05/linear-regression-in-c.html
 
-			public double AvgX { get; }
-			public double AvgY { get; }
+      public TrendLine(System.Collections.Generic.IEnumerable<T> series, System.Func<T, double> valueSelector)
+      {
+        var list = series.Select(t => valueSelector(t)).ToList();
 
-			public int Count { get; }
+        foreach (var (value, index) in list.Select((v, i) => (value: v, index: i)))
+        {
+          AvgX += index;
+          AvgY += value;
+        }
 
-			private readonly double v1, v2;
+        Count = series.Count();
 
-			public double Slope { get; }
-			public double Intercept { get; }
+        AvgX /= Count;
+        AvgY /= Count;
 
-			public TrendLine(System.Collections.Generic.IEnumerable<T> series, System.Func<T, double> valueSelector)
-			{
-				var list = series.Select(t => valueSelector(t)).ToList();
+        var v1 = 0d;
+        var v2 = 0d;
 
-				foreach (var (value, index) in list.Select((v, i) => (value: v, index: i)))
-				{
-					AvgX += index;
-					AvgY += value;
-				}
+        foreach (var (value, index) in list.Select((v, i) => (value: v, index: i)))
+        {
+          v1 += (index - AvgX) * (value - AvgY);
+          v2 += System.Math.Pow(index - AvgX, 2);
+        }
 
-				Count = series.Count();
+        Slope = v1 / v2;
+        Intercept = AvgY - Slope * AvgX;
+      }
 
-				AvgX /= Count;
-				AvgY /= Count;
+      public double AvgX { get; }
+      public double AvgY { get; }
 
-				foreach (var (value, index) in list.Select((v, i) => (value: v, index: i)))
-				{
-					v1 += (index - AvgX) * (value - AvgY);
-					v2 += System.Math.Pow(index - AvgX, 2);
-				}
+      public int Count { get; }
 
-				Slope = v1 / v2;
-				Intercept = AvgY - Slope * AvgX;
-			}
-		}
-	}
+      public double Slope { get; }
+      public double Intercept { get; }
+    }
+  }
 }
