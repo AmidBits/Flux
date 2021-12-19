281f92b13911fee1adced376458d374c6fc762fc
Rob <robert_hugo@hotmail.com>
2021-12-19 00:11:28 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Types/Memory/ReadOnlySpan/Extrema.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/Memory/ReadOnlySpan/Extrema.em.cs
@@ -3,9 +3,8 @@ namespace Flux
   public static partial class ExtensionMethods
   {
     /// <summary>Locate the index of the minimum element and the index of the maximum element of the sequence. Uses the specified comparer.</summary>
-    public static (int indexMin, int indexMax) Extrema<TSource, TKey>(this System.ReadOnlySpan<TSource> source, System.Func<TSource, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer)
+    public static (int indexMinimum, int indexMaximum) Extrema<TValue>(this System.ReadOnlySpan<TValue> source, System.Collections.Generic.IComparer<TValue> comparer)
     {
-      if (keySelector is null) throw new System.ArgumentNullException(nameof(keySelector));
       if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
 
       var indexMin = -1;
@@ -13,18 +12,43 @@ namespace Flux
 
       for (var index = source.Length - 1; index >= 0; index--)
       {
-        var key = keySelector(source[index]);
+        var key = source[index];
 
-        if (indexMin < 0 || comparer.Compare(key, keySelector(source[indexMin])) < 0)
+        if (indexMin < 0 || comparer.Compare(key, source[indexMin]) < 0)
           indexMin = index;
-        if (indexMax < 0 || comparer.Compare(key, keySelector(source[indexMax])) > 0)
+        if (indexMax < 0 || comparer.Compare(key, source[indexMax]) > 0)
           indexMax = index;
       }
 
       return (indexMin, indexMax);
     }
     /// <summary>Locate the index of the minimum element and the index of the maximum element of the sequence. Uses the default comparer.</summary>
-    public static (int indexMin, int indexMax) Extrema<TSource, TKey>(this System.ReadOnlySpan<TSource> source, System.Func<TSource, TKey> valueSelector)
-      => Extrema(source, valueSelector, System.Collections.Generic.Comparer<TKey>.Default);
+    public static (int indexMinimum, int indexMaximum) Extrema<TValue>(this System.ReadOnlySpan<TValue> source)
+      => Extrema(source, System.Collections.Generic.Comparer<TValue>.Default);
+
+    /// <summary>Locate the index of the minimum element and the index of the maximum element of the sequence. Uses the specified comparer.</summary>
+    public static (int indexMinimum, int indexMaximum) Extrema<TSource, TValue>(this System.ReadOnlySpan<TSource> source, System.Func<TSource, TValue> valueSelector, System.Collections.Generic.IComparer<TValue> comparer)
+    {
+      if (valueSelector is null) throw new System.ArgumentNullException(nameof(valueSelector));
+      if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
+
+      var indexMin = -1;
+      var indexMax = -1;
+
+      for (var index = source.Length - 1; index >= 0; index--)
+      {
+        var key = valueSelector(source[index]);
+
+        if (indexMin < 0 || comparer.Compare(key, valueSelector(source[indexMin])) < 0)
+          indexMin = index;
+        if (indexMax < 0 || comparer.Compare(key, valueSelector(source[indexMax])) > 0)
+          indexMax = index;
+      }
+
+      return (indexMin, indexMax);
+    }
+    /// <summary>Locate the index of the minimum element and the index of the maximum element of the sequence. Uses the default comparer.</summary>
+    public static (int indexMinimum, int indexMaximum) Extrema<TSource, TValue>(this System.ReadOnlySpan<TSource> source, System.Func<TSource, TValue> valueSelector)
+      => Extrema(source, valueSelector, System.Collections.Generic.Comparer<TValue>.Default);
   }
 }
--- FluxSolution/BaseLibrary/Source/Types/Memory/ReadOnlySpan/ExtremaClosestToKey.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/Memory/ReadOnlySpan/ExtremaClosestToKey.em.cs
@@ -3,13 +3,39 @@ namespace Flux
   public static partial class ExtensionMethods
   {
     /// <summary>Locate the index of the max element that is less than and the index of the min element that is greater than, the specified reference value identified by the <paramref name="valueSelector"/>. Uses the specified comparer.</summary>
-    public static (int indexLtKey, int indexGtKey) ExtremaClosestTo<TSource, TValue>(this System.ReadOnlySpan<TSource> source, TValue referenceValue, System.Func<TSource, TValue> valueSelector, System.Collections.Generic.IComparer<TValue> comparer)
+    public static (int indexLessThan, int indexGreaterThan) ExtremaClosestTo<TValue>(this System.ReadOnlySpan<TValue> source, TValue referenceValue, System.Collections.Generic.IComparer<TValue> comparer)
+    {
+      if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
+
+      var indexLt = -1;
+      var indexGt = -1;
+
+      for (var index = source.Length - 1; index >= 0; index--)
+      {
+        var value = source[index];
+
+        var cmp = comparer.Compare(value, referenceValue);
+
+        if (cmp < 0 && (indexLt < 0 || comparer.Compare(value, source[indexLt]) > 0))
+          indexLt = index;
+        if (cmp > 0 && (indexGt < 0 || comparer.Compare(value, source[indexGt]) < 0))
+          indexGt = index;
+      }
+
+      return (indexLt, indexGt);
+    }
+    /// <summary>Locate the index of the max element that is less than and the index of the min element that is greater than, the specified reference value identified by the <paramref name="valueSelector"/>. Uses the default comparer.</summary>
+    public static (int indexLessThan, int indexGreaterThan) ExtremaClosestTo<TValue>(this System.ReadOnlySpan<TValue> source, TValue referenceValue)
+      => ExtremaClosestTo(source, referenceValue, System.Collections.Generic.Comparer<TValue>.Default);
+
+    /// <summary>Locate the index of the max element that is less than and the index of the min element that is greater than, the specified reference value identified by the <paramref name="valueSelector"/>. Uses the specified comparer.</summary>
+    public static (int indexLessThan, int indexGreaterThan) ExtremaClosestTo<TSource, TValue>(this System.ReadOnlySpan<TSource> source, TValue referenceValue, System.Func<TSource, TValue> valueSelector, System.Collections.Generic.IComparer<TValue> comparer)
     {
       if (valueSelector is null) throw new System.ArgumentNullException(nameof(valueSelector));
       if (comparer is null) throw new System.ArgumentNullException(nameof(comparer));
 
-      var indexLtValue = -1;
-      var indexGtValue = -1;
+      var indexLt = -1;
+      var indexGt = -1;
 
       for (var index = source.Length - 1; index >= 0; index--)
       {
@@ -17,16 +43,16 @@ namespace Flux
 
         var cmp = comparer.Compare(value, referenceValue);
 
-        if (cmp < 0 && (indexLtValue < 0 || comparer.Compare(value, valueSelector(source[indexLtValue])) > 0))
-          indexLtValue = index;
-        if (cmp > 0 && (indexGtValue < 0 || comparer.Compare(value, valueSelector(source[indexGtValue])) < 0))
-          indexGtValue = index;
+        if (cmp < 0 && (indexLt < 0 || comparer.Compare(value, valueSelector(source[indexLt])) > 0))
+          indexLt = index;
+        if (cmp > 0 && (indexGt < 0 || comparer.Compare(value, valueSelector(source[indexGt])) < 0))
+          indexGt = index;
       }
 
-      return (indexLtValue, indexGtValue);
+      return (indexLt, indexGt);
     }
     /// <summary>Locate the index of the max element that is less than and the index of the min element that is greater than, the specified reference value identified by the <paramref name="valueSelector"/>. Uses the default comparer.</summary>
-    public static (int indexLtKey, int indexGtKey) ExtremaClosestTo<TSource, TValue>(this System.ReadOnlySpan<TSource> source, TValue referenceValue, System.Func<TSource, TValue> valueSelector)
+    public static (int indexLessThan, int indexGreaterThan) ExtremaClosestTo<TSource, TValue>(this System.ReadOnlySpan<TSource> source, TValue referenceValue, System.Func<TSource, TValue> valueSelector)
       => ExtremaClosestTo(source, referenceValue, valueSelector, System.Collections.Generic.Comparer<TValue>.Default);
   }
 }
