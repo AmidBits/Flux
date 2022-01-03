c08b252b087d0037c2fdb1705f2d372c34bd4949
Rob <robert_hugo@hotmail.com>
2022-01-02 20:25:42 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Foundation/Sequences/Evaluation/Compare/FuzzyCompare.em.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/Sequences/Evaluation/Compare/FuzzyCompare.em.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using System.Reflection;
 
 namespace Flux
 {
@@ -14,15 +13,9 @@ namespace Flux
 
       typeof(Flux.Metrical.ISimpleMatchingCoefficient<>).GetDerivedTypes().AsParallel().ForAll(type =>
       {
-        //try
-        //{
-        Flux.Metrical.ISimpleMatchingCoefficient<T>? instance = (Flux.Metrical.ISimpleMatchingCoefficient<T>?)type.CreateGenericInstance(typeof(char));
+        var instance = (Flux.Metrical.ISimpleMatchingCoefficient<T>?)type.CreateGenericInstance(typeof(char));
 
-        var smc = instance?.GetSimpleMatchingCoefficient(sarray, tarray) ?? 0;
-
-        scores.Add(type.Name.Replace("`1", string.Empty), smc);
-        //}
-        //catch { }
+        scores.Add(type.Name.Replace("`1", string.Empty), instance?.GetSimpleMatchingCoefficient(sarray, tarray) ?? 0);
       });
 
       return scores;
