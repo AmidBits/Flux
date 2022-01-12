7ee4843fc950a79295fe738f8abde5fe49b7e7e8
Rob <robert_hugo@hotmail.com>
2022-01-11 23:30:17 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Foundation/Quantities/Other/Fraction.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/Quantities/Other/Fraction.cs
@@ -1,34 +1,34 @@
 namespace Flux
 {
-  /// <summary>Simple fraction.</summary>
+  /// <summary>Simple fraction, unit of rational number, i.e. in the form of numerator and denominator.</summary>
   /// <see cref="https://en.wikipedia.org/wiki/Fraction#Simple,_common,_or_vulgar_fractions"/>
-  public struct SimpleFraction
-    : System.IComparable<SimpleFraction>, System.IEquatable<SimpleFraction>, IValueGeneralizedUnit<double>
+  public struct Fraction
+    : System.IComparable<Fraction>, System.IEquatable<Fraction>, IValueGeneralizedUnit<double>
   {
     public const char FractionSlash = '\u2044';
 
-    public static readonly SimpleFraction EpsilonLikeSingle = new(1, 1000000);
-    public static readonly SimpleFraction EpsilonLikeDouble = new(1, 1000000000000000);
+    public static readonly Fraction EpsilonLikeSingle = new(1, 1000000);
+    public static readonly Fraction EpsilonLikeDouble = new(1, 1000000000000000);
 
     /// <summary>Represents a BigFraction value of the Golden Ratio.</summary>
-    public static readonly SimpleFraction GoldenRatio = new(7540113804746346429L, 4660046610375530309L, true);
+    public static readonly Fraction GoldenRatio = new(7540113804746346429L, 4660046610375530309L, true);
     /// <summary>Represents a BigFraction value of PI.</summary>
-    public static readonly SimpleFraction PI = new(2646693125139304345L, 842468587426513207L, true);
+    public static readonly Fraction PI = new(2646693125139304345L, 842468587426513207L, true);
 
     /// <summary>Represents a SimpleFraction value of -1.</summary>
-    public static SimpleFraction MinusOne
+    public static Fraction MinusOne
       => new(-1, 1, false);
     /// <summary>Represents a SimpleFraction value of -1.</summary>
-    public static SimpleFraction MinusTwo
+    public static Fraction MinusTwo
       => new(-2, 1, false);
     /// <summary>Represents a SimpleFraction value of 1.</summary>
-    public static SimpleFraction One
+    public static Fraction One
       => new(1, 1, false);
     /// <summary>Represents a SimpleFraction value of 2.</summary>
-    public static SimpleFraction Two
+    public static Fraction Two
       => new(2, 1, false);
     /// <summary>Represents a SimpleFraction value of 0.</summary>
-    public static SimpleFraction Zero
+    public static Fraction Zero
       => new(0, 1, false);
 
     private readonly System.Numerics.BigInteger m_numerator;
@@ -38,7 +38,7 @@ namespace Flux
     /// <param name="numerator"></param>
     /// <param name="denominator"></param>
     /// <param name="reduceIfPossible">If true, reduce if possible, and if false, do not attempt to reduce.</param>
-    public SimpleFraction(System.Numerics.BigInteger numerator, System.Numerics.BigInteger denominator, bool reduceIfPossible)
+    public Fraction(System.Numerics.BigInteger numerator, System.Numerics.BigInteger denominator, bool reduceIfPossible)
     {
       if (reduceIfPossible && IsReducible(numerator, denominator, out var gcd))
       {
@@ -53,16 +53,16 @@ namespace Flux
 
       if (m_denominator == 0) throw new System.ArithmeticException(@"The denominator cannot be zero.");
     }
-    public SimpleFraction(System.Numerics.BigInteger whole, System.Numerics.BigInteger numerator, System.Numerics.BigInteger denominator)
+    public Fraction(System.Numerics.BigInteger whole, System.Numerics.BigInteger numerator, System.Numerics.BigInteger denominator)
       : this(whole * denominator + numerator, denominator, true)
     { }
     /// <summary>Creates a new simple fraction from the specified numerator and denominator. If the fraction can be reduced, it will be.</summary>
     /// <param name="numerator"></param>
     /// <param name="denominator"></param>
-    public SimpleFraction(System.Numerics.BigInteger numerator, System.Numerics.BigInteger denominator)
+    public Fraction(System.Numerics.BigInteger numerator, System.Numerics.BigInteger denominator)
       : this(numerator, denominator, true)
     { }
-    public SimpleFraction(System.Numerics.BigInteger value)
+    public Fraction(System.Numerics.BigInteger value)
       : this(value, 1, false)
     { }
 
@@ -123,10 +123,10 @@ namespace Flux
 
     #region Static methods
     /// <summary>Returns the absolute value a value.</summary>
-    public static SimpleFraction Abs(SimpleFraction value)
+    public static Fraction Abs(Fraction value)
       => CopySign(value, One);
     /// <summary>Returns the value with the sign of the second argument.</summary>
-    public static SimpleFraction CopySign(SimpleFraction value, SimpleFraction sign)
+    public static Fraction CopySign(Fraction value, Fraction sign)
       => (sign.Sign == 0 || value == Zero)
       ? Zero
       : (value.m_numerator.Sign < 0 && sign.Sign > 0) || (value.m_numerator.Sign > 0 && sign.Sign < 0)
@@ -135,7 +135,7 @@ namespace Flux
     /// <summary>Returns the greatest common divisor (GCD) of two values.</summary>
     /// <remarks>The result is guaranteed to be a reduced fraction. If you try to further simplify this to: (gcd(a,c) * gcd(b,d)) / (|b*d|), then the result will not be reduced, and the operation actually takes about 60% longer.</remarks>
     /// <example>gcd((a/b),(c/d)) = gcd(a,c) / lcm(b,d) = gcd(a,c) / (|b*d|/gcd(b,d))</example>
-    public static SimpleFraction GreatestCommonDivisor(SimpleFraction a, SimpleFraction b)
+    public static Fraction GreatestCommonDivisor(Fraction a, Fraction b)
       => (a == Zero)
       ? Abs(b)
       : (b == Zero)
@@ -151,7 +151,7 @@ namespace Flux
     /// <summary>Returns the least common multiple (LCM) of two values.</summary>
     /// <remarks>The result is guaranteed to be a reduced fraction. If you try to further simplify this to: |a*c| / (gcd(a,c) * gcd(b,d)), then the result will not be reduced, and the operation actually takes about 60% longer.</remarks>
     /// <example>lcm((a/b),(c/d)) = lcm(a,c) / gcd(b,d) = (|a*c| / gcd(a,c)) / gcd(b,d)</example>
-    public static SimpleFraction LeastCommonMultiple(SimpleFraction a, SimpleFraction b)
+    public static Fraction LeastCommonMultiple(Fraction a, Fraction b)
       => (a == Zero || b == Zero)
       ? Zero
       : new(
@@ -160,16 +160,16 @@ namespace Flux
           true
         );
     /// <summary>Returns the greater of two values.</summary>
-    public static SimpleFraction Max(SimpleFraction a, SimpleFraction b)
+    public static Fraction Max(Fraction a, Fraction b)
       => a.CompareTo(b) >= 0 ? a : b;
     /// <summary>Returns the mediant of two values.</summary>
-    public static SimpleFraction Mediant(SimpleFraction a, SimpleFraction b)
+    public static Fraction Mediant(Fraction a, Fraction b)
       => new(a.m_numerator + b.m_numerator, a.m_denominator + b.m_denominator, false);
     /// <summary>Returns the lesser of two values.</summary>
-    public static SimpleFraction Min(SimpleFraction a, SimpleFraction b)
+    public static Fraction Min(Fraction a, Fraction b)
       => a.CompareTo(b) <= 0 ? a : b;
     /// <summary>Returns the nth root of the value.</summary>
-    private static SimpleFraction NthRoot(SimpleFraction value, int n, SimpleFraction maxError)
+    private static Fraction NthRoot(Fraction value, int n, Fraction maxError)
     {
       if (n < 0)
         return NthRoot(Reciprocal(value), -n, maxError);
@@ -199,7 +199,7 @@ namespace Flux
       var guessNumerator = IntegerNthRoot(value.Numerator, n);
       var guessDenominator = IntegerNthRoot(value.Denominator, n);
 
-      var initialGuess = new SimpleFraction(guessNumerator.value, guessDenominator.value);
+      var initialGuess = new Fraction(guessNumerator.value, guessDenominator.value);
 
       // If we got exact roots for numerator and denominator, then we know the guess is exact.
 
@@ -283,7 +283,7 @@ namespace Flux
       }
     }
     /// <summary>Returns this^exponent. Note: 0^0 will return 1/1.</summary>
-    public static SimpleFraction Pow(SimpleFraction value, int exponent)
+    public static Fraction Pow(Fraction value, int exponent)
     {
       if (exponent < 0)
       {
@@ -298,72 +298,72 @@ namespace Flux
       else return new(System.Numerics.BigInteger.Pow(value.Numerator, exponent), System.Numerics.BigInteger.Pow(value.Denominator, exponent), true);
     }
     /// <summary>Returns the reciprocal of a value.</summary>
-    public static SimpleFraction Reciprocal(SimpleFraction value)
+    public static Fraction Reciprocal(Fraction value)
       => value == Zero
       ? throw new System.DivideByZeroException(@"Reciprocal of zero.")
       : new(value.m_denominator, value.m_numerator, true);
     /// <summary>Compute the square root of the specified value.</summary>
-    public static SimpleFraction Sqrt(SimpleFraction value)
+    public static Fraction Sqrt(Fraction value)
       => NthRoot(value, 2, EpsilonLikeDouble);
     #endregion Static methods
 
     #region Overloaded operators
-    public static explicit operator double(SimpleFraction v)
+    public static explicit operator double(Fraction v)
       => v.Value;
 
-    public static bool operator <(SimpleFraction a, SimpleFraction b)
+    public static bool operator <(Fraction a, Fraction b)
       => a.CompareTo(b) < 0;
-    public static bool operator <=(SimpleFraction a, SimpleFraction b)
+    public static bool operator <=(Fraction a, Fraction b)
       => a.CompareTo(b) <= 0;
-    public static bool operator >(SimpleFraction a, SimpleFraction b)
+    public static bool operator >(Fraction a, Fraction b)
       => a.CompareTo(b) > 0;
-    public static bool operator >=(SimpleFraction a, SimpleFraction b)
+    public static bool operator >=(Fraction a, Fraction b)
       => a.CompareTo(b) >= 0;
 
-    public static bool operator ==(SimpleFraction a, SimpleFraction b)
+    public static bool operator ==(Fraction a, Fraction b)
       => a.Equals(b);
-    public static bool operator !=(SimpleFraction a, SimpleFraction b)
+    public static bool operator !=(Fraction a, Fraction b)
       => !a.Equals(b);
 
-    public static SimpleFraction operator -(SimpleFraction f)
+    public static Fraction operator -(Fraction f)
       => new(f.m_numerator, -f.m_denominator, false);
-    public static SimpleFraction operator +(SimpleFraction a, SimpleFraction b)
+    public static Fraction operator +(Fraction a, Fraction b)
     {
       var lcm = Maths.LeastCommonMultiple(a.m_denominator, b.m_denominator);
 
       var an = lcm / a.m_denominator * a.m_numerator;
       var bn = lcm / b.m_denominator * b.m_numerator;
 
-      return new SimpleFraction(an + bn, lcm);
+      return new Fraction(an + bn, lcm);
     }
-    public static SimpleFraction operator +(SimpleFraction a, System.Numerics.BigInteger b)
-      => a + new SimpleFraction(b);
-    public static SimpleFraction operator /(SimpleFraction a, SimpleFraction b)
+    public static Fraction operator +(Fraction a, System.Numerics.BigInteger b)
+      => a + new Fraction(b);
+    public static Fraction operator /(Fraction a, Fraction b)
       => new(a.m_numerator * b.m_denominator, a.m_denominator * b.m_numerator);
-    public static SimpleFraction operator /(SimpleFraction a, System.Numerics.BigInteger b)
-      => a / new SimpleFraction(b);
-    public static SimpleFraction operator *(SimpleFraction a, SimpleFraction b)
+    public static Fraction operator /(Fraction a, System.Numerics.BigInteger b)
+      => a / new Fraction(b);
+    public static Fraction operator *(Fraction a, Fraction b)
       => new(a.m_numerator * b.m_numerator, a.m_denominator * b.m_denominator);
-    public static SimpleFraction operator *(SimpleFraction a, System.Numerics.BigInteger b)
-      => a * new SimpleFraction(b);
-    public static SimpleFraction operator %(SimpleFraction a, System.Numerics.BigInteger b)
+    public static Fraction operator *(Fraction a, System.Numerics.BigInteger b)
+      => a * new Fraction(b);
+    public static Fraction operator %(Fraction a, System.Numerics.BigInteger b)
       => new(a.m_numerator % (a.m_denominator * b), a.m_denominator);
-    public static SimpleFraction operator -(SimpleFraction a, SimpleFraction b)
+    public static Fraction operator -(Fraction a, Fraction b)
     {
       var lcm = Maths.LeastCommonMultiple(a.m_denominator, b.m_denominator);
 
       var an = lcm / a.m_denominator * a.m_numerator;
       var bn = lcm / b.m_denominator * b.m_numerator;
 
-      return new SimpleFraction(an - bn, lcm);
+      return new Fraction(an - bn, lcm);
     }
-    public static SimpleFraction operator -(SimpleFraction a, System.Numerics.BigInteger b)
-      => a - new SimpleFraction(b);
+    public static Fraction operator -(Fraction a, System.Numerics.BigInteger b)
+      => a - new Fraction(b);
     #endregion Overloaded operators
 
     #region Implemented interfaces
     // IComparable
-    public int CompareTo(SimpleFraction other)
+    public int CompareTo(Fraction other)
     {
       if (Sign is var sign && other.Sign is var otherSign && sign != otherSign)
         return sign - otherSign;
@@ -375,13 +375,13 @@ namespace Flux
     }
 
     // IEquatable
-    public bool Equals(SimpleFraction other)
+    public bool Equals(Fraction other)
       => m_numerator == other.m_numerator && m_denominator == other.m_denominator;
     #endregion Implemented interfaces
 
     #region Object overrides
     public override bool Equals(object? obj)
-      => obj is SimpleFraction o && Equals(o);
+      => obj is Fraction o && Equals(o);
     public override int GetHashCode()
       => System.HashCode.Combine(m_numerator, m_denominator);
     public override string ToString()
