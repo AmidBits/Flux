bbca2c91108408a944bb320601131e53a01661ca
Rob <robert_hugo@hotmail.com>
2022-01-19 11:55:59 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Types/TimeSpan/ToStringOf.em.cs
+++ FluxSolution/BaseLibrary/Source/Types/TimeSpan/ToStringOf.em.cs
@@ -6,13 +6,13 @@ namespace Flux
   {
     public static string ToStringOf(this System.TimeSpan source)
       => string.Join(' ', System.Array.Empty<string>().Append(ToStringOfDays(source), ToStringOfHours(source), ToStringOfMinutes(source), ToStringOfSeconds(source)).Where(s => s.Length > 0)).Trim();
-    public static string ToStringOfDays(this System.TimeSpan source, string symbol = @"d")
+    public static string ToStringOfDays(this System.TimeSpan source, string symbol = "d")
       => source.Days > 0 ? $"{source.Days}{symbol}" : string.Empty;
-    public static string ToStringOfHours(this System.TimeSpan source, string symbol = @"h")
+    public static string ToStringOfHours(this System.TimeSpan source, string symbol = "h")
       => source.Hours > 0 ? $"{source.Hours}{symbol}" : string.Empty;
-    public static string ToStringOfMinutes(this System.TimeSpan source, string symbol = @"min")
+    public static string ToStringOfMinutes(this System.TimeSpan source, string symbol = "min")
       => source.Minutes > 0 ? $"{source.Minutes}{symbol}" : string.Empty;
-    public static string ToStringOfSeconds(this System.TimeSpan source, string symbol = @"s")
+    public static string ToStringOfSeconds(this System.TimeSpan source, string symbol = "s")
       => source.Seconds > 0 ? $"{source.Seconds}{symbol}" : string.Empty;
   }
 }
