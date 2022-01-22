c9ba320accdd0ab985883e497450a266148e76fa
Rob <robert_hugo@hotmail.com>
2022-01-21 22:49:02 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Types/Memory/Span/ToStringBuilder.em.cs
+++
@@ -1,9 +0,0 @@
-namespace Flux
-{
-  public static partial class ExtensionMethods
-  {
-    /// <summary>Creates a string builder from the source.</summary>
-    public static System.Text.StringBuilder ToStringBuilder(this System.Span<char> source)
-      => new System.Text.StringBuilder().Append(source);
-  }
-}
--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -12,78 +12,6 @@ using Flux;
 
 namespace ConsoleApp
 {
-
-  public enum Colour { Red, Green, White, Yellow, Blue }
-  public enum Nationality { Englishman, Swede, Dane, Norwegian, German }
-  public enum Pet { Dog, Birds, Cats, Horse, Zebra }
-  public enum Drink { Coffee, Tea, Milk, Beer, Water }
-  public enum Smoke { PallMall, Dunhill, Blend, BlueMaster, Prince }
-
-  public static class ZebraPuzzle
-  {
-    private static (Colour[] colours, Drink[] drinks, Smoke[] smokes, Pet[] pets, Nationality[] nations) _solved;
-
-    static ZebraPuzzle()
-    {
-      var solve = from colours in Permute<Colour>()  //r1 5 range
-                  where (colours, Colour.White).IsRightOf(colours, Colour.Green) // r5
-                  from nations in Permute<Nationality>()
-                  where nations[0] == Nationality.Norwegian // r10
-                  where (nations, Nationality.Englishman).IsSameIndex(colours, Colour.Red) //r2
-                  where (nations, Nationality.Norwegian).IsNextTo(colours, Colour.Blue) // r15
-                  from drinks in Permute<Drink>()
-                  where drinks[2] == Drink.Milk //r9
-                  where (drinks, Drink.Coffee).IsSameIndex(colours, Colour.Green) // r6
-                  where (drinks, Drink.Tea).IsSameIndex(nations, Nationality.Dane) //r4
-                  from pets in Permute<Pet>()
-                  where (pets, Pet.Dog).IsSameIndex(nations, Nationality.Swede) // r3
-                  from smokes in Permute<Smoke>()
-                  where (smokes, Smoke.PallMall).IsSameIndex(pets, Pet.Birds) // r7
-                  where (smokes, Smoke.Dunhill).IsSameIndex(colours, Colour.Yellow) // r8
-                  where (smokes, Smoke.Blend).IsNextTo(pets, Pet.Cats) // r11
-                  where (smokes, Smoke.Dunhill).IsNextTo(pets, Pet.Horse) //r12
-                  where (smokes, Smoke.BlueMaster).IsSameIndex(drinks, Drink.Beer) //r13
-                  where (smokes, Smoke.Prince).IsSameIndex(nations, Nationality.German) // r14
-                  where (drinks, Drink.Water).IsNextTo(smokes, Smoke.Blend) // r16
-                  select (colours, drinks, smokes, pets, nations);
-
-      _solved = solve.First();
-    }
-
-    private static int IndexOf<T>(this T[] arr, T obj) => Array.IndexOf(arr, obj);
-
-    private static bool IsRightOf<T, U>(this (T[] a, T v) right, U[] a, U v) => right.a.IndexOf(right.v) == a.IndexOf(v) + 1;
-
-    private static bool IsSameIndex<T, U>(this (T[] a, T v) x, U[] a, U v) => x.a.IndexOf(x.v) == a.IndexOf(v);
-
-    private static bool IsNextTo<T, U>(this (T[] a, T v) x, U[] a, U v) => (x.a, x.v).IsRightOf(a, v) || (a, v).IsRightOf(x.a, x.v);
-
-    // made more generic from https://codereview.stackexchange.com/questions/91808/permutations-in-c
-    public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> values)
-    {
-      if (values.Count() == 1)
-        return values.ToSingleton();
-
-      return values.SelectMany(v => Permutations(values.Except(v.ToSingleton())), (v, p) => p.Prepend(v));
-    }
-
-    public static IEnumerable<T[]> Permute<T>() => ToEnumerable<T>().Permutations().Select(p => p.ToArray());
-
-    private static IEnumerable<T> ToSingleton<T>(this T item) { yield return item; }
-
-    private static IEnumerable<T> ToEnumerable<T>() => Enum.GetValues(typeof(T)).Cast<T>();
-
-    public static new String ToString()
-    {
-      var sb = new StringBuilder();
-      sb.AppendLine("House Colour Drink    Nationality Smokes     Pet");
-      sb.AppendLine("───── ────── ──────── ─────────── ────────── ─────");
-      var (colours, drinks, smokes, pets, nations) = _solved;
-      for (var i = 0; i < 5; i++)
-        sb.AppendLine($"{i + 1,5} {colours[i],-6} {drinks[i],-8} {nations[i],-11} {smokes[i],-10} {pets[i],-10}");
-      return sb.ToString();
-    }
-  }
   public class Program
   {
     private static void TimedMain(string[] args)
@@ -91,54 +19,13 @@ namespace ConsoleApp
       //if (args.Length is var argsLength && argsLength > 0) System.Console.WriteLine($"Args ({argsLength}):{System.Environment.NewLine}{string.Join(System.Environment.NewLine, System.Linq.Enumerable.Select(args, s => $"\"{s}\""))}");
       //if (Flux.Zamplez.IsSupported) { Flux.Zamplez.Run(); return; }
 
-      var x = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-      System.Console.WriteLine(x.ToConsoleBlock());
-      System.Console.WriteLine();
-      var y = x.RotateClockwise();
-      System.Console.WriteLine(y.ToConsoleBlock());
-      System.Console.WriteLine();
-
-      var a = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-      var b = a.Insert(10, 11, 12, 12);
-
-      //Flux.ArrayRank1.Remove(ref a, 6, 3);
-      //var b = a.Remove(7, 3);
-
-      var span = "robertHugo".EnumerateRunes().ToList().ToReadOnlySpan();
-
-      var rb = new Flux.DataStructures.RuneBuilder();
-      //rb.Append('*');
-      //rb.Prepend(Angle.PrimeSymbol);
-      rb.Append("'Snobben' ");
-      rb.Prepend((System.Text.Rune)' ');
-      rb.Append((System.Text.Rune)'H');
-      rb.Append((System.Text.Rune)'u');
-      rb.Append((System.Text.Rune)'g');
-      rb.Append((System.Text.Rune)'o');
-      rb.Append((System.Text.Rune)0x1D2E1);
-      rb.Prepend((System.Text.Rune)'t');
-      rb.Prepend((System.Text.Rune)'r');
-      rb.Prepend((System.Text.Rune)'e');
-      rb.Prepend((System.Text.Rune)'b');
-      rb.Prepend((System.Text.Rune)'o');
-      rb.Prepend((System.Text.Rune)'R');
-      rb.Prepend((System.Text.Rune)' ');
-      rb.Prepend((System.Text.Rune)'o');
-      rb.Prepend((System.Text.Rune)'T');
-      rb.Prepend("Congratulations ");
-      rb.Append(", to retirement.");
-
-      System.Console.WriteLine($"\"{rb.ToString(0, rb.Count)}\", {rb.Count}/{rb.GetUtf16SequenceLength()}/{rb.GetUtf8SequenceLength()}");
-      return;
-
-      var s1 = "lowerCase2UpperSLOWCase";
-      System.Console.WriteLine(s1);
-      var s2 = s1.ToStringBuilder().SplitCamelCase().ToLowerFirstCharacters().ToString();
-      System.Console.WriteLine(s2);
-      var s3 = s2.ToStringBuilder().MakeCamelCase(char.IsWhiteSpace).ToString();
-      System.Console.WriteLine(s3);
-
+      for (var index = 0; index < 100; index++)
+      {
+        var s1 = new System.Span<char>("robert hugo".ToCharArray());
+        s1.Shuffle();
+        System.Console.WriteLine(s1.ToString());
+        //System.Console.ReadKey();
+      }
       return;
 
       //Flux.ConstraintPropagationSolver.Example();
