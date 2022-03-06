8c5313c7432a293676e886b81d04bf976f5803c9
Rob <robert_hugo@hotmail.com>
2022-03-06 08:52:21 -0700

    Updates.

--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -17,38 +17,6 @@ namespace ConsoleApp
       //if (args.Length is var argsLength && argsLength > 0) System.Console.WriteLine($"Args ({argsLength}):{System.Environment.NewLine}{string.Join(System.Environment.NewLine, System.Linq.Enumerable.Select(args, s => $"\"{s}\""))}");
       //if (Flux.Zamplez.IsSupported) { Flux.Zamplez.Run(); return; }
 
-      var ba = new Flux.BitArray(4097);
-      //ba.SetBit(32, true);
-      //ba.SetByte(4, 1);
-      //ba.SetUInt16(2, 1);
-      //ba.SetUInt32(1, 1);
-      //ba.SetUInt64(0, 1UL << 32);
-      ba[1] = true;
-      ba[8] = true;
-      ba[32] = true;
-      ba[33] = true;
-      ba[34] = true;
-      ba[35] = true;
-      ba[36] = true;
-      ba[37] = true;
-      ba[38] = true;
-      ba[39] = true;
-      ba[1027] = true;
-      //ba.SetInt64(0, -1);
-      //ba.SetByte(1, 5);
-      var i64 = ba.GetUInt64(0);
-      var i32 = ba.GetUInt32(1);
-      var i16 = ba.GetUInt16(2);
-      var i8 = ba.GetByte(4);
-      var i1 = ba.GetBit(32);
-
-      var bi = ba.ToBigInteger();
-      var bl = bi.GetBitLength();
-      var bia = bi.ToByteArray();
-      
-      var s = ba.ToBinaryString();
-
-      System.Console.WriteLine(s);
     }
 
     private static void Main(string[] args)
