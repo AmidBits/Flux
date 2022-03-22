f49b71af137b7608bfb78f96a0348bb9db4e7169
Robert Hugo <Robert.Hugo@pima.gov>
2022-03-22 15:36:07 -0700

    Updates.

--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -15,23 +15,8 @@ namespace ConsoleApp
     private static void TimedMain(string[] args)
     {
       //if (args.Length is var argsLength && argsLength > 0) System.Console.WriteLine($"Args ({argsLength}):{System.Environment.NewLine}{string.Join(System.Environment.NewLine, System.Linq.Enumerable.Select(args, s => $"\"{s}\""))}");
-      if (Flux.Zamplez.IsSupported) { Flux.Zamplez.Run(); return; }
+      //if (Flux.Zamplez.IsSupported) { Flux.Zamplez.Run(); return; }
 
-      var source = "Robert";
-      System.Console.WriteLine($"source: \"{source}\"");
-      var target = "Rupert";
-      System.Console.WriteLine($"target: \"{target}\"");
-
-      var metric = new Flux.Metrical.OptimalStringAlignment<char>();
-      var matrix = metric.GetMatrix(source, target);
-      var btrack = metric.Backtrack(matrix, source, target, source.Length, target.Length, '-');
-      var s = matrix.ToConsoleStrings();
-
-      var (left, top) = System.Console.GetCursorPosition();
-      var max1 = s.WriteToConsole(left, top);
-      var max2 = s.WriteToConsole(max1.maxLeft + 3, top);
-
-      System.Console.WriteLine($"btrack: \"{string.Concat(btrack)}\"");
     }
 
     private static void Main(string[] args)
