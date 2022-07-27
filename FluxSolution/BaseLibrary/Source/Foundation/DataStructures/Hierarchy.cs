ef744be7b8ec3da2268fa5897e2b3a212de49a2c
Robert Hugo <robert_hugo@hotmail.com>
2022-07-26 18:59:10 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Foundation/DataStructures/Hierarchy.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/DataStructures/Hierarchy.cs
@@ -1,4 +1,4 @@
-namespace Flux2
+namespace Flux
 {
   // https://www.siepman.nl/blog/a-generic-tree-of-nodes-the-easy-way#:~:text=%2F%2F%20With%20just%20one%20line%20of%20code%21%21%20var,one%20tree%2C%20otherwise%20there%20would%20be%20more%20rootnodes
   #region Extension methods.
@@ -7,7 +7,7 @@ namespace Flux2
     public static System.Collections.Generic.IEnumerable<TValue> Values<TKey, TValue>(this System.Collections.Generic.IEnumerable<IHierarchy<TKey, TValue>> source)
       => source.Select(n => n.Value);
 
-    public static System.Collections.Generic.IEnumerable<TSource> Duplicates<TKey, TValue>(this System.Collections.Generic.IEnumerable<IHierarchy<TKey, TValue>> source, System.Func<IHierarchy<TKey, TValue>, TKey> selector)
+    public static System.Collections.Generic.IEnumerable<IHierarchy<TKey, TValue>> Duplicates<TKey, TValue>(this System.Collections.Generic.IEnumerable<IHierarchy<TKey, TValue>> source, System.Func<IHierarchy<TKey, TValue>, TKey> selector)
       => source.GroupBy(selector).Where(i => i.IsMultiple()).SelectMany(i => i);
 
     public static bool IsMultiple<T>(this System.Collections.Generic.IEnumerable<T> source)
@@ -66,20 +66,30 @@ namespace Flux2
 
   public sealed class Hierarchy<TKey, TValue>
     : IHierarchy<TKey, TValue>
-  //: System.IEquatable<Hierarchy<T>>//, System.Collections.Generic.IEnumerable<Hierarchy<T>>, System.Collections.Generic.IEnumerable<T>
+    where TKey : struct
+    where TValue : struct
+
+    //: System.IEquatable<Hierarchy<T>>//, System.Collections.Generic.IEnumerable<Hierarchy<T>>, System.Collections.Generic.IEnumerable<T>
   {
     public static readonly IHierarchy<TKey, TValue> Empty = new EmptyHierarchy();
 
-    private System.Collections.Generic.List<Hierarchy<TKey, TValue>> m_children = new System.Collections.Generic.List<Hierarchy<TKey, TValue>>();
-    private TKey m_key;
-    private TValue m_value;
-    private IHierarchy<TKey, TValue> m_parent;
+    private readonly System.Collections.Generic.List<Hierarchy<TKey, TValue>> m_children;
+    private readonly TKey m_key;
+    private readonly TValue m_value;
+    private readonly IHierarchy<TKey, TValue> m_parent;
 
     public Hierarchy(TKey key, TValue value, IHierarchy<TKey, TValue> parent)
     {
+      if (parent is null) throw new System.ArgumentNullException(nameof(parent));
+
+      m_children = new System.Collections.Generic.List<Hierarchy<TKey, TValue>>();
       m_key = key;
       m_value = value;
       m_parent = parent;
+    }
+    public Hierarchy(TKey key, TValue value)
+      : this(key, value, Empty)
+    {
     }
 
     public bool IsEmpty
@@ -222,55 +232,53 @@ namespace Flux2
     //public override string ToString()
     //  => $"{Value}";
 
-    public static IEnumerable<Hierarchy<T>> CreateTree<TId>(IEnumerable<T> values, Func<T, TId> idSelector, Func<T, TId?> parentIdSelector)
-        where TId : struct
-    {
-      var valuesCache = values.ToList();
-      if (!valuesCache.Any())
-        return System.Linq.Enumerable.Empty<Hierarchy<T>>();
-      T itemWithIdAndParentIdIsTheSame = valuesCache.FirstOrDefault(v => IsSameId(idSelector(v), parentIdSelector(v)));
-      if (itemWithIdAndParentIdIsTheSame != null) // Hier verwacht je ook een null terug te kunnen komen
-      {
-        throw new ArgumentException("At least one value has the samen Id and parentId [{0}]");
-      }
+    //public static IEnumerable<IHierarchy<TKey,TValue>> CreateTree<TId>(IEnumerable<IHierarchy<TKey, TValue>> values, Func<IHierarchy<TKey, TValue>, TId> idSelector, Func<IHierarchy<TKey, TValue>, TId?> parentIdSelector)
+    //  where TId : struct
+    //{
+    //  var valuesCache = values.ToList();
+    //  if (!valuesCache.Any())
+    //    return System.Linq.Enumerable.Empty<IHierarchy<TKey, TValue>>();
+    //  IHierarchy<TKey, TValue> itemWithIdAndParentIdIsTheSame = valuesCache.FirstOrDefault(v => IsSameId(idSelector(v), parentIdSelector(v)));
+    //  if (itemWithIdAndParentIdIsTheSame != null) // Hier verwacht je ook een null terug te kunnen komen
+    //  {
+    //    throw new ArgumentException("At least one value has the samen Id and parentId [{0}]");
+    //  }
 
-      var nodes = valuesCache.Select(v => new Hierarchy<T>(v));
-      return CreateTree(nodes, idSelector, parentIdSelector);
+    //  var nodes = valuesCache.Select(v => new Hierarchy<TKey, TValue>(v));
+    //  return CreateTree(nodes, idSelector, parentIdSelector);
 
-    }
+    //}
 
-    public static IEnumerable<Hierarchy<T>> CreateTree<TId>(IEnumerable<Hierarchy<TKey,TValue>> rootNodes, Func<T, TId> idSelector, Func<T, TId?> parentIdSelector)
-        where TId : struct
+    //public static IEnumerable<IHierarchy<TKey, TValue>> CreateTree<TId>(IEnumerable<Hierarchy<TKey,TValue>> rootNodes, Func<IHierarchy<TKey, TValue>, TId> idSelector, Func<IHierarchy<TKey, TValue>, TId?> parentIdSelector)
+    //  where TId : struct
+    //{
+    //  var rootNodesCache = rootNodes.ToList();
+    //  var duplicates = rootNodesCache.Duplicates(n => n).ToList();
+    //  if (duplicates.Any())
+    //  {
+    //    throw new ArgumentException("One or more values contains {0} duplicate keys. The first duplicate is: [{1}]");
+    //  }
 
-    {
-      var rootNodesCache = rootNodes.ToList();
-      var duplicates = rootNodesCache.Duplicates(n => n).ToList();
-      if (duplicates.Any())
-      {
-        throw new ArgumentException("One or more values contains {0} duplicate keys. The first duplicate is: [{1}]");
-      }
+    //  foreach (var rootNode in rootNodesCache)
+    //  {
+    //    var parentId = parentIdSelector(rootNode.Value);
+    //    var parent = rootNodesCache.FirstOrDefault(n => IsSameId(idSelector(n.Value), parentId));
 
-      foreach (var rootNode in rootNodesCache)
-      {
-        var parentId = parentIdSelector(rootNode.Value);
-        var parent = rootNodesCache.FirstOrDefault(n => IsSameId(idSelector(n.Value), parentId));
+    //    if (parent != null)
+    //    {
+    //      parent.Add(rootNode);
+    //    }
+    //    else if (parentId != null)
+    //    {
+    //      throw new ArgumentException("A value has the parent ID [{0}] but no other nodes has this ID");
+    //    }
+    //  }
+    //  var result = rootNodesCache.Where(n => n.IsRoot);
+    //  return result;
+    //}
 
-        if (parent != null)
-        {
-          parent.Add(rootNode);
-        }
-        else if (parentId != null)
-        {
-          throw new ArgumentException("A value has the parent ID [{0}] but no other nodes has this ID");
-        }
-      }
-      var result = rootNodesCache.Where(n => n.IsRoot);
-      return result;
-    }
-
-
-    //private static bool IsSameId<TId>(TId id, TId? parentId)
-    //    where TId : struct
+    //private static bool IsSameId<TKey, TValue>(IHierarchy<TKey, TValue> id, IHierarchy<TKey, TValue>? parentId)
+    //  //where TId : struct
     //{
     //  return parentId != null && id.Equals(parentId.Value);
     //}
--- FluxSolution/BaseLibrary/Source/Foundation/DataStructures/Node.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/DataStructures/Node.cs
@@ -1,281 +1,281 @@
-namespace Flux
-{
-  #region Extension methods.
-  public static partial class ExtensionMethods
-  {
-    public static System.Collections.Generic.IEnumerable<T> Values<T>(this System.Collections.Generic.IEnumerable<Node<T>> nodes)
-      => nodes.Select(n => n.Value);
+//namespace Flux
+//{
+//  #region Extension methods.
+//  public static partial class ExtensionMethods
+//  {
+//    public static System.Collections.Generic.IEnumerable<T> Values<T>(this System.Collections.Generic.IEnumerable<Node<T>> nodes)
+//      => nodes.Select(n => n.Value);
 
-    public static System.Collections.Generic.IEnumerable<TSource> Duplicates<TSource, TKey>(this System.Collections.Generic.IEnumerable<TSource> source, Func<TSource, TKey> selector)
-      => source.GroupBy(selector).Where(i => i.IsMultiple()).SelectMany(i => i);
+//    public static System.Collections.Generic.IEnumerable<TSource> Duplicates<TSource, TKey>(this System.Collections.Generic.IEnumerable<TSource> source, Func<TSource, TKey> selector)
+//      => source.GroupBy(selector).Where(i => i.IsMultiple()).SelectMany(i => i);
 
-    public static bool IsMultiple<T>(this System.Collections.Generic.IEnumerable<T> source)
-    {
-      using var enumerator = source.GetEnumerator();
+//    public static bool IsMultiple<T>(this System.Collections.Generic.IEnumerable<T> source)
+//    {
+//      using var enumerator = source.GetEnumerator();
 
-      return enumerator.MoveNext() && enumerator.MoveNext();
-    }
+//      return enumerator.MoveNext() && enumerator.MoveNext();
+//    }
 
-    public static System.Collections.Generic.IEnumerable<T> ToIEnumarable<T>(this T item)
-    {
-      yield return item;
-    }
-  }
-  #endregion Extension methods.
+//    public static System.Collections.Generic.IEnumerable<T> ToIEnumarable<T>(this T item)
+//    {
+//      yield return item;
+//    }
+//  }
+//  #endregion Extension methods.
 
-  public class Node<T>
-    : System.Collections.Generic.IEqualityComparer<Node<T>>, System.Collections.Generic.IEnumerable<Node<T>>, System.Collections.Generic.IEnumerable<T>
-  {
-    private readonly List<Node<T>> m_children = new List<Node<T>>();
+//  public class Node<T>
+//    : System.Collections.Generic.IEqualityComparer<Node<T>>, System.Collections.Generic.IEnumerable<Node<T>>, System.Collections.Generic.IEnumerable<T>
+//  {
+//    private readonly List<Node<T>> m_children = new List<Node<T>>();
 
-    public Node<T> Parent { get; private set; }
+//    public Node<T> Parent { get; private set; }
 
-    public T Value { get; set; }
+//    public T Value { get; set; }
 
-    public Node(T value)
-    {
-      Value = value;
-    }
+//    public Node(T value)
+//    {
+//      Value = value;
+//    }
 
-    public Node<T> this[int index]
-      => m_children[index];
+//    public Node<T> this[int index]
+//      => m_children[index];
 
-    public Node<T> Add(T value, int index = -1)
-    {
-      var childNode = new Node<T>(value);
-      Add(childNode, index);
-      return childNode;
-    }
+//    public Node<T> Add(T value, int index = -1)
+//    {
+//      var childNode = new Node<T>(value);
+//      Add(childNode, index);
+//      return childNode;
+//    }
 
-    public void Add(Node<T> childNode, int index = -1)
-    {
-      if (index < -1)
-        throw new ArgumentException("The index can not be lower then -1");
-      if (index > Children.Count() - 1)
-        throw new ArgumentException("The index ({0}) can not be higher then index of the last iten. Use the AddChild() method without an index to add at the end");
-      if (!childNode.IsRoot)
-        throw new ArgumentException("The child node with value [{0}] can not be added because it is not a root node.");
-      if (Root == childNode)
-        throw new ArgumentException("The child node with value [{0}] is the rootnode of the parent.");
-      if (childNode.SelfAndDescendants.Any(n => this == n))
-        throw new ArgumentException("The childnode with value [{0}] can not be added to itself or its descendants.");
+//    public void Add(Node<T> childNode, int index = -1)
+//    {
+//      if (index < -1)
+//        throw new ArgumentException("The index can not be lower then -1");
+//      if (index > Children.Count() - 1)
+//        throw new ArgumentException("The index ({0}) can not be higher then index of the last iten. Use the AddChild() method without an index to add at the end");
+//      if (!childNode.IsRoot)
+//        throw new ArgumentException("The child node with value [{0}] can not be added because it is not a root node.");
+//      if (Root == childNode)
+//        throw new ArgumentException("The child node with value [{0}] is the rootnode of the parent.");
+//      if (childNode.SelfAndDescendants.Any(n => this == n))
+//        throw new ArgumentException("The childnode with value [{0}] can not be added to itself or its descendants.");
 
-      childNode.Parent = this;
+//      childNode.Parent = this;
 
-      if (index == -1)
-      {
-        m_children.Add(childNode);
-      }
-      else
-      {
-        m_children.Insert(index, childNode);
-      }
-    }
+//      if (index == -1)
+//      {
+//        m_children.Add(childNode);
+//      }
+//      else
+//      {
+//        m_children.Insert(index, childNode);
+//      }
+//    }
 
-    public void AddFirstChild(Node<T> childNode)
-      => Add(childNode, 0);
-    public Node<T> AddFirstChild(T value)
-    {
-      var childNode = new Node<T>(value);
-      AddFirstChild(childNode);
-      return childNode;
-    }
+//    public void AddFirstChild(Node<T> childNode)
+//      => Add(childNode, 0);
+//    public Node<T> AddFirstChild(T value)
+//    {
+//      var childNode = new Node<T>(value);
+//      AddFirstChild(childNode);
+//      return childNode;
+//    }
 
-    public void AddFirstSibling(Node<T> childNode)
-      => Parent.AddFirstChild(childNode);
-    public Node<T> AddFirstSibling(T value)
-    {
-      var siblingNode = new Node<T>(value);
-      AddFirstSibling(siblingNode);
-      return siblingNode;
-    }
+//    public void AddFirstSibling(Node<T> childNode)
+//      => Parent.AddFirstChild(childNode);
+//    public Node<T> AddFirstSibling(T value)
+//    {
+//      var siblingNode = new Node<T>(value);
+//      AddFirstSibling(siblingNode);
+//      return siblingNode;
+//    }
 
-    public Node<T> AddLastSibling(T value)
-    {
-      var childNode = new Node<T>(value);
-      AddLastSibling(childNode);
-      return childNode;
-    }
+//    public Node<T> AddLastSibling(T value)
+//    {
+//      var childNode = new Node<T>(value);
+//      AddLastSibling(childNode);
+//      return childNode;
+//    }
 
-    public void AddLastSibling(Node<T> childNode)
-    {
-      Parent.Add(childNode);
-    }
+//    public void AddLastSibling(Node<T> childNode)
+//    {
+//      Parent.Add(childNode);
+//    }
 
-    public Node<T> AddParent(T value)
-    {
-      var newNode = new Node<T>(value);
-      AddParent(newNode);
-      return newNode;
-    }
+//    public Node<T> AddParent(T value)
+//    {
+//      var newNode = new Node<T>(value);
+//      AddParent(newNode);
+//      return newNode;
+//    }
 
-    public void AddParent(Node<T> parentNode)
-    {
-      if (!IsRoot)
-      {
-        throw new ArgumentException("This node [{0}] already has a parent", "parentNode");
-      }
-      parentNode.Add(this);
-    }
+//    public void AddParent(Node<T> parentNode)
+//    {
+//      if (!IsRoot)
+//      {
+//        throw new ArgumentException("This node [{0}] already has a parent", "parentNode");
+//      }
+//      parentNode.Add(this);
+//    }
 
-    public System.Collections.Generic.IEnumerable<Node<T>> Ancestors
-      => IsRoot ? System.Linq.Enumerable.Empty<Node<T>>() : Parent.ToIEnumarable().Concat(Parent.Ancestors);
+//    public System.Collections.Generic.IEnumerable<Node<T>> Ancestors
+//      => IsRoot ? System.Linq.Enumerable.Empty<Node<T>>() : Parent.ToIEnumarable().Concat(Parent.Ancestors);
 
-    public System.Collections.Generic.IEnumerable<Node<T>> Descendants
-      => SelfAndDescendants.Skip(1);
+//    public System.Collections.Generic.IEnumerable<Node<T>> Descendants
+//      => SelfAndDescendants.Skip(1);
 
-    public System.Collections.Generic.IEnumerable<Node<T>> Children
-      => m_children;
+//    public System.Collections.Generic.IEnumerable<Node<T>> Children
+//      => m_children;
 
-    public System.Collections.Generic.IEnumerable<Node<T>> Siblings
-      => SelfAndSiblings.Where(Other);
+//    public System.Collections.Generic.IEnumerable<Node<T>> Siblings
+//      => SelfAndSiblings.Where(Other);
 
-    private bool Other(Node<T> node)
-      => !ReferenceEquals(node, this);
+//    private bool Other(Node<T> node)
+//      => !ReferenceEquals(node, this);
 
-    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndChildren
-      => this.ToIEnumarable().Concat(Children);
+//    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndChildren
+//      => this.ToIEnumarable().Concat(Children);
 
-    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndAncestors
-      => this.ToIEnumarable().Concat(Ancestors);
+//    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndAncestors
+//      => this.ToIEnumarable().Concat(Ancestors);
 
-    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndDescendants
-      => this.ToIEnumarable().Concat(Children.SelectMany(c => c.SelfAndDescendants));
+//    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndDescendants
+//      => this.ToIEnumarable().Concat(Children.SelectMany(c => c.SelfAndDescendants));
 
-    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndSiblings
-      => IsRoot ? this.ToIEnumarable() : Parent.Children;
+//    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndSiblings
+//      => IsRoot ? this.ToIEnumarable() : Parent.Children;
 
-    public System.Collections.Generic.IEnumerable<Node<T>> All
-      => Root.SelfAndDescendants;
+//    public System.Collections.Generic.IEnumerable<Node<T>> All
+//      => Root.SelfAndDescendants;
 
-    public System.Collections.Generic.IEnumerable<Node<T>> SameLevel
-      => SelfAndSameLevel.Where(Other);
+//    public System.Collections.Generic.IEnumerable<Node<T>> SameLevel
+//      => SelfAndSameLevel.Where(Other);
 
-    public int Level
-      => Ancestors.Count();
+//    public int Level
+//      => Ancestors.Count();
 
-    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndSameLevel
-      => GetNodesAtLevel(Level);
+//    public System.Collections.Generic.IEnumerable<Node<T>> SelfAndSameLevel
+//      => GetNodesAtLevel(Level);
 
-    private System.Collections.Generic.IEnumerable<Node<T>> GetNodesAtLevelInternal(int level)
-      => level == Level ? this.ToIEnumarable() : Children.SelectMany(c => c.GetNodesAtLevelInternal(level));
+//    private System.Collections.Generic.IEnumerable<Node<T>> GetNodesAtLevelInternal(int level)
+//      => level == Level ? this.ToIEnumarable() : Children.SelectMany(c => c.GetNodesAtLevelInternal(level));
 
-    public System.Collections.Generic.IEnumerable<Node<T>> GetNodesAtLevel(int level)
-      => Root.GetNodesAtLevelInternal(level);
+//    public System.Collections.Generic.IEnumerable<Node<T>> GetNodesAtLevel(int level)
+//      => Root.GetNodesAtLevelInternal(level);
 
-    public Node<T> Root
-      => SelfAndAncestors.Last();
+//    public Node<T> Root
+//      => SelfAndAncestors.Last();
 
-    public void Disconnect()
-    {
-      if (IsRoot) throw new System.InvalidOperationException("The root node [{0}] can not get disconnected from a parent.");
+//    public void Disconnect()
+//    {
+//      if (IsRoot) throw new System.InvalidOperationException("The root node [{0}] can not get disconnected from a parent.");
 
-      Parent.m_children.Remove(this);
-      Parent = null;
-    }
+//      Parent.m_children.Remove(this);
+//      Parent = null;
+//    }
 
-    public bool IsRoot
-    {
-      get { return Parent == null; }
-    }
+//    public bool IsRoot
+//    {
+//      get { return Parent == null; }
+//    }
 
-    System.Collections.Generic.IEnumerator<T> System.Collections.Generic.IEnumerable<T>.GetEnumerator()
-      => m_children.Values().GetEnumerator();
+//    System.Collections.Generic.IEnumerator<T> System.Collections.Generic.IEnumerable<T>.GetEnumerator()
+//      => m_children.Values().GetEnumerator();
 
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-      => m_children.GetEnumerator();
+//    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+//      => m_children.GetEnumerator();
 
-    public System.Collections.Generic.IEnumerator<Node<T>> GetEnumerator()
-      => m_children.GetEnumerator();
+//    public System.Collections.Generic.IEnumerator<Node<T>> GetEnumerator()
+//      => m_children.GetEnumerator();
 
-    public override string ToString()
-      => $"{Value}";
+//    public override string ToString()
+//      => $"{Value}";
 
-    public static IEnumerable<Node<T>> CreateTree<TId>(IEnumerable<T> values, Func<T, TId> idSelector, Func<T, TId?> parentIdSelector)
-        where TId : struct
-    {
-      var valuesCache = values.ToList();
-      if (!valuesCache.Any())
-        return System.Linq.Enumerable.Empty<Node<T>>();
-      T itemWithIdAndParentIdIsTheSame = valuesCache.FirstOrDefault(v => IsSameId(idSelector(v), parentIdSelector(v)));
-      if (itemWithIdAndParentIdIsTheSame != null) // Hier verwacht je ook een null terug te kunnen komen
-      {
-        throw new ArgumentException("At least one value has the samen Id and parentId [{0}]");
-      }
+//    public static IEnumerable<Node<T>> CreateTree<TId>(IEnumerable<T> values, Func<T, TId> idSelector, Func<T, TId?> parentIdSelector)
+//        where TId : struct
+//    {
+//      var valuesCache = values.ToList();
+//      if (!valuesCache.Any())
+//        return System.Linq.Enumerable.Empty<Node<T>>();
+//      T itemWithIdAndParentIdIsTheSame = valuesCache.FirstOrDefault(v => IsSameId(idSelector(v), parentIdSelector(v)));
+//      if (itemWithIdAndParentIdIsTheSame != null) // Hier verwacht je ook een null terug te kunnen komen
+//      {
+//        throw new ArgumentException("At least one value has the samen Id and parentId [{0}]");
+//      }
 
-      var nodes = valuesCache.Select(v => new Node<T>(v));
-      return CreateTree(nodes, idSelector, parentIdSelector);
+//      var nodes = valuesCache.Select(v => new Node<T>(v));
+//      return CreateTree(nodes, idSelector, parentIdSelector);
 
-    }
+//    }
 
-    public static IEnumerable<Node<T>> CreateTree<TId>(IEnumerable<Node<T>> rootNodes, Func<T, TId> idSelector, Func<T, TId?> parentIdSelector)
-        where TId : struct
+//    public static IEnumerable<Node<T>> CreateTree<TId>(IEnumerable<Node<T>> rootNodes, Func<T, TId> idSelector, Func<T, TId?> parentIdSelector)
+//        where TId : struct
 
-    {
-      var rootNodesCache = rootNodes.ToList();
-      var duplicates = rootNodesCache.Duplicates(n => n).ToList();
-      if (duplicates.Any())
-      {
-        throw new ArgumentException("One or more values contains {0} duplicate keys. The first duplicate is: [{1}]");
-      }
+//    {
+//      var rootNodesCache = rootNodes.ToList();
+//      var duplicates = rootNodesCache.Duplicates(n => n).ToList();
+//      if (duplicates.Any())
+//      {
+//        throw new ArgumentException("One or more values contains {0} duplicate keys. The first duplicate is: [{1}]");
+//      }
 
-      foreach (var rootNode in rootNodesCache)
-      {
-        var parentId = parentIdSelector(rootNode.Value);
-        var parent = rootNodesCache.FirstOrDefault(n => IsSameId(idSelector(n.Value), parentId));
+//      foreach (var rootNode in rootNodesCache)
+//      {
+//        var parentId = parentIdSelector(rootNode.Value);
+//        var parent = rootNodesCache.FirstOrDefault(n => IsSameId(idSelector(n.Value), parentId));
 
-        if (parent != null)
-        {
-          parent.Add(rootNode);
-        }
-        else if (parentId != null)
-        {
-          throw new ArgumentException("A value has the parent ID [{0}] but no other nodes has this ID");
-        }
-      }
-      var result = rootNodesCache.Where(n => n.IsRoot);
-      return result;
-    }
+//        if (parent != null)
+//        {
+//          parent.Add(rootNode);
+//        }
+//        else if (parentId != null)
+//        {
+//          throw new ArgumentException("A value has the parent ID [{0}] but no other nodes has this ID");
+//        }
+//      }
+//      var result = rootNodesCache.Where(n => n.IsRoot);
+//      return result;
+//    }
 
 
-    private static bool IsSameId<TId>(TId id, TId? parentId)
-        where TId : struct
-    {
-      return parentId != null && id.Equals(parentId.Value);
-    }
+//    private static bool IsSameId<TId>(TId id, TId? parentId)
+//        where TId : struct
+//    {
+//      return parentId != null && id.Equals(parentId.Value);
+//    }
 
-    #region Equals en ==
+//    #region Equals en ==
 
-    public static bool operator ==(Node<T> value1, Node<T> value2)
-    {
-      if ((object)(value1) == null && (object)value2 == null)
-      {
-        return true;
-      }
-      return ReferenceEquals(value1, value2);
-    }
+//    public static bool operator ==(Node<T> value1, Node<T> value2)
+//    {
+//      if ((object)(value1) == null && (object)value2 == null)
+//      {
+//        return true;
+//      }
+//      return ReferenceEquals(value1, value2);
+//    }
 
-    public static bool operator !=(Node<T> value1, Node<T> value2)
-    {
-      return !(value1 == value2);
-    }
+//    public static bool operator !=(Node<T> value1, Node<T> value2)
+//    {
+//      return !(value1 == value2);
+//    }
 
-    public bool Equals(Node<T>? value1, Node<T>? value2)
-      => ReferenceEquals(value1, value2);
-    public bool Equals(Node<T>? value)
-      => Equals(this, value);
+//    public bool Equals(Node<T>? value1, Node<T>? value2)
+//      => ReferenceEquals(value1, value2);
+//    public bool Equals(Node<T>? value)
+//      => Equals(this, value);
 
-    public int GetHashCode(Node<T> value)
-    {
-      return base.GetHashCode();
-    }
-    #endregion
+//    public int GetHashCode(Node<T> value)
+//    {
+//      return base.GetHashCode();
+//    }
+//    #endregion
 
-    public override bool Equals(object? obj)
-      => obj is Node<T> o && Equals(o);
-    public override int GetHashCode()
-    {
-      return GetHashCode(this);
-    }
-  }
-}
+//    public override bool Equals(object? obj)
+//      => obj is Node<T> o && Equals(o);
+//    public override int GetHashCode()
+//    {
+//      return GetHashCode(this);
+//    }
+//  }
+//}
