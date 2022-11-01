64485c893bdf4e790ffa8d8ee4c1414865250fab
Rob <robert_hugo@hotmail.com>
2022-10-31 22:22:17 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Numerics/GenericMath/Helper.cs
+++ FluxSolution/BaseLibrary/Source/Numerics/GenericMath/Helper.cs
@@ -3,28 +3,47 @@ namespace Flux
 {
   public static partial class GenericMath
   {
+    //public static TSelf Div2<TSelf>(this TSelf value)
+    //  where TSelf : System.Numerics.INumberBase<TSelf>
+    //  => value / (TSelf.One + TSelf.One);
+    //public static TSelf Div3<TSelf>(this TSelf value)
+    //  where TSelf : System.Numerics.INumberBase<TSelf>
+    //  => value / (TSelf.One + TSelf.One + TSelf.One);
+
+    public static TResult Div<TSelf, TScalar, TResult>(this TSelf value, TScalar scalar, out TResult result)
+      where TSelf : System.Numerics.INumber<TSelf>
+      where TScalar : System.Numerics.INumber<TScalar>
+      where TResult : System.Numerics.INumber<TResult>
+      => result = TResult.CreateChecked(value) / TResult.CreateChecked(scalar);
     public static TSelf Div2<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
-      => value / (TSelf.One + TSelf.One);
-    public static TSelf Div3<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
-      => value / (TSelf.One + TSelf.One + TSelf.One);
+      where TSelf : System.Numerics.INumber<TSelf>
+      => Div(value, 2, out TSelf _);
+
+    public static TResult Mul<TSelf, TScalar, TResult>(this TSelf value, TScalar scalar, out TResult result)
+      where TSelf : System.Numerics.INumber<TSelf>
+      where TScalar : System.Numerics.INumber<TScalar>
+      where TResult : System.Numerics.INumber<TResult>
+      => result = TResult.CreateChecked(value) * TResult.CreateChecked(scalar);
 
     public static TSelf Mul2<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
-      => value * (TSelf.One + TSelf.One);
+      where TSelf : System.Numerics.INumber<TSelf>
+      => Mul(value, 2, out TSelf _);
+
+    //public static TSelf Mul2<TSelf>(this TSelf value)
+    //  where TSelf : System.Numerics.INumberBase<TSelf>
+    //  => value * (TSelf.One + TSelf.One);
 
     public static TSelf Pow2<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>
       => value * value;
     public static TSelf Pow8<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>
       => Pow2(Pow2(Pow2(value)));
     public static TSelf Pow10<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>
       => Pow2(value * Pow2(Pow2(value)));
     public static TSelf Pow16<TSelf>(this TSelf value)
-      where TSelf : System.Numerics.INumberBase<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>
       => Pow2(Pow8(value));
   }
 }
--- FluxSolution/BaseLibrary/Source/Numerics/GenericMath/Trigonometric/TrigonometricFunctions.cs
+++ FluxSolution/BaseLibrary/Source/Numerics/GenericMath/Trigonometric/TrigonometricFunctions.cs
@@ -174,7 +174,7 @@ namespace Flux
     /// <summary>PREVIEW! Returns the inverse hyperbolic cotangent of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Inverse_hyperbolic_function"/>
     static public TSelf Acoth<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ILogarithmicFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ILogarithmicFunctions<TSelf>
       => TSelf.Log((x + TSelf.One) / (x - TSelf.One)).Div2();
 
     #endregion
@@ -240,25 +240,25 @@ namespace Flux
     /// <summary>PREVIEW! Returns the haversed sine of the specified angle. This is the famous Haversin function.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine"/>
     public static TSelf Hvsin<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => (TSelf.One - TSelf.Cos(x)).Div2();
 
     /// <summary>PREVIEW! Returns the haversed cosine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine"/>
     public static TSelf Hvcos<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => (TSelf.One + TSelf.Cos(x)).Div2();
 
     /// <summary>PREVIEW! Returns the inverse of haversed sine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine#Inverse_functions"/>
     public static TSelf Ahvsin<TSelf>(this TSelf y)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => TSelf.Acos(TSelf.One - y.Mul2()); // An extra subtraction saves a call to the Sqrt function: 2 * System.Math.Asin(System.Math.Sqrt(y));
 
     /// <summary>PREVIEW! Returns the inverse of haversed cosine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine#Inverse_functions"/>
     public static TSelf Ahvcos<TSelf>(this TSelf y)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => TSelf.Acos(y.Mul2() - TSelf.One); // An extra subtraction saves a call to the Sqrt function: 2 * System.Math.Acos(System.Math.Sqrt(y));
 
     #endregion
@@ -268,25 +268,25 @@ namespace Flux
     /// <summary>PREVIEW! Returns the hacoversed sine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine"/>
     public static TSelf Hcvsin<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => (TSelf.One - TSelf.Sin(x)).Div2();
 
     /// <summary>PREVIEW! Returns the hacoversed cosine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine"/>
     public static TSelf Hcvcos<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => (TSelf.One + TSelf.Sin(x)).Div2();
 
     /// <summary>PREVIEW! Returns the inverse of cohaversed sine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine#Inverse_functions"/>
     public static TSelf Ahcvsin<TSelf>(this TSelf y)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => TSelf.Asin(TSelf.One - y.Mul2());
 
     /// <summary>PREVIEW! Returns the inverse of cohaversed cosine of the specified angle.</summary>
     /// <see cref="https://en.wikipedia.org/wiki/Versine#Inverse_functions"/>
     public static TSelf Ahcvcos<TSelf>(this TSelf y)
-      where TSelf : System.Numerics.ITrigonometricFunctions<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>, System.Numerics.ITrigonometricFunctions<TSelf>
       => TSelf.Asin(y.Mul2() - TSelf.One);
 
     #endregion
