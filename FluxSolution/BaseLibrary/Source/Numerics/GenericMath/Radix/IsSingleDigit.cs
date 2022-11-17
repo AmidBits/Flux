c1b5c5e296dc302ed8df50ac32905d9c0d1d8116
Rob <robert_hugo@hotmail.com>
2022-11-17 12:57:58 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Numerics/GenericMath/Radix/IsSingleDigit.cs
+++ FluxSolution/BaseLibrary/Source/Numerics/GenericMath/Radix/IsSingleDigit.cs
@@ -5,6 +5,6 @@ namespace Flux
     /// <summary>Indicates whether the <paramref name="number"/> is single digit using the base <paramref name="radix"/>, i.e. in the range [-<paramref name="radix"/>, <paramref name="radix"/>].</summary>
     public static bool IsSingleDigit<TSelf>(this TSelf number, TSelf radix)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
-      => TSelf.IsZero(number) || (TSelf.Abs(radix) is var absRadix && ((TSelf.IsNegative(number) && number > -absRadix) || (TSelf.IsPositive(number) && number < absRadix)));
+      => TSelf.IsZero(number) || (TSelf.IsPositive(number) && number < AssertRadix(radix)) || (TSelf.IsNegative(number) && number > -AssertRadix(radix));
   }
 }
