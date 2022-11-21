e7165b7f13d8f05a82c1c32bf364ae7adf8e0539
Rob <robert_hugo@hotmail.com>
2022-11-20 17:13:32 -0700

    Updates.

--- FluxSolution/BaseLibrary/Source/Foundation/Linq/ISet/Zamplez.cs
+++ FluxSolution/BaseLibrary/Source/Foundation/Linq/ISet/Zamplez.cs
@@ -18,14 +18,42 @@ namespace Flux
       var padding = 2;
       padding++;
 
-      var (minLeft, minTop, maxLeft, maxTop) = os1.Select(v => v.ToString()).Prepend("Set:1").WriteToConsole();
-      (minLeft, minTop, maxLeft, maxTop) = os2.Select(v => v.ToString()).Prepend("Set:2").WriteToConsole(maxLeft + padding, minTop);
+      System.Console.WriteLine($"Set:1");
+      System.Console.WriteLine($"{string.Join(", ", os1)}");
+      System.Console.WriteLine();
 
-      (minLeft, minTop, maxLeft, maxTop) = os1.SourceDifference(os2).Select(v => v.ToString()).Prepend("Source-Diff.").WriteToConsole(maxLeft + padding, minTop);
-      (minLeft, minTop, maxLeft, maxTop) = os1.SymmetricDifference(os2).Select(v => v.ToString()).Prepend("Sym-Diff.").WriteToConsole(maxLeft + padding, minTop);
-      (minLeft, minTop, maxLeft, maxTop) = os1.TargetDifference(os2).Select(v => v.ToString()).Prepend("Target-Diff.").WriteToConsole(maxLeft + padding, minTop);
-      (minLeft, minTop, maxLeft, maxTop) = os1.CartesianProduct(new System.Collections.Generic.HashSet<int>[] { os2 }).Select(v => string.Join(",", v)).Prepend($"Cartesian-Product ").WriteToConsole(maxLeft + padding, minTop);
-      (minLeft, minTop, maxLeft, maxTop) = os1.PowerSet().Select(v => string.Join(",", v)).Prepend($"Power-Set ").WriteToConsole(maxLeft + padding, minTop);
+      System.Console.WriteLine($"Set:2");
+      System.Console.WriteLine($"{string.Join(", ", os2)}");
+      System.Console.WriteLine();
+
+      System.Console.WriteLine($"Source-Diff.");
+      System.Console.WriteLine($"{string.Join(", ", os1.SourceDifference(os2))}");
+      System.Console.WriteLine();
+
+      System.Console.WriteLine($"Sym-Diff.");
+      System.Console.WriteLine($"{string.Join(", ", os1.SymmetricDifference(os2))}");
+      System.Console.WriteLine();
+
+      System.Console.WriteLine($"Target-Diff.");
+      System.Console.WriteLine($"{string.Join(", ", os1.TargetDifference(os2))}");
+      System.Console.WriteLine();
+
+      System.Console.WriteLine($"Cartesian-Product");
+      System.Console.WriteLine($"{string.Join("; ", os1.CartesianProduct(new System.Collections.Generic.HashSet<int>[] { os2 }).Select(v => string.Join(',', v)))}");
+      System.Console.WriteLine();
+
+      System.Console.WriteLine($"Power-Set");
+      System.Console.WriteLine($"{string.Join("; ", os1.PowerSet().Where(list => list.Count > 0).Select(list => string.Join(',', list)))}");
+      System.Console.WriteLine();
+
+      //var (minLeft, minTop, maxLeft, maxTop) = os1.Select(v => v.ToString()).Prepend("Set:1").WriteToConsole();
+      //(minLeft, minTop, maxLeft, maxTop) = os2.Select(v => v.ToString()).Prepend("Set:2").WriteToConsole(maxLeft + padding, minTop);
+
+      //(minLeft, minTop, maxLeft, maxTop) = os1.SourceDifference(os2).Select(v => v.ToString()).Prepend("Source-Diff.").WriteToConsole(maxLeft + padding, minTop);
+      //(minLeft, minTop, maxLeft, maxTop) = os1.SymmetricDifference(os2).Select(v => v.ToString()).Prepend("Sym-Diff.").WriteToConsole(maxLeft + padding, minTop);
+      //(minLeft, minTop, maxLeft, maxTop) = os1.TargetDifference(os2).Select(v => v.ToString()).Prepend("Target-Diff.").WriteToConsole(maxLeft + padding, minTop);
+      // (minLeft, minTop, maxLeft, maxTop) = os1.CartesianProduct(new System.Collections.Generic.HashSet<int>[] { os2 }).Select(v => string.Join(",", v)).Prepend($"Cartesian-Product ").WriteToConsole(maxLeft + padding, minTop);
+      //(minLeft, minTop, maxLeft, maxTop) = os1.PowerSet().Select(v => string.Join(",", v)).Prepend($"Power-Set ").WriteToConsole(maxLeft + padding, minTop);
 
       System.Console.WriteLine($"{System.Environment.NewLine}{nameof(System.Collections.Generic.ISet<int>.SetEquals)} = {os1.SetEquals(os2)}");
       System.Console.WriteLine($"{System.Environment.NewLine}{nameof(System.Collections.Generic.ISet<int>.Overlaps)} = {os1.Overlaps(os2)}");
