b66aeed28c1132433e090bbd74d87d30cd0fb87d
Rob <robert_hugo@hotmail.com>
2023-03-28 19:12:15 -0700

    Re-org only.

--- FluxSolution/BaseLibrary/Source/Numerics/GenericMath/AssertRadix.cs
+++ FluxSolution/BaseLibrary/Source/Numerics/GenericMath/AssertRadix.cs
@@ -2,26 +2,26 @@ namespace Flux
 {
   public static partial class GenericMath
   {
-    /// <summary>Asserts the number is a valid <paramref name="radix"/>, with an <paramref name="upperLimit"/> (throws an exception with an optional <paramref name="paramName"/>, if not).</summary>
-    /// <exception cref="System.ArgumentOutOfRangeException"></exception>
-    public static TSelf AssertRadix<TSelf>(TSelf radix, TSelf upperLimit, string? paramName = null)
-      where TSelf : System.Numerics.INumber<TSelf>
-      => IsRadix(radix, upperLimit) ? radix : throw new System.ArgumentOutOfRangeException(paramName ?? nameof(radix), $"Must be an integer, greater than or equal to 2 and less than or equal to {upperLimit}.");
-
     /// <summary>Asserts the number is a valid <paramref name="radix"/> (throws an exception with an optional <paramref name="paramName"/>, if not).</summary>
     /// <exception cref="System.ArgumentOutOfRangeException"></exception>
     public static TSelf AssertRadix<TSelf>(TSelf radix, string? paramName = null)
       where TSelf : System.Numerics.INumber<TSelf>
       => IsRadix(radix) ? radix : throw new System.ArgumentOutOfRangeException(paramName ?? nameof(radix), "Must be an integer, greater than or equal to 2.");
 
-    /// <summary>Returns whether the number is a valid <paramref name="radix"/>, with an <paramref name="upperLimit"/>.</summary>
-    public static bool IsRadix<TSelf>(TSelf radix, TSelf upperLimit)
-      where TSelf : System.Numerics.INumber<TSelf> // Accomodate INumber so that other types than integer can be used if needed.
-      => radix > TSelf.One && radix <= upperLimit && TSelf.IsInteger(radix);
+    /// <summary>Asserts the number is a valid <paramref name="radix"/>, with an <paramref name="upperLimit"/> (throws an exception with an optional <paramref name="paramName"/>, if not).</summary>
+    /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+    public static TSelf AssertRadix<TSelf>(TSelf radix, TSelf upperLimit, string? paramName = null)
+      where TSelf : System.Numerics.INumber<TSelf>
+      => IsRadix(radix, upperLimit) ? radix : throw new System.ArgumentOutOfRangeException(paramName ?? nameof(radix), $"Must be an integer, greater than or equal to 2 and less than or equal to {upperLimit}.");
 
     /// <summary>Returns whether the number is a valid <paramref name="radix"/>.</summary>
     public static bool IsRadix<TSelf>(TSelf radix)
       where TSelf : System.Numerics.INumber<TSelf> // Accomodate INumber so that other types than integer can be used if needed.
       => radix > TSelf.One && TSelf.IsInteger(radix);
+
+    /// <summary>Returns whether the number is a valid <paramref name="radix"/>, with an <paramref name="upperLimit"/>.</summary>
+    public static bool IsRadix<TSelf>(TSelf radix, TSelf upperLimit)
+      where TSelf : System.Numerics.INumber<TSelf> // Accomodate INumber so that other types than integer can be used if needed.
+      => radix > TSelf.One && radix <= upperLimit && TSelf.IsInteger(radix);
   }
 }
