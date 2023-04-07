7396f3384c4a23d08de2432e0e1aaa6126a87cd7
Rob <robert_hugo@hotmail.com>
2023-04-06 20:24:21 -0700

    More re-org and cleaning.

--- FluxSolution/ConsoleApp/Program.cs
+++ FluxSolution/ConsoleApp/Program.cs
@@ -17,21 +17,12 @@ namespace ConsoleApp
     private static void TimedMain(string[] _)
     {
       //if (args.Length is var argsLength && argsLength > 0) System.Console.WriteLine($"Args ({argsLength}):{System.Environment.NewLine}{string.Join(System.Environment.NewLine, System.Linq.Enumerable.Select(args, s => $"\"{s}\""))}");
-      if (Zamplez.IsSupported) { Zamplez.Run(); return; }
+      //if (Zamplez.IsSupported) { Zamplez.Run(); return; }
 
       // At some point? https://github.com/jeffshrager/elizagen.org/blob/master/Other_Elizas/20120310ShragerNorthEliza.c64basic
 
 
 
-      System.Console.WriteLine(string.Join(System.Environment.NewLine, new Flux.Text.MathTokenizer(false).GetTokens("2.0*(-2--3)").Select(t => t.ToTokenString())));
-
-
-
-      //var sr = new System.IO.StringReader("Hello\u241F\"World,\r\n\"\u241EGoodbye\u241FWorld\u241D");
-
-      //var table = Flux.Unicode.Utility.ReadGroup(sr, out var read);
-      //var text = Flux.Unicode.Utility.WriteGroup(table);
-      //var table2 = Flux.Unicode.Utility.ReadGroup(new System.IO.StringReader(text), out var rd);
     }
 
     private static void Main(string[] args)
