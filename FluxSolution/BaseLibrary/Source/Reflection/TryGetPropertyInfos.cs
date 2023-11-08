24b70d1a45b9c48a3dc293ddb2e8d1c7cb39f23d
Robert Hugo <robert_hugo@hotmail.com>
2023-11-08 13:48:10 -0700

    Changed to generic with constraint.

--- FluxSolution/BaseLibrary/Source/Reflection/TryGetPropertyInfos.cs
+++ FluxSolution/BaseLibrary/Source/Reflection/TryGetPropertyInfos.cs
@@ -5,7 +5,8 @@ namespace Flux
     /// <summary>Enumerates all PropertyInfo matching the specified binding flags. If the source is a System.Type, the properties are enumerated from the type, otherwise the instance is used.</summary>
     /// <param name="source">An instance object or a System.Type to enumerate properties on.</param>
     /// <param name="bindingFlags"></param>
-    public static System.Reflection.PropertyInfo[] GetPropertyInfos(this object source, System.Reflection.BindingFlags bindingFlags = System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Public)
+    public static System.Reflection.PropertyInfo[] GetPropertyInfos<T>(this T source, System.Reflection.BindingFlags bindingFlags = System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Public)
+      where T : notnull
       => source is null
       ? throw new System.ArgumentNullException(nameof(source))
       : source is System.Type type
