9c59a0da65514b154b594901b7558e2dae9dd150
Rob <robert_hugo@hotmail.com>
2023-12-05 01:42:43 -0700

    And conversion error, to cartesian from angle (Ex method).

--- FluxSolution/BaseLibrary/Source/Units/DerivedSI/WithNamedUnits/Angle.cs
+++ FluxSolution/BaseLibrary/Source/Units/DerivedSI/WithNamedUnits/Angle.cs
@@ -199,17 +199,14 @@
 
       /// <summary>Convert the specified counter-clockwise rotation angle [0, PI*2] (i.e. radians) where 'zero' is 'right-center' (i.e. positive-x and neutral-y) to a cartesian 2D coordinate (x, y). Looking at the face of a clock, this goes counter-clockwise from and to 3 o'clock.</summary>
       /// <see cref="https://en.wikipedia.org/wiki/Rotation_matrix#In_two_dimensions"/>
-
       public static (double x, double y) ConvertRotationAngleToCartesian2(double radAngle)
         => (System.Math.Cos(radAngle), System.Math.Sin(radAngle));
 
       /// <summary>Convert the specified clockwise rotation angle [0, PI*2] (i.e. radians) where 'zero' is 'center-up' (i.e. neutral-x and positive-y) to a cartesian 2D coordinate (x, y). Looking at the face of a clock, this goes clockwise from and to 12 o'clock.</summary>
       /// <see cref="https://en.wikipedia.org/wiki/Rotation_matrix#In_two_dimensions"/>
-
-      //public static (double x, double y) ConvertRotationAngleToCartesian2Ex(double radAngle)
-      //  => ConvertRotationAngleToCartesian2(System.Math.Tau - (radAngle % System.Math.Tau is var rad && rad < 0 ? rad + System.Math.Tau : rad) + System.Math.PI / 2);
       public static (double x, double y) ConvertRotationAngleToCartesian2Ex(double radAngle)
-        => (-System.Math.Sin(radAngle), System.Math.Cos(radAngle));
+        => ConvertRotationAngleToCartesian2(System.Math.Tau - (radAngle % System.Math.Tau is var rad && rad < 0 ? rad + System.Math.Tau : rad) + System.Math.PI / 2);
+      //=> (-System.Math.Sin(radAngle), System.Math.Cos(radAngle));
 
       public static double ConvertSexagesimalDegreeToDecimalDegree(double degrees, double minutes, double seconds)
         => degrees + minutes / 60 + seconds / 3600;
