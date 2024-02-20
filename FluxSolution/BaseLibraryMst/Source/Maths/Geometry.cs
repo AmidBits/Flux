6b9e93c8f86e4916d58b36d3109dfe343fcb2c0b
Rob <robert_hugo@hotmail.com>
2024-02-19 22:00:24 -0700

    Fixed unit test error.

--- FluxSolution/BaseLibraryMst/Source/Maths/Geometry.cs
+++ FluxSolution/BaseLibraryMst/Source/Maths/Geometry.cs
@@ -31,8 +31,8 @@ namespace Maths
     {
       var geometry = new Flux.Geometry.EllipseGeometry(5, 7);
 
-      Assert.AreEqual(5, geometry.B);
-      Assert.AreEqual(7, geometry.A);
+      Assert.AreEqual(5, geometry.A);
+      Assert.AreEqual(7, geometry.B);
     }
 
     [TestMethod]
