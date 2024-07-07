1bd36006963f0e5cab924795aa88c806bddbb30d
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-07 12:27:36 +0200

    Update CoordinateTimeDifference.em.cs

--- FluxSolution/BaseLibrary/System/DateTime/CoordinateTimeDifference.em.cs
+++ FluxSolution/BaseLibrary/System/DateTime/CoordinateTimeDifference.em.cs
@@ -4,15 +4,19 @@ namespace Flux
   {
     private const double Lg = 6.969290134e-10;
 
-    /// <summary></summary>
+    /// <summary>
+    /// <para>A clock that performs exactly the same movements as the Earth but is outside the system's (Earth's) gravity well.</para>
     /// <see href="https://en.wikipedia.org/wiki/Geocentric_Coordinate_Time"/>
+    /// </summary>
     public static double GetGeocentricCoordinateTimeDifference(this System.DateTime dateTime)
       => Lg * (dateTime.ToJulianDate(Quantities.TemporalCalendar.GregorianCalendar).Value - 2443144.5003725) * 86400;
 
     private const double Lb = 1.55051976772e-08;
 
-    /// <summary></summary>
+    /// <summary>
+    /// <para>A clock that performs exactly the same movements as the Solar System but is outside the system's (the Solar System's) gravity well.</para>
     /// <see href="https://en.wikipedia.org/wiki/Barycentric_Coordinate_Time"/>
+    /// </summary>
     public static double GetBarycentricCoordinateTimeDifference(this System.DateTime dateTime)
       => Lb * (dateTime.ToJulianDate(Quantities.TemporalCalendar.GregorianCalendar).Value - 2443144.5003725) * 86400;
   }
