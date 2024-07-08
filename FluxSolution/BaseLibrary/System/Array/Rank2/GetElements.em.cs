aa8a9797c2d4d0f82c6a4b3bf948f06f83c8db63
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-08 19:39:54 +0200

    Update GetElements.em.cs

--- FluxSolution/BaseLibrary/System/Array/Rank2/GetElements.em.cs
+++ FluxSolution/BaseLibrary/System/Array/Rank2/GetElements.em.cs
@@ -9,9 +9,7 @@ namespace Flux
     /// <remarks>Since an array is arbitrary in terms of e.g. rows and columns, we just adopt a this view, so we'll consider dimension 0 as the row dimension and dimension 1 as the column dimension.</remarks>
     public static T[] GetElements<T>(this T[,] source, int dimension, int index)
     {
-      System.ArgumentNullException.ThrowIfNull(source);
-
-      if (source.Rank != 2) throw new System.ArgumentException($"Invalid rank ({source.Rank}).", nameof(source));
+      source.ThrowIfUnequalRank(2);
 
       var sourceLength0 = source.GetLength(0);
       var sourceLength1 = source.GetLength(1);
