0624187f9e3f3ba653019fcc0ce36fb2a9a04112
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-08 17:54:30 +0200

    Update ArrayDimension.em.cs

--- FluxSolution/BaseLibrary/System/Array/ArrayDimension.em.cs
+++ FluxSolution/BaseLibrary/System/Array/ArrayDimension.em.cs
@@ -1,8 +1,9 @@
 namespace Flux
 {
   // <summary>
-  // <para>Since two dimensional arrays are arbitrary in terms of row/column orientation, we choose the native storage orientation.</para>
+  // <para>Two dimensional arrays are arbitrary in terms of row/column orientation, we choose the native storage orientation.</para>
   // <para>This just happens to be row-major order, i.e. the array consists of elements by row then by column, as opposed to first by column then by row.</para>
+  // <para>This is also how .NET yield elements using the IEnumerate interface of such an array.</para>
   // <see href="https://en.wikipedia.org/wiki/Row-_and_column-major_order"/>
   // </summary>
   public enum ArrayDimension
@@ -10,12 +11,12 @@ namespace Flux
     // <summary>
     // <para>The row is represented as dimension 0.</para>
     // </summary>
-    // <remark>The concept of dimension 0 being the row is entirely arbitrary, and simply a matter of adoption.</remark>
+    // <remark>The concept of dimension 0 as the row is entirely a matter of choice, and simply a matter of adoption.</remark>
     Row = 0,
     // <summary>
     // <para>The column is represented as dimension 1.</para>
     // </summary>
-    // <remark>The concept of dimension 1 being the column is entirely arbitrary, and simply a matter of adoption.</remark>
+    // <remark>The concept of dimension 1 as the column is entirely a matter of choice, and simply a matter of adoption.</remark>
     Column = 1
   }
 }
