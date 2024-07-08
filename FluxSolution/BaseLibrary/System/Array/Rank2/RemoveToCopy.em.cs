f43b1f86909037c6c5604a98543166c522d0494b
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-08 19:36:41 +0200

    Update RemoveToCopy.em.cs

--- FluxSolution/BaseLibrary/System/Array/Rank2/RemoveToCopy.em.cs
+++ FluxSolution/BaseLibrary/System/Array/Rank2/RemoveToCopy.em.cs
@@ -8,9 +8,7 @@ namespace Flux
     /// <remarks>Since an array is arbitrary in terms of e.g. rows and columns, we just adopt a this view, so we'll consider dimension 0 as the row dimension and dimension 1 as the column dimension.</remarks>
     public static T[,] RemoveToCopy<T>(this T[,] source, int dimension, params int[] indices)
     {
-      System.ArgumentNullException.ThrowIfNull(source);
-
-      if (source.Rank != 2) throw new System.ArgumentException($"Invalid rank ({source.Rank}).", nameof(source));
+      source.ThrowIfUnequalRank(2);
 
       var hs = new System.Collections.Generic.HashSet<int>(indices);
 
