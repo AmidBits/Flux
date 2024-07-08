2d9bbb1fb90d8249ee08f6f399ea1a3f4dbb05da
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-08 20:30:28 +0200

    Update GraphAssertProperty.em.cs

--- FluxSolution/BaseLibrary/System/Array/Rank2/AdjacencyMatrix/GraphAssertProperty.em.cs
+++ FluxSolution/BaseLibrary/System/Array/Rank2/AdjacencyMatrix/GraphAssertProperty.em.cs
@@ -4,9 +4,12 @@ namespace Flux
   {
     public static void GraphAssertProperty<T>(this T[,] source, out int length)
     {
-      System.ArgumentNullException.ThrowIfNull(source);
+      if (!source.TryGetSymmetricalLength(out length) || source.IsUnequalRank(2))
+      {
+        System.ArgumentNullException.ThrowIfNull(source);
 
-      if (!source.TryGetSymmetricalLength(out length) || source.Rank != 2) throw new System.ArgumentException($"An asymmetrical array does not represent an adjacency matrix.");
+        throw new System.ArgumentException($"An asymmetrical array does not represent an adjacency matrix.");
+      }
     }
   }
 }
