37e78dc18a441ec5649c9a9877e54bc6b9c7a214
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-09 19:41:05 +0200

    Update Color.em.cs

--- FluxSolution/BaseLibrary/System/Drawing/Color.em.cs
+++ FluxSolution/BaseLibrary/System/Drawing/Color.em.cs
@@ -64,18 +64,18 @@ namespace Flux
 
     /// <summary>Returns the luma for the RGB value, using Rec.601 coefficients.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Rec._601"/>
-    public double GetLuma601(this System.Drawing.Color source) => ComputeLuma(source, 0.2989, 0.5870, 0.1140);
+    public double ComputeLuma601(this System.Drawing.Color source) => ComputeLuma(source, 0.2989, 0.5870, 0.1140);
 
     /// <summary>Returns the luma for the RGB value, using Adobe/SMPTE 240M coefficients.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Adobe_RGB_color_space"/>
-    public double GetLuma240(this System.Drawing.Color source) => ComputeLuma(source, 0.212, 0.701, 0.087);
+    public double ComputeLuma240(this System.Drawing.Color source) => ComputeLuma(source, 0.212, 0.701, 0.087);
 
     /// <summary>Returns the luma for the RGB value, using Rec.709 coefficients.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Rec._709"/>
-    public double GetLuma709(this System.Drawing.Color source) => ComputeLuma(source, 0.2126, 0.7152, 0.0722);
+    public double ComputeLuma709(this System.Drawing.Color source) => ComputeLuma(source, 0.2126, 0.7152, 0.0722);
 
     /// <summary>Returns the luma for the RGB value, using Rec.2020 coefficients.</summary>
-    public double GetLuma2020(this System.Drawing.Color source) => ComputeLuma(source, 0.2627, 0.6780, 0.0593);
+    public double ComputeLuma2020(this System.Drawing.Color source) => ComputeLuma(source, 0.2627, 0.6780, 0.0593);
 
     /// <summary>Creates a CMYK color corresponding to the RGB instance.</summary>
     public (double A, double C, double M, double Y, double K) ToAcmyk(this System.Drawing.Color source)
@@ -91,10 +91,18 @@ namespace Flux
       return (a, c, m, y, k);
     }
 
+    /// <summary>Creates an HWB color corresponding to the RGB instance.</summary>
+    public (double A, double H, double W, double B) ToAhwb(this System.Drawing.Color source)
+    {
+      var (min, max) = ComputeMinMax(source, out var a, out var _, out var _, out var _);
+      
+      return (a, source.GetHue(), min, 1 - max);
+    }
+
     /// <summary>Converts the RGB color to grayscale using the specified method.
     /// <para><see href="https://onlinetools.com/image/grayscale-image"/></para>
     /// </summary>
-    public (double A, double R, double G, double B) ToGrayscale(this System.Drawing.Color source, GrayscaleMethod method)
+    public (double A, double R, double G, double B) ToArgbGrayscale(this System.Drawing.Color source, GrayscaleMethod method)
     {
       (a, r, g, b) = ComputeArgb(source);
 
@@ -105,14 +113,6 @@ namespace Flux
         GrayscaleMethod.Luminosity709 => (a, r * 0.21, g * 0.72, b * 0.07),
         _ => throw new System.ArgumentOutOfRangeException(nameof(method))
       };
-    }
-
-    /// <summary>Creates an HWB color corresponding to the RGB instance.</summary>
-    public (double A, double H, double W, double B) ToHwb(this System.Drawing.Color source)
-    {
-      var (min, max) = ComputeMinMax(source, out var a, out var _, out var _, out var _);
-      
-      return (a, source.GetHue(), min, 1 - max);
     }
   }
 }
