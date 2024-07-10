5e178ab203807ab835b7f7caa11486a35f44e7f0
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-10 13:33:12 +0200

    Update Color.em.cs

--- FluxSolution/BaseLibrary/System/Drawing/Color.em.cs
+++ FluxSolution/BaseLibrary/System/Drawing/Color.em.cs
@@ -2,31 +2,35 @@ namespace Flux
 {
   public static partial class Fx
   {
-    /// <summary>Returns the chroma and associated values for the RGB value.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Chrominance"/>
-    public double ComputeChroma(this System.Drawing.Color source, out double a, out double r, out double g, out double b, out double min, out double max)
+    /// <summary>
+    /// <para>Returns the chroma [0, 1] and other related values for the <see cref="System.Drawing.Color"/> as out parameters in alpha, red, green, blue, min, and max.</para>
+    /// <para><see href="https://en.wikipedia.org/wiki/Chrominance"/></para>
+    /// </summary>
+    public double ComputeChroma(this System.Drawing.Color source, out double alpha, out double red, out double green, out double blue, out double min, out double max)
     {
-      (min, max) = ComputeMinMax(source, out a, out r, out g, out b);
+      (min, max) = ComputeMinMax(source, out alpha, out red, out green, out blue);
 
       return System.Math.Clamp(max - min, 0, 1);
     }
 
-    /// <summary>Returns the hue [0, 360] for the RGB value.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Hue"/>
-    public double ComputeHue(this System.Drawing.Color source, out double a, out double r, out double g, out double b, out double min, out double max, out double chroma)
+    /// <summary>
+    /// <para>Returns the hue [0, 360] and other related values for the <see cref="System.Drawing.Color"/> as out parameters in alpha, red, green, blue, min, max, and chroma.</para>
+    /// <para><see href="https://en.wikipedia.org/wiki/Hue"/></para>
+    /// </summary>
+    public double ComputeHue(this System.Drawing.Color source, out double alpha, out double red, out double green, out double blue, out double min, out double max, out double chroma)
     {
-      chroma = ComputeChroma(source, out a, out r, out g, out b, out min, out max);
+      chroma = ComputeChroma(source, out alpha, out red, out green, out blue, out min, out max);
 
       double hue;
 
       if (chroma == 0) // No range, hue = 0.
         return 0;
-      else if (max == r)
-        hue = ((g - b) / chroma + 6) % 6; // The % operator doesn't do proper modulo on negative numbers, so we'll add 6 before using it.
-      else if (max == g)
-        hue = 2 + (b - r) / chroma;
+      else if (max == red)
+        hue = ((green - blue) / chroma + 6) % 6; // The % operator doesn't do proper modulo on negative numbers, so we'll add 6 before using it.
+      else if (max == green)
+        hue = 2 + (blue - red) / chroma;
       else // if (max == blue) // No need for comparison, at this point blue must be max.
-        hue = 4 + (r - g) / chroma;
+        hue = 4 + (red - green) / chroma;
 
       hue *= 60; // Convert to [0, 360] range.
 
@@ -36,38 +40,51 @@ namespace Flux
       return System.Math.Clamp(hue, 0, 360);
     }
 
-    /// <summary>Returns the chroma and associated values for the RGB value.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Chrominance"/>
-    public (double Min, double Max) ComputeMinMax(this System.Drawing.Color source, out double a, out double r, out double g, out double b)
+    /// <summary>
+    /// <para>Returns the min/max of the red, green and blue values from the <see cref="System.Drawing.Color"/>. Also returns the color as unit values in the out parameters alpha, red, green, blue.</para>
+    /// </summary>
+    public (double Min, double Max) ComputeMinMax(this System.Drawing.Color source, out double alpha, out double red, out double green, out double blue)
     {
-      (a, r, g, b) = ToArgb(source);
+      (alpha, red, green, blue) = ToArgb(source);
 
       return (
-        System.Math.Min(System.Math.Min(r, g), b),
-        System.Math.Max(System.Math.Max(r, g), b)
+        System.Math.Min(System.Math.Min(red, green), blue),
+        System.Math.Max(System.Math.Max(red, green), blue)
       );
     }
 
-    /// <summary>Returns the luma for the RGB value, using the specified coefficients.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Luma_(video)"/>
+    /// <summary>
+    /// <para>Returns the luma for the RGB value, using the specified coefficients.</para>
+    /// <para><see href="https://en.wikipedia.org/wiki/Luma_(video)"/></para>
+    /// </summary>
     public double ComputeLuma(this System.Drawing.Color source, double rc, double gc, double bc) => rc * source.R + gc * source.G + bc * source.B;
 
-    /// <summary>Returns the luma for the RGB value, using Adobe/SMPTE 240M coefficients.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Adobe_RGB_color_space"/>
+    /// <summary>
+    /// <para>Returns the luma for the RGB value, using Adobe/SMPTE 240M coefficients.</para>
+    /// <para><see href="https://en.wikipedia.org/wiki/Adobe_RGB_color_space"/></para>
+    /// </summary>
     public double ComputeLuma240(this System.Drawing.Color source) => ComputeLuma(source, 0.212, 0.701, 0.087);
 
-    /// <summary>Returns the luma for the RGB value, using Rec.601 coefficients.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Rec._601"/>
+    /// <summary>
+    /// <para>Returns the luma for the RGB value, using Rec.601 coefficients.</para>
+    /// <para><see href="https://en.wikipedia.org/wiki/Rec._601"/></para>
+    /// </summary>
     public double ComputeLuma601(this System.Drawing.Color source) => ComputeLuma(source, 0.2989, 0.5870, 0.1140);
 
-    /// <summary>Returns the luma for the RGB value, using Rec.709 coefficients.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Rec._709"/>
+    /// <summary>
+    /// <para>Returns the luma for the RGB value, using Rec.709 coefficients.</para>
+    /// <para><see href="https://en.wikipedia.org/wiki/Rec._709"/></para>
+    /// </summary>
     public double ComputeLuma709(this System.Drawing.Color source) => ComputeLuma(source, 0.2126, 0.7152, 0.0722);
 
-    /// <summary>Returns the luma for the RGB value, using Rec.2020 coefficients.</summary>
+    /// <summary>
+    /// <para>Returns the luma for the RGB value, using Rec.2020 coefficients.</para>
+    /// </summary>
     public double ComputeLuma2020(this System.Drawing.Color source) => ComputeLuma(source, 0.2627, 0.6780, 0.0593);
 
-    /// <summary>Creates a ACMYK color of unit values corresponding to the <see cref="System.Drawing.Color"/> instance.</summary>
+    /// <summary>
+    /// <para>Creates ACMYK unit color values corresponding to the <see cref="System.Drawing.Color"/>.</para>
+    /// </summary>
     public (double A, double C, double M, double Y, double K) ToAcmyk(this System.Drawing.Color source)
     {
       var (_, max) = ComputeMinMax(source, out var a, out var r, out var g, out var b);
@@ -81,7 +98,9 @@ namespace Flux
       return (a, c, m, y, k);
     }
 
-    /// <summary>Creates an AHWB color of unit values corresponding to the <see cref="System.Drawing.Color"/> instance.</summary>
+    /// <summary>
+    /// <para>Creates AHWB unit color values corresponding to the <see cref="System.Drawing.Color"/>.</para>
+    /// </summary>
     public (double A, double H, double W, double B) ToAhwb(this System.Drawing.Color source)
     {
       var (min, max) = ComputeMinMax(source, out var a, out var _, out var _, out var _);
@@ -89,8 +108,9 @@ namespace Flux
       return (a, source.GetHue(), min, 1 - max);
     }
 
-    /// <summary>Creates an ARGB color of unit values.</summary>
-    /// <see href="https://en.wikipedia.org/wiki/Chrominance"/>
+    /// <summary>
+    /// <para>Creates ARGB unit color values corresponding to the <see cref="System.Drawing.Color"/>.</para>
+    /// </summary>
     public (double A, double R, double G, double B) ToArgb(this System.Drawing.Color source)
       => (
         System.Math.Clamp(source.A / 255d, 0, 1),
@@ -100,7 +120,7 @@ namespace Flux
       );
 
     /// <summary>
-    /// <para>Creates a grayscale ARGB color of unit values from the <see cref="System.Drawing.Color"/> using the specified grayscale method.</para>
+    /// <para>Creates grayscale ARGB unit color values corresponding to the <see cref="System.Drawing.Color"/> using the specified grayscale method.</para>
     /// <para><see href="https://onlinetools.com/image/grayscale-image"/></para>
     /// </summary>
     public (double A, double R, double G, double B) ToArgbGrayscale(this System.Drawing.Color source, GrayscaleMethod method)
@@ -117,7 +137,7 @@ namespace Flux
     }
 
     /// <summary>
-    /// <para>Creates a scaled ARGB color of unit values from the <see cref="System.Drawing.Color"/>.</para>
+    /// <para>Creates scaled ARGB unit color values corresponding to the <see cref="System.Drawing.Color"/>.</para>
     /// </summary>
     public (double A, double R, double G, double B) ToArgbScaled(this System.Drawing.Color source, double sa, double sr, double sg, double sb)
     {
