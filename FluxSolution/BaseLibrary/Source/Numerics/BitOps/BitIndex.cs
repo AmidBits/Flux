3534de97f2adc581704ed74c86afc897c464c375
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-12 23:54:35 +0200

    Update BitIndex.cs

--- FluxSolution/BaseLibrary/Source/Numerics/BitOps/BitIndex.cs
+++ FluxSolution/BaseLibrary/Source/Numerics/BitOps/BitIndex.cs
@@ -5,28 +5,28 @@ namespace Flux
     /// <summary>
     /// <para>Clear a bit of <paramref name="source"/> based on the zero-based <paramref name="index"/>.</para>
     /// </summary>
-    public static TSelf ClearIndexBit<TSelf>(this TSelf source, int index)
+    public static TSelf BitIndexClear<TSelf>(this TSelf source, int index)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
       => source &= ~(TSelf.One << index);
 
     /// <summary>
     /// <para>Flip a bit of <paramref name="source"/> based on the zero-based <paramref name="index"/>.</para>
     /// </summary>
-    public static TSelf FlipIndexBit<TSelf>(this TSelf source, int index)
+    public static TSelf BitIndexFlip<TSelf>(this TSelf source, int index)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
       => source ^= TSelf.One << index;
 
     /// <summary>
     /// <para>Determine the state of a bit in <paramref name="source"/> based on the zero-based <paramref name="index"/>.</para>
     /// </summary>
-    public static bool GetIndexBit<TSelf>(this TSelf source, int index)
+    public static bool BitIndexGet<TSelf>(this TSelf source, int index)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
       => (source &= TSelf.One << index) != TSelf.Zero;
 
     /// <summary>
     /// <para>Set a bit of <paramref name="source"/> based on the zero-based <paramref name="index"/>.</para>
     /// </summary>
-    public static TSelf SetIndexBit<TSelf>(this TSelf source, int index)
+    public static TSelf BitIndexSet<TSelf>(this TSelf source, int index)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
       => source |= TSelf.One << index;
   }
