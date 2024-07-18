0a5c2c777f800d87f1a0042f9ddaf99ddb334bb5
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-18 14:24:58 +0200

    Update IIterable.cs

--- FluxSolution/BaseLibrary/Source/Iteration/IIterable.cs
+++ FluxSolution/BaseLibrary/Source/Iteration/IIterable.cs
@@ -3,7 +3,7 @@ namespace Flux
   public interface IIterable<TSelf>
     where TSelf : System.Numerics.INumber<TSelf>
   {
-    TSelf IterateBackward();
-    TSelf IterateForward();
+    TSelf StepBackward();
+    TSelf StepForward();
   }
 }
