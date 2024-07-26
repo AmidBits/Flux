5b065b41493ad26b6ba891e0e492302499da8a4f
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-26 02:37:19 +0200

    Create IMinMaxValue.em.cs

---
+++ FluxSolution/BaseLibrary/System/Numerics/IMinMaxValue.em.cs
@@ -0,0 +1,21 @@
+namespace Flux
+{
+  public static partial class Fx
+  {
+    /// <summary>
+    /// <para>Asserts that the <paramref name="value"/> is a member of the System.Numerics.IMinMaxValue in <paramref name="source"/> constrained by <paramref name="notation"/>. If not, it throws an exception.</para>
+    /// </summary>
+    /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+    public static TSelf AssertMember<TSelf>(this System.Numerics.IMinMaxValue<TSelf> source, TSelf value, IntervalNotation notation, string? paramName = null)
+      where TSelf : System.Numerics.IMinMaxValue<TSelf>
+      => notation.AssertValidMember(value, TSelf.CreateChecked(source.MinValue), TSelf.CreateChecked(source.MaxValue), paramName ?? nameof(value));
+
+    /// <summary>
+    /// <para>Returns whether the <paramref name="value"/> is a member of the System.Numerics.IMinMaxValue in <paramref name="source"/> constrained by <paramref name="notation"/>.</para>
+    /// </summary>
+    /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+    public static bool VerifyMember<TSelf>(this System.Numerics.IMinMaxValue<TSelf> source, TSelf value, IntervalNotation notation)
+      where TSelf : System.Numerics.IMinMaxValue<TSelf>
+      => notation.IsValidMember(value, TSelf.CreateChecked(source.MinValue), TSelf.CreateChecked(source.MaxValue));
+  }
+}
