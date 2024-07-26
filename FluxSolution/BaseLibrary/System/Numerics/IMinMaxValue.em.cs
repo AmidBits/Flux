3bd602dcd8c98d0ac3ca411b89ff8ac6859fb97b
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-26 21:32:19 +0200

    Update IMinMaxValue.em.cs

--- FluxSolution/BaseLibrary/System/Numerics/IMinMaxValue.em.cs
+++ FluxSolution/BaseLibrary/System/Numerics/IMinMaxValue.em.cs
@@ -11,11 +11,27 @@ namespace Flux
       => notation.AssertValidMember(value, TSelf.CreateChecked(source.MinValue), TSelf.CreateChecked(source.MaxValue), paramName ?? nameof(value));
 
     /// <summary>
+    /// <para>Asserts that the <paramref name="value"/> is a member of the System.Numerics.IMinMaxValue in <paramref name="source"/> constrained by IntervalNotation.Closed enum value. If not, it throws an exception.</para>
+    /// </summary>
+    /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+    public static TSelf AssertMember<TSelf>(this System.Numerics.IMinMaxValue<TSelf> source, TSelf value, string? paramName = null)
+      where TSelf : System.Numerics.IMinMaxValue<TSelf>
+      => IntervalNotation.Closed.AssertValidMember(value, TSelf.CreateChecked(source.MinValue), TSelf.CreateChecked(source.MaxValue), paramName ?? nameof(value));
+
+    /// <summary>
     /// <para>Returns whether the <paramref name="value"/> is a member of the System.Numerics.IMinMaxValue in <paramref name="source"/> constrained by <paramref name="notation"/>.</para>
     /// </summary>
     /// <exception cref="System.ArgumentOutOfRangeException"></exception>
     public static bool VerifyMember<TSelf>(this System.Numerics.IMinMaxValue<TSelf> source, TSelf value, IntervalNotation notation)
       where TSelf : System.Numerics.IMinMaxValue<TSelf>
       => notation.IsValidMember(value, TSelf.CreateChecked(source.MinValue), TSelf.CreateChecked(source.MaxValue));
+
+    /// <summary>
+    /// <para>Returns whether the <paramref name="value"/> is a member of the System.Numerics.IMinMaxValue in <paramref name="source"/> constrained by IntervalNotation.Closed enum value.</para>
+    /// </summary>
+    /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+    public static bool VerifyMember<TSelf>(this System.Numerics.IMinMaxValue<TSelf> source, TSelf value)
+      where TSelf : System.Numerics.IMinMaxValue<TSelf>
+      => IntervalNotation.Closed.IsValidMember(value, TSelf.CreateChecked(source.MinValue), TSelf.CreateChecked(source.MaxValue));
   }
 }
