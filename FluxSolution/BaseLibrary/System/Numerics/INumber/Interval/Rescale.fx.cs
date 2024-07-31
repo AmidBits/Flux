bbb8aeb391a5f302cfc9235ea40e74f2fdc49e07
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-31 20:19:13 +0200

    Update and rename Rescale.cs to Rescale.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/INumber/Interval/Rescale.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/INumber/Interval/Rescale.fx.cs
@@ -1,6 +1,6 @@
 namespace Flux
 {
-  public static partial class Maths
+  public static partial class Fx
   {
     /// <summary>Proportionally rescale the <paramref name="value"/> from the closed interval [<paramref name="minSource"/>, <paramref name="maxSource"/>] to the closed interval [<paramref name="minTarget"/>, <paramref name="maxTarget"/>]. The <paramref name="value"/> retains its proportional interval ratio, e.g. a 5 from the closed interval [0, 10] becomes 50 when rescaled to the closed interval [0, 100].</summary>
     public static TSelf Rescale<TSelf>(this TSelf value, TSelf minSource, TSelf maxSource, TSelf minTarget, TSelf maxTarget)
