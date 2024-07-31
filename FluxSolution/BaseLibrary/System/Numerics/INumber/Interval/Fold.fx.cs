12844d97632e5c28b91e2b189c71d3737bf9662d
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-31 20:17:32 +0200

    Update and rename Fold.cs to Fold.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/INumber/Interval/Fold.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/INumber/Interval/Fold.fx.cs
@@ -1,6 +1,6 @@
 namespace Flux
 {
-  public static partial class Maths
+  public static partial class Fx
   {
     /// <summary>Folds an out-of-bound <paramref name="value"/> (back and forth) over the closed interval [<paramref name="min"/>, <paramref name="max"/>], until the <paramref name="value"/> is within the closed interval.</summary>
     public static TSelf Fold<TSelf>(this TSelf value, TSelf min, TSelf max)
