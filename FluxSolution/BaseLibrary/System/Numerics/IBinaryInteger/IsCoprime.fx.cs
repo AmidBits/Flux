b038eae5e7fc572462824ab4c7616dea170705cf
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-31 20:15:44 +0200

    Update and rename IsCoprime.cs to IsCoprime.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/IsCoprime.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/IsCoprime.fx.cs
@@ -1,6 +1,6 @@
 namespace Flux
 {
-  public static partial class Maths
+  public static partial class Fx
   {
     /// <summary>Returns whether <paramref name="a"/> and <paramref name="b"/> are co-prime.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Coprime_integers"/>
