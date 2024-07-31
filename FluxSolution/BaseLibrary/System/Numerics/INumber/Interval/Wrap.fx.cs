830eb056252d90dd01dfafb9bb19bf03e1543d39
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-31 20:18:23 +0200

    Update and rename Wrap.cs to Wrap.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/INumber/Interval/Wrap.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/INumber/Interval/Wrap.fx.cs
@@ -1,6 +1,6 @@
 namespace Flux
 {
-  public static partial class Maths
+  public static partial class Fx
   {
     /// <summary>Wraps an out-of-bound <paramref name="value"/> around the closed interval [<paramref name="min"/>, <paramref name="max"/>], until the <paramref name="value"/> is within the closed interval.</summary>
     public static TSelf Wrap<TSelf>(this TSelf value, TSelf min, TSelf max)
