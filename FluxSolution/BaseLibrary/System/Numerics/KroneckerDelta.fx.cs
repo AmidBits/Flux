31a6259f0e50db3facac3fdbfc4da705346548d7
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-07-31 20:28:54 +0200

    Update and rename FluxSolution/BaseLibrary/Source/Numerics/Maths/KroneckerDelta.cs to FluxSolution/BaseLibrary/System/Numerics/KroneckerDelta.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/KroneckerDelta.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/KroneckerDelta.fx.cs
@@ -1,11 +1,11 @@
 namespace Flux
 {
-  public static partial class Maths
+  public static partial class Fx
   {
     /// <summary>Usually used with integers, but can really be applied to any equatable object.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Kronecker_delta"/>
     public static TSelf KroneckerDelta<TSelf>(this TSelf a, TSelf b)
-      where TSelf : System.Numerics.INumber<TSelf>
+      where TSelf : System.Numerics.INumberBase<TSelf>
       => a == b ? TSelf.One : TSelf.Zero;
   }
 }
