5d67323384fc96a787f6823a3d134b5bc09ca9e5
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-01 16:29:26 +0200

    Update and rename FluxSolution/BaseLibrary/System/Numerics/Modular.fx.cs to FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/Modular.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/Modular.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/Modular.fx.cs
@@ -2,25 +2,6 @@ namespace Flux
 {
   public static partial class Fx
   {
-    /// <summary>Returns the integer (i.e. floor/truncate for floating point) quotient and also returns the <paramref name="remainder"/> (<paramref name="dividend"/> modulo <paramref name="divisor"/>) as an output parameter.</summary>
-    /// <remarks>This function is equivalent to the DivRem() function, only provided for here for all INumber<> types.</remarks>
-    public static TSelf DivMod<TSelf>(this TSelf dividend, TSelf divisor, out TSelf remainder)
-      where TSelf : System.Numerics.INumber<TSelf>
-    {
-      remainder = dividend % divisor;
-
-      return dividend / divisor;
-    }
-
-    /// <summary>Returns the full quotient and also returns the <paramref name="remainder"/> (<paramref name="dividend"/> modulo <paramref name="divisor"/>) and integer (i.e. truncated/floor) quotient <paramref name="truncatedQuotient"/> as output parameters.</summary>
-    public static TSelf DivModTrunc<TSelf>(this TSelf dividend, TSelf divisor, out TSelf remainder, out TSelf truncatedQuotient)
-      where TSelf : System.Numerics.INumber<TSelf>
-    {
-      truncatedQuotient = TruncMod(dividend, divisor, out remainder);
-
-      return dividend / divisor;
-    }
-
     /// <summary>Returns the remainder of the Euclidean division of <paramref name="dividend"/> and <paramref name="divisor"/>, i.e. the remainder of modular division of a and b.</summary>
     public static TSelf Mod<TSelf>(this TSelf dividend, TSelf divisor)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
@@ -135,20 +116,6 @@ namespace Flux
       remainder = dividend % divisor;
 
       return TSelf.IsZero(remainder) ? remainder : -remainder + divisor;
-    }
-
-    ///// <summary>Computes the integer (i.e. truncated/floor) quotient.</summary>
-    //public static TSelf TruncMod<TSelf>(this TSelf dividend, TSelf divisor)
-    //  where TSelf : System.Numerics.INumber<TSelf>
-    //  => (dividend - (dividend % divisor)) / divisor;
-
-    /// <summary>Computes the integer (i.e. truncated/floor) quotient and also returns the <paramref name="remainder"/> (<paramref name="dividend"/> modulo <paramref name="divisor"/>) as an output parameter.</summary>
-    public static TSelf TruncMod<TSelf>(this TSelf dividend, TSelf divisor, out TSelf remainder)
-      where TSelf : System.Numerics.INumber<TSelf>
-    {
-      remainder = dividend % divisor;
-
-      return (dividend - remainder) / divisor;
     }
   }
 }
