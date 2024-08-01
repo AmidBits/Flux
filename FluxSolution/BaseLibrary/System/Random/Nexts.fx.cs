e71c459dc372ca8bf277afae487de7193c013282
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-01 15:04:27 +0200

    Update Nexts.fx.cs

--- FluxSolution/BaseLibrary/System/Random/Nexts.fx.cs
+++ FluxSolution/BaseLibrary/System/Random/Nexts.fx.cs
@@ -6,16 +6,6 @@ namespace Flux
   {
     // https://docs.microsoft.com/en-us/dotnet/api/system.random?view=netstandard-2.0
 
-    /// <summary>Generates an array with the specified <paramref name="count"/> of random bytes.</summary>
-    public static byte[] GetRandomBytes(this System.Random source, int count)
-    {
-      System.ArgumentNullException.ThrowIfNull(source);
-
-      var buffer = new byte[count];
-      source.NextBytes(buffer);
-      return buffer;
-    }
-
     /// <summary>Returns a non-negative random BigInteger that is less than the specified <paramref name="maxValue"/>.</summary>
     public static System.Numerics.BigInteger NextBigInteger(this System.Random source, System.Numerics.BigInteger maxValue)
     {
