66b84bb59f0b2f12f745214a1e711dbf89b7b3f5
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-02 00:26:34 +0200

    Update and rename StepFunctions.fx.cs to StepFunctions.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/INumber/StepFunctions.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/INumber/StepFunctions.fx.cs
@@ -15,15 +15,15 @@ namespace Flux
     /// <see href="https://en.wikipedia.org/wiki/Step_function"/>
     /// <seealso href="https://en.wikipedia.org/wiki/Sign_function"/>
     public static TSelf Sign<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ISignedNumber<TSelf>
-      => TSelf.IsNegative(x) ? -TSelf.One : TSelf.IsZero(x) ? TSelf.Zero : TSelf.One;
+      where TSelf : System.Numerics.INumber<TSelf>
+      => TSelf.Sign(x);
 
     /// <summary>The unit sign step function.</summary>
     /// <remarks>LT 0 = -1, GTE 0 = +1.</remarks>
     /// <see href="https://en.wikipedia.org/wiki/Step_function"/>
     /// <seealso href="https://en.wikipedia.org/wiki/Sign_function"/>
     public static TSelf UnitSign<TSelf>(this TSelf x)
-      where TSelf : System.Numerics.ISignedNumber<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>
       => TSelf.IsNegative(x) ? -TSelf.One : TSelf.One;
   }
 }
