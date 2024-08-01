5bc849b23f43ca1eb35e5d5734b4a95236f010fb
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-02 01:03:22 +0200

    Update and rename KroneckerDelta.fx.cs to KroneckerDelta.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/INumber/KroneckerDelta.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/INumber/KroneckerDelta.fx.cs
@@ -5,7 +5,7 @@ namespace Flux
     /// <summary>Usually used with integers, but can really be applied to any equatable object.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Kronecker_delta"/>
     public static TSelf KroneckerDelta<TSelf>(this TSelf a, TSelf b)
-      where TSelf : System.Numerics.INumberBase<TSelf>
+      where TSelf : System.Numerics.INumber<TSelf>
       => a == b ? TSelf.One : TSelf.Zero;
   }
 }
