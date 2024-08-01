2ef026c4d4291ce8bc5e700e35b9b4cbbd270e1c
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-01 15:07:43 +0200

    Update and rename FluxSolution/BaseLibrary/System/Environment.em.cs to FluxSolution/BaseLibrary/System/Environment/SpecialFolder/GetDirectoryInfo.fx.cs

--- FluxSolution/BaseLibrary/System/Environment.em.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Flux
-{
-  public static partial class Fx
-  {
-    /// <summary>Returns a <see cref="System.IO.DirectoryInfo"/> object for the specified <see cref="System.Environment.SpecialFolder"/>.</summary>
-    public static System.IO.DirectoryInfo? GetDirectoryInfo(this System.Environment.SpecialFolder specialFolder)
-      => System.Environment.GetFolderPath(specialFolder) is var fp && string.IsNullOrEmpty(fp) ? default : new System.IO.DirectoryInfo(fp);
-  }
-}
