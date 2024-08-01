d03c759f1284c717ae275652d80542f3a2415528
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-02 00:58:53 +0200

    Update MinMaxFunctions.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/IMinMaxValue/MinMaxFunctions.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/IMinMaxValue/MinMaxFunctions.fx.cs
@@ -5,21 +5,21 @@ namespace Flux
     /// <summary>
     /// <para>The centre of the System.Numerics.IMinMaxValue in <paramref name="source"/>.</para>
     /// </summary>
-    public static TSelf MinMaxCentre<TSelf>(this TSelf source)
+    public static TSelf MinMaxIntervalCentre<TSelf>(this TSelf source)
       where TSelf : System.Numerics.IMinMaxValue<TSelf>
       => (source.MinValue + source.MaxValue) / TSelf.CreateChecked(2);
 
     /// <summary>
-    /// <para>The diameter of the System.Numerics.IMinMaxValue in <paramref name="source"/>.</para>
+    /// <para>The length (a.k.a. diameter, range, size) of the System.Numerics.IMinMaxValue in <paramref name="source"/>.</para>
     /// </summary>
-    public static TSelf MinMaxDiameter<TSelf>(this TSelf source)
+    public static TSelf MinMaxIntervalLength<TSelf>(this TSelf source)
       where TSelf : System.Numerics.IMinMaxValue<TSelf>
       => TSelf.Abs(source.MinValue - source.MaxValue);
 
     /// <summary>
     /// <para>The radius of the System.Numerics.IMinMaxValue in <paramref name="source"/>.</para>
     /// </summary>
-    public static TSelf MinMaxRadius<TSelf>(this TSelf source)
+    public static TSelf MinMaxIntervalRadius<TSelf>(this TSelf source)
       where TSelf : System.Numerics.IMinMaxValue<TSelf>
       => TSelf.Abs(source.MinValue - source.MaxValue) / TSelf.CreateChecked(2);
   }
