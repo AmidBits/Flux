f15d61de4bb6d2947cb7cdd76414745698b174d2
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-02 01:02:10 +0200

    Update ToBigInteger.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/ToBigInteger.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/IBinaryInteger/ToBigInteger.fx.cs
@@ -6,10 +6,5 @@ namespace Flux
     public static System.Numerics.BigInteger ToBigInteger<TSelf>(this TSelf value)
       where TSelf : System.Numerics.IBinaryInteger<TSelf>
       => System.Numerics.BigInteger.CreateChecked(value);
-
-    /// <summary>Returns the <paramref name="value"/> (of floating-point type <typeparamref name="TSelf"/>) as a <see cref="System.Numerics.BigInteger"/> using the rounding <paramref name="mode"/>.</summary>
-    public static System.Numerics.BigInteger ToBigInteger<TSelf>(this TSelf value, RoundingMode mode)
-      where TSelf : System.Numerics.IFloatingPoint<TSelf>
-      => System.Numerics.BigInteger.CreateChecked(value.Round(mode));
   }
 }
