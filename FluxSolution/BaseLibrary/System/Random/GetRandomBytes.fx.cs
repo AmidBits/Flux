6840da7da03c01aa717cacd9cace3cd30da35af2
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-01 15:05:11 +0200

    Update GetRandomBytes.fx.cs

--- FluxSolution/BaseLibrary/System/Random/GetRandomBytes.fx.cs
+++ FluxSolution/BaseLibrary/System/Random/GetRandomBytes.fx.cs
@@ -1,12 +1,10 @@
-//Implement this (down a bit in the article) : https://stackoverflow.com/questions/218060/random-gaussian-variables
-
 namespace Flux
 {
   public static partial class Fx
   {
-    // https://docs.microsoft.com/en-us/dotnet/api/system.random?view=netstandard-2.0
-
-    /// <summary>Generates an array with the specified <paramref name="count"/> of random bytes.</summary>
+    /// <summary>
+    /// <para>Generates an array with the specified <paramref name="count"/> of random bytes.</para>
+    /// </summary>
     public static byte[] GetRandomBytes(this System.Random source, int count)
     {
       System.ArgumentNullException.ThrowIfNull(source);
