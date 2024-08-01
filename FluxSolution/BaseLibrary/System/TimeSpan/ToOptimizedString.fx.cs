3e936b9219e9dc625a68f5cd3329ae5585515e0e
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-01 15:19:00 +0200

    Update ToOptimizedString.fx.cs

--- FluxSolution/BaseLibrary/System/TimeSpan/ToOptimizedString.fx.cs
+++ FluxSolution/BaseLibrary/System/TimeSpan/ToOptimizedString.fx.cs
@@ -5,6 +5,7 @@ namespace Flux
     [System.Text.RegularExpressions.GeneratedRegex(@"^(00.)(00\:)+0+(?=.)")]
     private static partial System.Text.RegularExpressions.Regex RegexTimeSpanOptimizedString();
 
-    public static string ToOptimizedString(this System.TimeSpan source) => RegexTimeSpanOptimizedString().Replace(source.ToString(), string.Empty);
+    public static string ToOptimizedString(this System.TimeSpan source)
+      => RegexTimeSpanOptimizedString().Replace(source.ToString(), string.Empty);
   }
 }
