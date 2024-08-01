402592099a3204b04fd0a90093fa98255070076a
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-02 00:42:58 +0200

    Update and rename FluxSolution/BaseLibrary/System/Numerics/HelmertsExpansionParameterK1.fx.cs to FluxSolution/BaseLibrary/System/Numerics/IFloatingPoint/HelmertsExpansionParameterK1.fx.cs

--- FluxSolution/BaseLibrary/System/Numerics/IFloatingPoint/HelmertsExpansionParameterK1.fx.cs
+++ FluxSolution/BaseLibrary/System/Numerics/IFloatingPoint/HelmertsExpansionParameterK1.fx.cs
@@ -4,7 +4,7 @@ namespace Flux
   {
     /// <summary>https://en.wikipedia.org/wiki/Vincenty%27s_formulae</summary>
     public static TSelf HelmertsExpansionParameterK1<TSelf>(this TSelf u)
-      where TSelf : System.Numerics.INumberBase<TSelf>, System.Numerics.IRootFunctions<TSelf>
+      where TSelf : System.Numerics.IFloatingPoint<TSelf>, System.Numerics.IRootFunctions<TSelf>
       => TSelf.Sqrt(TSelf.One + u * u) is var k ? (k - TSelf.One) / (k + TSelf.One) : throw new System.ArithmeticException();
   }
 }
