d558b5b8ace7f828f0b3be25774dcc84d0abe4c7
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-04 08:40:51 +0200

    Update ToXsdString.fx.cs

--- FluxSolution/BaseLibrary/System/TimeSpan/ToXsdString.fx.cs
+++ FluxSolution/BaseLibrary/System/TimeSpan/ToXsdString.fx.cs
@@ -2,9 +2,9 @@ namespace Flux
 {
   public static partial class Fx
   {
-    public static System.Text.StringBuilder ToXsdString(this System.TimeSpan source)
+	  public static string ToXsdString(System.TimeSpan source)
     {
-      var sb = new System.Text.StringBuilder(24);
+      var sb = new System.Text.StringBuilder();
 
       if (source.Ticks < 0)
         sb.Append('-');
@@ -14,33 +14,45 @@ namespace Flux
       if (System.Math.Abs(source.Days) is var days && days > 0)
         sb.Append(days).Append('D');
 
-      if (System.Math.Abs(source.Hours) is var h && System.Math.Abs(source.Minutes) is var m && System.Math.Abs(source.Seconds) is var s && System.Math.Abs(source.Milliseconds) is var ms && (h > 0 || m > 0 || s > 0 || ms > 0))
+      if (
+        System.Math.Abs(source.Hours) is var h
+  		  && System.Math.Abs(source.Minutes) is var m
+  		  && System.Math.Abs(source.Seconds) is var s
+  		  && System.Math.Abs(source.Milliseconds) is var milli
+  		  && System.Math.Abs(source.Microseconds) is var micro
+  		  && System.Math.Abs(source.Nanoseconds) is var nano
+  		  && (h != 0 || m != 0 || s != 0 || milli != 0 || micro != 0 || nano != 0)
+  		)
       {
         sb.Append('T');
 
-        if (h > 0)
+        if (h != 0)
           sb.Append(h).Append('H');
 
-        if (m > 0)
+        if (m != 0)
           sb.Append(m).Append('M');
 
-        if (s > 0 || ms > 0)
+        if (s != 0 || milli != 0 || micro != 0 || nano != 0)
         {
           sb.Append(s);
 
-          if (ms > 0)
-            sb.Append('.').Append(ms);
-
+    		  if(milli != 0 || micro != 0 || nano != 0)
+          {
+    			  sb.Append('.');
+    
+    			  if (milli != 0)
+    				  sb.Append(milli.ToString("D3"));
+    			  if (micro != 0)
+    				  sb.Append(micro.ToString("D3"));
+    			  if (nano != 0)
+    				  sb.Append(nano.ToString("D3"));
+          }
+    
           sb.Append('S');
         }
       }
 
-      return sb;
+      return sb.ToString();
     }
-
-    public static System.Text.StringBuilder ToXsdBasicString(this System.TimeSpan source)
-      => new System.Text.StringBuilder(source.Ticks < 0 ? "-" : string.Empty).Append("P000000").AppendFormat("{0:D2}", System.Math.Abs(source.Days)).Append('T').AppendFormat("{0:D2}", System.Math.Abs(source.Hours)).AppendFormat("{0:D2}", System.Math.Abs(source.Minutes)).AppendFormat("{0:D2}", System.Math.Abs(source.Seconds));
-    public static System.Text.StringBuilder ToXsdBasicExtendedString(this System.TimeSpan source)
-      => new System.Text.StringBuilder(source.Ticks < 0 ? "-" : string.Empty).Append("P0000-00-").AppendFormat("{0:D2}", System.Math.Abs(source.Days)).Append('T').AppendFormat("{0:D2}", System.Math.Abs(source.Hours)).AppendFormat("{0:D2}", System.Math.Abs(source.Minutes)).AppendFormat("{0:D2}", System.Math.Abs(source.Seconds));
   }
 }
