420c4e041e68b8a4536894e5942999ef3b135999
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-04 14:34:50 +0200

    Update UnicodeNotation.em.cs

--- FluxSolution/BaseLibrary/Source/Unicode/UnicodeNotation.em.cs
+++ FluxSolution/BaseLibrary/Source/Unicode/UnicodeNotation.em.cs
@@ -1,16 +1,5 @@
 namespace Flux
 {
-  ///// <summary></summary>
-  ///// <remarks>
-  ///// <para>A code point is a Unicode number representing a defined meaning. One or more code points may be used to represent higher order constructs, e.g. a grapheme.</para>
-  ///// <para>In .NET, the <see cref="System.Text.Rune"/> represents a Unicode code point.</para>
-  ///// <para>A code unit is a unit of storage for encoding code points. E.g. UTF-16 is a 16-bit code unit. One or more code units may be used to represent a code point.</para>
-  ///// <para>In .NET, the type <see cref="char"/> is a code unit identified as UTF-16. Multiple <see cref="char"/>s are used to represent larger constructs, e.g. code points (<see cref="System.Text.Rune"/>) and graphemes (text elements).</para>
-  ///// <para>A grapheme is one or more code points representing an element of writing.</para>
-  ///// <para>In .NET, a grapheme is a text element represented as a sequence of <see cref="char"/> instances, e.g. in a <see cref="string"/>.</para>
-  ///// <para>A glyph is a visual "image", e.g. in a font, used to represent visual "symbols". One or more glyphs may be used to represent a grapheme.</para>
-  ///// </remarks>
-
   public static partial class Unicode
   {
 #if NET7_0_OR_GREATER
