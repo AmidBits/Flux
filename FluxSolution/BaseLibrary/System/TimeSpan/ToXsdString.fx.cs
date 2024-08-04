793ded1e929f7163e50398065f4c252c7a4ea327
AmidBits <65741552+AmidBits@users.noreply.github.com>
2024-08-04 08:42:59 +0200

    Update ToXsdString.fx.cs

--- FluxSolution/BaseLibrary/System/TimeSpan/ToXsdString.fx.cs
+++ FluxSolution/BaseLibrary/System/TimeSpan/ToXsdString.fx.cs
@@ -2,7 +2,7 @@ namespace Flux
 {
   public static partial class Fx
   {
-	  public static string ToXsdString(System.TimeSpan source)
+    public static string ToXsdString(this System.TimeSpan source)
     {
       var sb = new System.Text.StringBuilder();
 
@@ -16,13 +16,13 @@ namespace Flux
 
       if (
         System.Math.Abs(source.Hours) is var h
-  		  && System.Math.Abs(source.Minutes) is var m
-  		  && System.Math.Abs(source.Seconds) is var s
-  		  && System.Math.Abs(source.Milliseconds) is var milli
-  		  && System.Math.Abs(source.Microseconds) is var micro
-  		  && System.Math.Abs(source.Nanoseconds) is var nano
-  		  && (h != 0 || m != 0 || s != 0 || milli != 0 || micro != 0 || nano != 0)
-  		)
+	&& System.Math.Abs(source.Minutes) is var m
+	&& System.Math.Abs(source.Seconds) is var s
+	&& System.Math.Abs(source.Milliseconds) is var milli
+	&& System.Math.Abs(source.Microseconds) is var micro
+	&& System.Math.Abs(source.Nanoseconds) is var nano
+	&& (h != 0 || m != 0 || s != 0 || milli != 0 || micro != 0 || nano != 0)
+      )
       {
         sb.Append('T');
 
@@ -36,16 +36,16 @@ namespace Flux
         {
           sb.Append(s);
 
-    		  if(milli != 0 || micro != 0 || nano != 0)
+    	  if(milli != 0 || micro != 0 || nano != 0)
           {
-    			  sb.Append('.');
-    
-    			  if (milli != 0)
-    				  sb.Append(milli.ToString("D3"));
-    			  if (micro != 0)
-    				  sb.Append(micro.ToString("D3"));
-    			  if (nano != 0)
-    				  sb.Append(nano.ToString("D3"));
+	    sb.Append('.');
+		
+	    if (milli != 0)
+	      sb.Append(milli.ToString("D3"));
+	    if (micro != 0)
+	      sb.Append(micro.ToString("D3"));
+	    if (nano != 0)
+	      sb.Append(nano.ToString("D3"));
           }
     
           sb.Append('S');
