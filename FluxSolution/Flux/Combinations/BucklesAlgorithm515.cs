dc5d1c91e45e22666f09ba610987610664ef44b7
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-07-16 10:52:58 +0200

    Update BucklesAlgorithm515.cs

--- FluxSolution/Flux/Combinations/BucklesAlgorithm515.cs
+++ FluxSolution/Flux/Combinations/BucklesAlgorithm515.cs
@@ -206,3 +206,108 @@ namespace Flux.Combinations
     }
   }
 }
+
+/*
+public record class BucklesAlgorithm515
+{
+    /// <summary>
+    /// <para>Find the permutation for a <paramref name="lexiographicIndex"/> of a subset with <paramref name="source"/>.Length (P) elements, in a set with <paramref name="alphabetLength"/> (N) elements.</para>
+    /// <para>Selects a subset of order <paramref name="source"/>.Length (P) from a set of order <paramref name="alphabetLength"/> (N). This subroutine finds the combination set of <paramref name="alphabetLength"/> (N) things taken <paramref name="source"/>.Length (P) at a time for a given <paramref name="lexiographicIndex"/>.</para>
+    /// <para><seealso href="https://github.com/sleeepyjack/alg515"/></para>
+    /// <para><seealso href="https://stackoverflow.com/questions/561/how-to-use-combinations-of-sets-as-test-data#794"/></para>
+    /// <para><seealso href="https://people.math.sc.edu/Burkardt/f_src/toms515/toms515.html"/></para>
+    /// <code><![CDATA[
+    /// var(n, p) = (5, 3);
+    /// 
+    /// var nCp = n.BinomialCoefficient(p);
+    /// 
+    /// System.Console.WriteLine($"# of distinct combinations: {nCp}");
+    /// 
+    /// foreach (var l in System.Linq.Enumerable.Range(0, nCp))
+    ///   System.Console.WriteLine($"{l} : [{string.Join(", ", n.PermuteAlgorithm515b(p, l))}]");
+    ///   
+    /// # of distinct combinations: 10
+    /// [0, 1, 2]
+    /// [0, 1, 3]
+    /// [0, 1, 4]
+    /// [0, 2, 3]
+    /// [0, 2, 4]
+    /// [0, 3, 4]
+    /// [1, 2, 3]
+    /// [1, 2, 4]
+    /// [1, 3, 4]
+    /// [2, 3, 4]
+    /// ]]></code>
+    /// </summary>
+    /// <param name="source">The combination storage, and therefor the subset size (K).</param>
+    /// <param name="alphabetLength">The size (N) of the entire set.</param>
+    /// <param name="lexiographicIndex">A 0-based index of the subset (or index array) to generate.</param>
+    /// <returns></returns>
+    public static void GetCombination(System.Span<int> source, int alphabetLength, int lexiographicIndex)
+    {
+      var x = 1;
+      var r = (alphabetLength - x).BinomialCoefficient(source.Length - 1);
+      var k = r;
+      
+      while (k <= lexiographicIndex)
+      {
+        x += 1;
+        r = (alphabetLength - x).BinomialCoefficient(source.Length - 1);
+        k += r;
+      }
+
+      k -= r;
+
+      source[0] = x - 1;
+
+      for (var i = 2; i < source.Length; i++)
+      {
+        x += 1;
+        r = (alphabetLength - x).BinomialCoefficient(source.Length - i);
+        k += r;
+
+        while (k <= lexiographicIndex)
+        {
+          x += 1;
+          r = (alphabetLength - x).BinomialCoefficient(source.Length - i);
+          k += r;
+        }
+
+        k -= r;
+
+        source[i - 1] = x - 1;
+      }
+
+      source[source.Length - 1] = x + lexiographicIndex - k;
+    }
+
+    /// <summary>
+    /// <para>Yields a new sequence, in lexiographical order, of subset combinations from <paramref name="source"/> of the <paramref name="source"/> data.</para>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source">The alphabet.</param>
+    /// <param name="combinationLength">The length of each combination.</param>
+    /// <returns></returns>
+    public static System.Collections.Generic.IEnumerable<T[]> GetCombinations<T>(T[] source, int combinationLength)
+    {
+		var indices = new int[combinationLength];
+		
+		for(var i = 0; i < combinationLength; i++)
+			indices[i] = i;
+		
+      	var count = (source.Length).BinomialCoefficient(combinationLength);
+
+      	var permutation = new T[combinationLength];
+
+		for (var l = 0; l < count; l++)
+      	{
+        	GetCombination(indices, source.Length, l);
+
+			for(var i = 0; i < combinationLength; i++)
+				permutation[i] = source[indices[i]];
+			
+        	yield return permutation;
+      	}
+    }
+}
+*/
