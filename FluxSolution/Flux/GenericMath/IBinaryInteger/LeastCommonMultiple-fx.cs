eacfb5f8fc51cd75b8d9f5389fb8a1d0292dae4d
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-07-26 07:51:25 +0200

    Update LeastCommonMultiple-fx.cs

--- FluxSolution/Flux/GenericMath/IBinaryInteger/LeastCommonMultiple-fx.cs
+++ FluxSolution/Flux/GenericMath/IBinaryInteger/LeastCommonMultiple-fx.cs
@@ -4,16 +4,16 @@ namespace Flux
   {
     /// <summary>Returns the least common multiple of all values.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Least_common_multiple"/>
-    public static TNumber Lcm<TNumber>(this TNumber a, params TNumber[] other)
-      where TNumber : System.Numerics.IBinaryInteger<TNumber>
+    public static TInteger Lcm<TInteger>(this TInteger a, params TInteger[] other)
+      where TInteger : System.Numerics.IBinaryInteger<TInteger>
       => other.Length >= 1
       ? a.LeastCommonMultiple(other.Aggregate((b, c) => b.LeastCommonMultiple(c)))
       : throw new System.ArgumentOutOfRangeException(nameof(other));
 
     /// <summary>Returns the least common multiple of <paramref name="a"/> and <paramref name="b"/>.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Least_common_multiple"/>
-    public static TNumber LeastCommonMultiple<TNumber>(this TNumber a, TNumber b)
-      where TNumber : System.Numerics.IBinaryInteger<TNumber>
+    public static TInteger LeastCommonMultiple<TInteger>(this TInteger a, TInteger b)
+      where TInteger : System.Numerics.IBinaryInteger<TInteger>
       => a / a.GreatestCommonDivisor(b) * b;
   }
 }
