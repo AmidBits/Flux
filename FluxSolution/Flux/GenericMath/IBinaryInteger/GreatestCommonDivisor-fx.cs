1bfdc07154ec0b3a02729d0270d598ddaf7f26af
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-07-26 07:51:05 +0200

    Update GreatestCommonDivisor-fx.cs

--- FluxSolution/Flux/GenericMath/IBinaryInteger/GreatestCommonDivisor-fx.cs
+++ FluxSolution/Flux/GenericMath/IBinaryInteger/GreatestCommonDivisor-fx.cs
@@ -4,44 +4,44 @@ namespace Flux
   {
     /// <summary>Returns the greatest common divisor of all values.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Greatest_common_divisor"/>
-    public static TNumber Gcd<TNumber>(this TNumber a, params TNumber[] other)
-      where TNumber : System.Numerics.IBinaryInteger<TNumber>
+    public static TInteger Gcd<TInteger>(this TInteger a, params TInteger[] other)
+      where TInteger : System.Numerics.IBinaryInteger<TInteger>
       => other.Length >= 1
       ? a.GreatestCommonDivisor(other.Aggregate((b, c) => b.GreatestCommonDivisor(c)))
       : throw new System.ArgumentOutOfRangeException(nameof(other));
 
     /// <summary>Returns the greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Greatest_common_divisor"/>
-    public static TNumber GreatestCommonDivisor<TNumber>(this TNumber a, TNumber b)
-      where TNumber : System.Numerics.IBinaryInteger<TNumber>
+    public static TInteger GreatestCommonDivisor<TInteger>(this TInteger a, TInteger b)
+      where TInteger : System.Numerics.IBinaryInteger<TInteger>
     {
-      while (b != TNumber.Zero)
+      while (b != TInteger.Zero)
       {
         var t = b;
         b = a % b;
         a = t;
       }
 
-      return TNumber.Abs(a);
+      return TInteger.Abs(a);
     }
 
     /// <summary>The extended GCD (or Euclidean algorithm) yields in addition the GCD of <paramref name="a"/> and <paramref name="b"/>, also the addition the coefficients of Bézout's identity.</summary>
     /// <remarks>When a and b are coprime (i.e. GCD equals 1), x is the modular multiplicative inverse of a modulo b, and y is the modular multiplicative inverse of b modulo a.</remarks>
     /// <see href="https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm"/>
     /// <seealso href="https://en.wikipedia.org/wiki/Bézout%27s_identity"/>
-    public static TNumber GreatestCommonDivisorEx<TNumber>(this TNumber a, TNumber b, out TNumber x, out TNumber y)
-      where TNumber : System.Numerics.IBinaryInteger<TNumber>
+    public static TInteger GreatestCommonDivisorEx<TInteger>(this TInteger a, TInteger b, out TInteger x, out TInteger y)
+      where TInteger : System.Numerics.IBinaryInteger<TInteger>
     {
-      if (a < TNumber.Zero) throw new System.ArgumentOutOfRangeException(nameof(a));
-      if (b < TNumber.Zero) throw new System.ArgumentOutOfRangeException(nameof(b));
+      if (a < TInteger.Zero) throw new System.ArgumentOutOfRangeException(nameof(a));
+      if (b < TInteger.Zero) throw new System.ArgumentOutOfRangeException(nameof(b));
 
-      x = TNumber.One;
-      y = TNumber.Zero;
+      x = TInteger.One;
+      y = TInteger.Zero;
 
-      var u = TNumber.Zero;
-      var v = TNumber.One;
+      var u = TInteger.Zero;
+      var v = TInteger.One;
 
-      while (!TNumber.IsZero(b))
+      while (!TInteger.IsZero(b))
       {
         a = b;
         b = a % b;
