394f8ffdb0a4d15026eeae52898a3e5935f972f1
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-07-26 07:51:53 +0200

    Update IsCoprime-fx.cs

--- FluxSolution/Flux/GenericMath/IBinaryInteger/IsCoprime-fx.cs
+++ FluxSolution/Flux/GenericMath/IBinaryInteger/IsCoprime-fx.cs
@@ -4,8 +4,8 @@ namespace Flux
   {
     /// <summary>Returns whether <paramref name="a"/> and <paramref name="b"/> are co-prime.</summary>
     /// <see href="https://en.wikipedia.org/wiki/Coprime_integers"/>
-    public static bool IsCoprime<TNumber>(this TNumber a, TNumber b)
-      where TNumber : System.Numerics.IBinaryInteger<TNumber>
-      => GreatestCommonDivisor(a, b) == TNumber.One;
+    public static bool IsCoprime<TInteger>(this TInteger a, TInteger b)
+      where TInteger : System.Numerics.IBinaryInteger<TInteger>
+      => GreatestCommonDivisor(a, b) == TInteger.One;
   }
 }
