f337163cbad0b0b71e98b135362427e7fcc6b525
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-07-26 15:57:29 +0200

    Update SortOrder.enum.cs

--- FluxSolution/Flux/SortOrder.enum.cs
+++ FluxSolution/Flux/SortOrder.enum.cs
@@ -1,10 +1,11 @@
 ﻿namespace Flux
 {
+  // Follows convention from the "System.Windows.Forms.SortOrder" enum.
   public enum SortOrder
   {
     /// <summary>The order is ascending, i.e. going from low-to-high or first-to-last.</summary>
-    Ascending,
+    Ascending = 1,
     /// <summary>The order is descending, i.e. going from high-to-low or last-to-first.</summary>
-    Descending
+    Descending = 2
   }
 }
