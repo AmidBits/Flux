d32ebf94894da85461c8710e3b224823344671e8
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-07-29 22:51:33 +0200

    Update BucklesAlgorithm515.cs

--- FluxSolution/Flux/Combinations/BucklesAlgorithm515.cs
+++ FluxSolution/Flux/Combinations/BucklesAlgorithm515.cs
@@ -1,5 +1,43 @@
 namespace Flux.Combinations
 {
+	/// <summary>
+	/// <para>Find the permutation for a <paramref name="lexiographicIndex"/> of a subset with <paramref name="combinationLength"/> (P) elements, in a set with <paramref name="alphabetLength"/> (N) elements.</para>
+	/// <para>Selects a subset of order <paramref name="combinationLength"/> (P) from a set of order <paramref name="alphabetLength"/> (N). This subroutine finds the combination set of <paramref name="alphabetLength"/> (N) things taken <paramref name="combinationLength"/> (P) at a time for a given <paramref name="lexiographicIndex"/>.</para>
+	/// <para><seealso href="https://github.com/sleeepyjack/alg515"/></para>
+	/// <para><seealso href="https://stackoverflow.com/questions/561/how-to-use-combinations-of-sets-as-test-data#794"/></para>
+	/// <para><seealso href="https://people.math.sc.edu/Burkardt/f_src/toms515/toms515.html"/></para>
+	/// <code><![CDATA[
+	/// var(n, p) = (5, 3);
+	/// 
+	/// var nCp = n.BinomialCoefficient(p);
+	/// 
+	/// System.Console.WriteLine($"# of distinct combinations: {nCp}");
+	/// 
+	/// foreach (var l in System.Linq.Enumerable.Range(0, nCp))
+	///   System.Console.WriteLine($"{l} : [{string.Join(", ", n.PermuteAlgorithm515b(p, l))}]");
+	///   
+	/// # of distinct combinations: 10
+	/// [0, 1, 2]
+	/// [0, 1, 3]
+	/// [0, 1, 4]
+	/// [0, 2, 3]
+	/// [0, 2, 4]
+	/// [0, 3, 4]
+	/// [1, 2, 3]
+	/// [1, 2, 4]
+	/// [1, 3, 4]
+	/// [2, 3, 4]
+	/// ]]></code>
+	/// </summary>
+	/// <typeparam name="TNumber"></typeparam>
+	/// <param name="alphabetLength">The size (N) of the entire set.</param>
+	/// <param name="combinationLength">The subset size (K).</param>
+	/// <param name="lexiographicIndex">The 1-based index of the subset (or index array) to generate.</param>
+	/// <returns></returns>
+	/// <remarks>
+	/// <para>Note that <paramref name="lexiographicIndex"/> is 1-based.</para>
+	/// <para>The <paramref name="alphabetLength"/> is the <see langword="this"/> argument for the extension method.</para>
+	/// </remarks>
   public static partial class BucklesAlgorithm515
   {
     #region Extension methods
