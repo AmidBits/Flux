cb97fc67944ae9249e7b242105c38a15c15e42df
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-08-05 01:08:42 +0200

    Update LehmerCode.cs

--- FluxSolution/Flux/Permutations/LehmerCode.cs
+++ FluxSolution/Flux/Permutations/LehmerCode.cs
@@ -2,6 +2,19 @@ namespace Flux.Permutations
 {
   public static class LehmerCode
   {
+  	public static System.Collections.Generic.List<T> NthPermutation<T>(T[] atoms, int index, int size)
+  	{
+  		var result = new System.Collections.Generic.List<T>();
+  		for (var i = 0; i < size; i++)
+  		{
+  			var item = index % atoms.Length;
+  			index /= atoms.Length;
+  			result.Add(atoms[item]);
+  			RemoveInPlace(ref atoms, item, 1);
+  		}
+  		return result;
+  	}
+    
     // https://stackoverflow.com/a/24257996
     // https://stackoverflow.com/a/24257996
     public static int Pack(System.ReadOnlySpan<int> digits, System.ReadOnlySpan<int> radixes)
