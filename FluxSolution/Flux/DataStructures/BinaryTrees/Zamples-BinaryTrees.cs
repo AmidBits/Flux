985144a999a28972b7de5e53289a5e863588adfc
AmidBits <65741552+AmidBits@users.noreply.github.com>
2025-08-06 19:34:33 +0200

    Create Zamples-BinaryTrees.cs

---
+++ FluxSolution/Flux/DataStructures/BinaryTrees/Zamples-BinaryTrees.cs
@@ -0,0 +1,26 @@
+#if DEBUG
+namespace Flux
+{
+  public static partial class Zamples
+  {
+    public static void DataStructuresImmutableAvlTree()
+    {
+      var bst = Flux.DataStructures.Immutable.ImmutableAvlTree<int, string>.Empty;
+
+      for (var index = 0; bst.GetNodeCount() < 16; index++)
+      {
+        var r = System.Security.Cryptography.RandomNumberGenerator.GetInt32(0, 31);
+
+        if (!bst.Contains(r))
+          bst = bst.Add(r, r.ToEnglishCardinalNumeralCompoundString());
+      }
+
+      System.Console.WriteLine(bst.ToConsoleBlock().ToString());
+
+      var counter = 0;
+      foreach (var item in bst.TraverseDfsInOrder())
+        System.Console.WriteLine($"{counter++:D2} : {item.Value}");
+    }
+  }
+}
+#endif
